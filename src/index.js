3508507bb4bc7b5adb8c75f6e76817361c8ab189
Steve Garon <steve.garon@cyber.gc.ca>
2020-07-02 01:36:28 -0400

    Working on submit page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {
-  theme,
-  ThemeProvider,
-  CSSReset,
-  ColorModeProvider
+    theme,
+    ThemeProvider,
+    CSSReset,
+    ColorModeProvider, Flex, Box, Heading, Text
 } from "@chakra-ui/core";
 
-import Body from "./body";
+import Submit from "./submit";
 import Header from "./header";
 
-const breakpoints = ["300px", "768px", "1024px", "1440px"];
+const breakpoints = ["300px", "768px", "992px", "1440px"];
 breakpoints.sm = breakpoints[0];
 breakpoints.md = breakpoints[1];
 breakpoints.lg = breakpoints[2];
@@ -34,7 +34,11 @@ function App() {
     <div>
       <CSSReset />
       <Header />
-      <Body />
+      <Flex justify="center" px={{sm: "1rem", md: "0"}} py={{sm: "1rem", md: "2rem"}}>
+          <Box maxW={{sm: "100%", md: "750px", lg: "970px", xl: "1350px"}}>
+            <Submit />
+          </Box>
+        </Flex>
     </div>
   );
 }
--- src/submit.js
+++ src/submit.js
@@ -1,5 +1,30 @@
 import React, { useState, useEffect }  from "react";
-import { Heading, Box, Text } from "@chakra-ui/core";
+import {Flex, Image, Tabs, TabList, Tab, TabPanel, TabPanels, Box, Text, Link} from "@chakra-ui/core";
+import {useDropzone} from 'react-dropzone';
+import {AiOutlineSecurityScan} from 'react-icons/ai'
+
+function FileDropper(props) {
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  return (
+    <Box>
+      <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        <AiOutlineSecurityScan style={{fontSize: '140px'}}/>
+        <Text fontSize="lg">Select a file to scan</Text>
+      </div>
+      <aside>
+        <ul>{files}</ul>
+      </aside>
+    </Box>
+  );
+}
 
 function Submit() {
   const [currentTime, setCurrentTime] = useState(-1);
@@ -9,10 +34,38 @@ function Submit() {
     })
   }, []);
   return (
-    <Box p={5}>
-      <Heading>Welcome to Assemblyline</Heading>
-  <Text>This is just some text... don't worry about this. {currentTime}</Text>
-    </Box>
+    <>
+      <Flex justify="center"
+            marginTop="20px"
+            marginBottom="60px">
+        <Image
+            w={{sm: "75%", md: "initial"}}
+            className="banner"
+            src="banner.svg"
+            alt="Assemblyline Logo"/>
+      </Flex>
+      <Tabs variant="enclosed">
+        <TabList>
+          <Tab>File</Tab>
+          <Tab>Url</Tab>
+          <Tab>Options</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Box marginTop="30px">
+              <FileDropper/>
+              <Text color={{dark: "gray.200", light: "gray.600"}} marginTop="60px" fontSize="sm" textAlign="center">By clicking <i>Upload and Scan</i>, you consent to our <Link href="/tos.html">Terms of Service</Link>.</Text>
+            </Box>
+          </TabPanel>
+          <TabPanel>
+            <p>URL input box</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Service options</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
   );
 }
 
