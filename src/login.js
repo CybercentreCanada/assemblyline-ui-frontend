476ad02ad7abe7d9e967ed6b867c0a7b16bc37be
Steve Garon <steve.garon@cyber.gc.ca>
2020-07-03 00:59:19 -0400

    Fixed warnings

--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import {Box, Button, Image, Input, Stack, Text} from "@chakra-ui/core";
+import {Button, Image, Input, Stack} from "@chakra-ui/core";
 import React from "react";
 
 function Separator(){
@@ -21,10 +21,10 @@ function UserPassLogin(){
             <Input name="username" placeholder="Username"/>
             <Input type="password" name="password" placeholder="Password"/>
             <Button>Sign in</Button>
-            <div style={{"margin-top": "1rem", display: "block", "text-align": "center", "font-size": "0.75rem"}}>
+            <div style={{marginTop: "1rem", display: "block", textAlign: "center", fontSize: "0.75rem"}}>
                 Do not have an account? <Button size="xs" variant="link">Sign up!</Button>
             </div>
-            <div style={{"margin-top": ".5rem", display: "block", "text-align": "center", "font-size": "0.75rem"}}>
+            <div style={{marginTop: ".5rem", display: "block", textAlign: "center", fontSize: "0.75rem"}}>
                 Forgot password? <Button size="xs" variant="link">Reset it!</Button>
             </div>
         </Stack>
@@ -34,12 +34,11 @@ function UserPassLogin(){
 function LoginScreen(){
     return (
         <div style={{position: "fixed", top: '50%', left: "50%", transform: "translate(-50%, -50%)"}}>
-            <Box
-                display={"grid"}
+            <Stack
                 borderWidth={{sm: "0", md: "1px"}}
                 maxW={{sm: "xs", md: "22rem"}}
                 minW={{sm: "xs", md: "22rem"}}
-                rounded={"lg"}
+                rounded={"md"}
                 overflow={"hidden"}
                 boxShadow={{sm: "", md: "lg"}}
                 px={"2rem"}
@@ -48,7 +47,7 @@ function LoginScreen(){
                 <UserPassLogin/>
                 <Separator/>
                 <AuthProvider name={"Azure AD"}/>
-            </Box>
+            </Stack>
         </div>
     );
 }
