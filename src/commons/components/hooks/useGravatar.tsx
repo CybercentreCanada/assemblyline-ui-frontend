b703ba27962a04830860d42a3b6b56448e0049c6
Steve Garon <steve.garon@cyber.gc.ca>
2020-08-05 02:42:03 +0000

    Allow useGravatar hook to be used with null values

--- src/commons/components/hooks/useGravatar.tsx
+++ src/commons/components/hooks/useGravatar.tsx
@@ -1,9 +1,9 @@
 import { useMemo } from "react";
 const md5 = require("md5");
 
-const useGravatar = (email: string)  => {
+const useGravatar = (userEmail: string)  => {
+    let email = userEmail ? userEmail : ""
     return useMemo(() => `https://s.gravatar.com/avatar/${md5(email)}?d=404`, [email])
 }
 
 export default useGravatar
-
