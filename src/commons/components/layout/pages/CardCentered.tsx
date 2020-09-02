40c3896badc2e061198ebbee6d64d40743953ab3
Steve Garon <steve.garon@cyber.gc.ca>
2020-09-02 03:16:23 +0000

    Use Media query instead of withWidth

--- src/commons/components/layout/pages/CardCentered.tsx
+++ src/commons/components/layout/pages/CardCentered.tsx
@@ -1,37 +1,40 @@
-import { Card, isWidthDown, makeStyles, withWidth } from '@material-ui/core';
+import { Card, makeStyles, useMediaQuery, useTheme } from '@material-ui/core';
 import React from 'react';
 
-function CardCentered({ width, children }) {
-  const useStyles = makeStyles(theme => ({
-    card: {
-      display: 'flex',
-      flexDirection: 'column',
-      position: 'absolute',
-      left: '50%', // X
-      top: '50%', // Y
-      transform: 'translate(-50%, -50%)',
+const useStyles = makeStyles(theme => ({
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'absolute',
+    left: '50%', // X
+    top: '50%', // Y
+    transform: 'translate(-50%, -50%)',
+    maxWidth: '22rem',
+    backgroundColor: theme.palette.background.paper,
+    borderRadius: '4px',
+    [theme.breakpoints.down('xs')]: {
+      backgroundColor: theme.palette.background.default,
+      width: '100%',
       maxWidth: '22rem',
-      backgroundColor: theme.palette.background.paper,
-      borderRadius: '4px',
-      [theme.breakpoints.down('xs')]: {
-        backgroundColor: theme.palette.background.default,
-        width: '100%',
-        maxWidth: '22rem',
-        padding: '0rem 1rem 3rem'
-      },
-      [theme.breakpoints.up('sm')]: {
-        width: '22rem',
-        padding: '0 2rem 3rem'
-      }
+      padding: '0rem 1rem 3rem'
+    },
+    [theme.breakpoints.up('sm')]: {
+      width: '22rem',
+      padding: '0 2rem 3rem'
     }
-  }));
+  }
+}));
+
+function CardCentered({ children }) {
+  const theme = useTheme();
+  const isXs = useMediaQuery(theme.breakpoints.only('xs'));
   const classes = useStyles();
 
   return (
-    <Card elevation={isWidthDown('xs', width) ? 0 : 4} className={classes.card}>
+    <Card elevation={isXs ? 0 : 4} className={classes.card}>
       {children}
     </Card>
   );
 }
 
-export default withWidth()(CardCentered);
+export default CardCentered;
