96fce2b4fe51b481b144fb458a4ce4d9ec968a4f
Steve Garon <steve.garon@cyber.gc.ca>
2020-09-03 02:06:05 +0000

    Created admin links page

--- src/components/routes/admin.tsx
+++ src/components/routes/admin.tsx
@@ -1,5 +1,23 @@
+import { Box, Button, Typography } from '@material-ui/core';
+import useMyLayout from 'components/hooks/useMyLayout';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Admin() {
-  return <div>admin</div>;
+  const layout = useMyLayout();
+
+  return (
+    <Box display="flex" flexDirection="row" flexWrap="wrap" justifyContent="space-around">
+      {layout.topnav.adminMenu.map((e, i) => {
+        return (
+          <Button key={i} component={Link} to={e.route}>
+            <Box height="300px" width="300px" p={10} textAlign="center">
+              {React.cloneElement(e.icon, { style: { fontSize: '8rem' } }, null)}
+              <Typography>{e.name}</Typography>
+            </Box>
+          </Button>
+        );
+      })}
+    </Box>
+  );
 }
