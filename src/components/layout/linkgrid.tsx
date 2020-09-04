5d022af5bc71fe93c5df8a1141beb00f213c1f28
Steve Garon <steve.garon@cyber.gc.ca>
2020-09-04 18:14:27 +0000

    Change link color

--- src/components/layout/linkgrid.tsx
+++ src/components/layout/linkgrid.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from '@material-ui/core';
+import { Box, Button, Typography, useTheme } from '@material-ui/core';
 import LinkIcon from '@material-ui/icons/Link';
 import { LeftNavItemProps } from 'commons/components/layout/leftnav/LeftNavDrawer';
 import { UserMenuElement } from 'commons/components/layout/topnav/UserProfile';
@@ -10,12 +10,13 @@ type LinkGridProps = {
 };
 
 export default function LinkGrid<LinkGridProps>({ items }) {
+  const theme = useTheme();
   return (
     <Box display="flex" flexDirection="row" flexWrap="wrap" justifyContent="space-around">
       {items.map((e, i) => {
         return (
           <Button key={i} component={Link} to={e.route}>
-            <Box height="300px" width="300px" p={10} textAlign="center">
+            <Box height="300px" width="300px" p={10} textAlign="center" color={theme.palette.action.active}>
               {e.icon ? (
                 React.cloneElement(e.icon, { style: { fontSize: '8rem' } }, null)
               ) : (
