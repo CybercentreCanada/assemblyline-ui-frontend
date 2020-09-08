dae1eb4196bb3a1348cbb9dc2158fa1c3ebd8616
broy1 <benoit.roy@cyber.gc.ca>
2020-09-08 11:51:45 -0400

    To inifinity...and flex box.

--- src/components/elements/lists/infinite-list.tsx
+++ src/components/elements/lists/infinite-list.tsx
@@ -9,8 +9,9 @@ const useStyles = makeStyles(theme => ({
     width: '100%',
 
     position: 'relative',
-    overflow: 'auto'
+    overflow: 'auto',
     // height: '500px'
+    outline: 'none'
   },
   infiniteListInnerCt: {
     position: 'relative',
@@ -62,34 +63,68 @@ export default function InfiniteList<I extends InfiniteListItem>({
   const containerEl = useRef<HTMLDivElement>();
   const innerEl = useRef<HTMLDivElement>();
 
-  const [displayItems, setDisplayItems] = useState<{ index: number; item: I }[]>([]);
+  const [frame, setFrame] = useState<{ index: number; item: I }[]>([]);
 
-  //
-  const onScroll = (event: React.UIEvent<HTMLElement>) => {
-    const fH = containerEl.current.getBoundingClientRect().height;
-    const sT = event.currentTarget.scrollTop;
+  const computeFrame = (_items: I[], _rowHeight: number) => {
+    const { current: _containerEl } = containerEl;
+    const { current: _innerEl } = innerEl;
+    const tH = _innerEl.getBoundingClientRect().height;
+    const fH = _containerEl.getBoundingClientRect().height;
+    const sT = _containerEl.scrollTop;
     const cP = fH + sT;
+    const rH = tH - cP;
 
-    const itemCount = Math.ceil(fH / rowHeight);
+    const itemCount = Math.ceil(fH / _rowHeight);
 
-    const topIndex = Math.floor(sT / rowHeight);
+    const topIndex = Math.floor(sT / _rowHeight);
 
     console.log(`${topIndex}-${topIndex + itemCount}`);
 
-    setDisplayItems(items.slice(topIndex, topIndex + itemCount).map((item, index) => ({ index, item })));
+    const displayItems = _items
+      .slice(topIndex, topIndex + itemCount)
+      .map((item, index) => ({ index: topIndex + index, item }));
+
+    return { displayItems, rH };
+  };
+
+  //
+  const onScroll = (event: React.UIEvent<HTMLElement>) => {
+    // const tH = innerEl.current.getBoundingClientRect().height;
+    // const fH = containerEl.current.getBoundingClientRect().height;
+    // const sT = event.currentTarget.scrollTop;
+    // const cP = fH + sT;
+    // const rH = tH - cP;
+
+    // const itemCount = Math.ceil(fH / rowHeight);
+
+    // const topIndex = Math.floor(sT / rowHeight);
+
+    // console.log(`${topIndex}-${topIndex + itemCount}`);
+
+    // const _items = items
+    //   .slice(topIndex, topIndex + itemCount)
+    //   .map((item, index) => ({ index: topIndex + index, item }));
+
+    const { displayItems, rH } = computeFrame(items, rowHeight);
+
+    setFrame(displayItems);
+
+    if (rH === 0) {
+      console.log('fetching next page of items...');
+      onNextPage(items.length, items.length + 10);
+    }
   };
 
   // Render children relative to top
   // Row renderer.
   const rowRenderer = (displayItem: { index: number; item: I }) => {
-    const sT = containerEl.current.scrollTop;
     return (
       <Box
         mr={0}
         key={`listitem[${displayItem.index}].id[${displayItem.item.id}]`}
         onClick={() => onItemSelected(displayItem.item)}
         style={{
-          top: sT + displayItem.index * rowHeight,
+          top: displayItem.index * rowHeight,
           left: 0,
           position: 'absolute',
           width: '100%',
@@ -108,17 +143,20 @@ export default function InfiniteList<I extends InfiniteListItem>({
     // initialize scrolling container height.
     innerEl.current.style.height = `${items.length * rowHeight}px`;
 
-    const fH = containerEl.current.getBoundingClientRect().height;
+    const { displayItems } = computeFrame(items, rowHeight);
+    setFrame(displayItems);
 
-    const itemCount = Math.ceil(fH / rowHeight);
-    setDisplayItems(items.slice(0, itemCount).map((item, index) => ({ index, item })));
-    console.log(itemCount);
+    // const fH = containerEl.current.getBoundingClientRect().height;
+
+    // const itemCount = Math.ceil(fH / rowHeight);
+    // setDisplayItems(items.slice(0, itemCount).map((item, index) => ({ index, item })));
+    // console.log(itemCount);
   }, [items, rowHeight]);
 
   return (
     <div ref={containerEl} className={classes.infiniteListCt} tabIndex={-1} onScroll={onScroll}>
       <div ref={innerEl} className={classes.infiniteListInnerCt}>
-        {displayItems.map(item => rowRenderer(item))}
+        {frame.map(item => rowRenderer(item))}
       </div>
     </div>
   );
