657a6e9ee4bdbdade541f5c7e0c8ca3da0340c04
Steve Garon <steve.garon@cyber.gc.ca>
2020-09-11 14:43:41 +0000

    Extracted 403 translation to its own file

--- src/components/routes/403.tsx
+++ src/components/routes/403.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import BlockIcon from '@material-ui/icons/Block';
@@ -10,22 +11,26 @@ type ForbiddenPageProps = {
 };
 
 export default function ForbiddenPage<ForbiddenPageProps>({ disabled = false }) {
-  const { t } = useTranslation();
+  const { t } = useTranslation(['error403']);
+  const theme = useTheme();
   return (
     <PageCenter width="65%">
       <Box pt={6} textAlign="center" fontSize={200}>
-        <BlockIcon color="secondary" fontSize="inherit" />
+        <BlockIcon
+          style={{ color: theme.palette.type === 'dark' ? theme.palette.error.light : theme.palette.error.dark }}
+          fontSize="inherit"
+        />
       </Box>
       <Box pb={2}>
-        <Typography variant="h3">{t('page.403.title')}</Typography>
+        <Typography variant="h3">{t('title')}</Typography>
       </Box>
       {disabled ? (
         <Box>
-          <Typography variant="h6">{t('page.403.disabled')}</Typography>
+          <Typography variant="h6">{t('disabled')}</Typography>
         </Box>
       ) : (
         <Box>
-          <Typography variant="h6">{t('page.403.not_allowed')}</Typography>
+          <Typography variant="h6">{t('not_allowed')}</Typography>
         </Box>
       )}
     </PageCenter>
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,17 +1,21 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import Error403EN from 'locales/en/403.json';
 import tosEN from 'locales/en/tos.json';
 import translationEN from 'locales/en/translation.json';
+import Error403FR from 'locales/fr/403.json';
 import tosFR from 'locales/fr/tos.json';
 import translationFR from 'locales/fr/translation.json';
 import { initReactI18next } from 'react-i18next';
 
 const resources = {
   en: {
+    error403: Error403EN,
     translation: translationEN,
     tos: tosEN
   },
   fr: {
+    error403: Error403FR,
     translation: translationFR,
     tos: tosFR
   }
