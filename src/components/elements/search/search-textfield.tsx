00aeea1fd9e4f174b5b879f384f6d5896172aca0
broy1 <benoit.roy@cyber.gc.ca>
2020-09-16 16:26:35 -0400

    searchbar  autocomplete impl.

--- src/components/elements/search/search-textfield.tsx
+++ src/components/elements/search/search-textfield.tsx
@@ -1,21 +1,28 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import { Box, makeStyles, TextField } from '@material-ui/core';
+import { Box, makeStyles, TextField, Typography } from '@material-ui/core';
 import React, { useEffect, useRef, useState } from 'react';
 import { isArrowDown, isArrowUp, isEnter, isEscape } from '../utils/keyboard';
 import Throttler from '../utils/throttler';
 
 const useStyles = makeStyles(theme => ({
-  searchTextFieldCt: {
-    position: 'relative'
+  searchTextFieldOptionsCt: {
+    position: 'relative',
+    height: 0
   },
-  searchTextFieldInner: {
+  searchTextFieldOptionsInner: {
     position: 'absolute',
     overflow: 'auto',
     zIndex: 1,
     top: 0,
     minWidth: 400,
     maxHeight: 250,
-    backgroundColor: theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[200]
+    backgroundColor: theme.palette.type === 'dark' ? 'hsl(0, 0%, 10%)' : 'hsl(0, 0%, 90%)',
+    boxShadow: theme.shadows[4]
+    // border: '1px solid transparent',
+    // borderRightColor: theme.palette.type === 'dark' ? 'hsl(0, 0%, 25%)' : 'hsl(0, 0%, 85%)',
+    // borderBottomColor: theme.palette.type === 'dark' ? 'hsl(0, 0%, 25%)' : 'hsl(0, 0%, 85%)',
+    // borderLeftColor: theme.palette.type === 'dark' ? 'hsl(0, 0%, 25%)' : 'hsl(0, 0%, 85%)',
+    // borderRadius: '3px'
   },
   searchTextFieldItem: {
     padding: theme.spacing(1),
@@ -53,13 +60,14 @@ const SearchTextField: React.FC<SearchTextFieldProps> = ({
   onClear
 }) => {
   const classes = useStyles();
+  const [precursor, setPrecursor] = useState<string>(value);
   const [cursor, setCursor] = useState<number>(-1);
   const [open, setOpen] = useState<boolean>(false);
   const [filteredOptions, setFilteredOptions] = useState<string[]>(options);
   const element = useRef<HTMLDivElement>();
   const optionsElement = useRef<HTMLDivElement>();
 
-  // Ensure we update the filtered items if the list of options changes.
+  // Ensure we update options if a new list is provided.
   useEffect(() => setFilteredOptions(options), [options]);
 
   // Just the the text input element.
@@ -71,6 +79,14 @@ const SearchTextField: React.FC<SearchTextFieldProps> = ({
   const _onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value: _value } = event.currentTarget;
     onChange(_value, options);
+
+    //
+    const inputEl = getInputEl();
+    const thisCursor = inputEl.selectionStart;
+    setPrecursor(_value.substr(0, thisCursor));
+
+    //
+    filterOptions(_value);
   };
 
   // Handler for KeyDown event on either the text input of the options menu.
@@ -91,14 +107,14 @@ const SearchTextField: React.FC<SearchTextFieldProps> = ({
     } else if (isArrowUp(keyCode)) {
       event.preventDefault();
       if (open) {
-        const nextIndex = cursor - 1 > -1 ? cursor - 1 : options.length - 1;
+        const nextIndex = cursor - 1 > -1 ? cursor - 1 : filteredOptions.length - 1;
         setCursor(nextIndex);
         scrollTo(nextIndex);
       }
     } else if (isArrowDown(keyCode)) {
       event.preventDefault();
       if (open) {
-        const nextIndex = cursor + 1 < options.length ? cursor + 1 : 0;
+        const nextIndex = cursor + 1 < filteredOptions.length ? cursor + 1 : 0;
         setCursor(nextIndex);
         scrollTo(nextIndex);
       } else {
@@ -129,29 +145,26 @@ const SearchTextField: React.FC<SearchTextFieldProps> = ({
 
   // Scroll options element to the element at specified position.
   const scrollTo = (position: number) => {
+    console.log(`scollTo:${position}`);
     optionsElement.current
       .querySelector(`[data-searchtextfieldoption-position="${position}"`)
       .scrollIntoView({ block: 'nearest' });
   };
 
-  // TODO: Filtering logic.
-  const filterOptions = () => {
+  // Options filter.
+  // Split filter value by single space.
+  // Grab nearest group to the left of cursor.
+  // Filter entire list with that value if not empty.
+  const filterOptions = (inputValue: string) => {
     let _options = options;
-    // Split filter value by whitespace.
-    // Grab nearest group to the left of cursor.
-    // Filter entire list with that value.
     const inputEl = getInputEl();
     const thisCursor = inputEl.selectionStart;
-    const _value = inputEl.value;
-    // const parts = _value.substr(0, thisCursor).split(/\s+/);
-    const parts = _value.substr(0, thisCursor).split(' ');
+    const parts = inputValue.substr(0, thisCursor).split(' ');
     if (parts[parts.length - 1] !== '') {
       const filterValue = parts[parts.length - 1];
-      console.log(filterValue);
       _options = _options.filter(option => option.includes(filterValue));
     }
-    console.log(parts);
-    return _options.length > 0 ? _options : options;
+    setFilteredOptions(_options.length > 0 ? _options : options);
   };
 
   return (
@@ -168,9 +181,12 @@ const SearchTextField: React.FC<SearchTextFieldProps> = ({
         fullWidth
       />
       {open ? (
-        <div ref={optionsElement} className={classes.searchTextFieldCt} tabIndex={-1} onKeyDown={_onKeyDown}>
-          <div className={classes.searchTextFieldInner}>
-            {filterOptions().map((item, index) => (
+        <div ref={optionsElement} className={classes.searchTextFieldOptionsCt} tabIndex={-1} onKeyDown={_onKeyDown}>
+          <div style={{ display: 'inline-block', height: 0, lineHeight: 0, overflow: 'hidden' }}>
+            <Typography> {precursor}</Typography>
+          </div>
+          <div style={{ display: 'inline-block' }} className={classes.searchTextFieldOptionsInner}>
+            {filteredOptions.map((item, index) => (
               <SearchTextOption
                 key={`SearchTextField-item-${index}`}
                 text={item}
