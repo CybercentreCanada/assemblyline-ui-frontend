f4d8212212b933a4b2b216f779509b70a059715b
broy1 <benoit.roy@cyber.gc.ca>
2020-09-17 10:37:54 -0400

    Fixed up few bugs in searchbar contentassist.

--- src/components/elements/search/search-textfield.tsx
+++ src/components/elements/search/search-textfield.tsx
@@ -1,16 +1,17 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import { Box, makeStyles, TextField, Typography } from '@material-ui/core';
+import { Box, makeStyles, TextField, Typography, useMediaQuery, useTheme } from '@material-ui/core';
 import React, { useEffect, useRef, useState } from 'react';
-import { insertText } from '../utils/brower';
+import { insertText } from '../utils/browser';
 import { isArrowDown, isArrowLeft, isArrowRight, isArrowUp, isEnter, isEscape } from '../utils/keyboard';
-import Throttler from '../utils/throttler';
 
 const useStyles = makeStyles(theme => ({
   searchTextFieldOptionsCt: {
     position: 'relative',
-    height: 0
+    height: 0,
+    outline: 'none'
   },
   searchTextFieldOptionsInner: {
+    display: 'inline-block',
     position: 'absolute',
     overflow: 'auto',
     zIndex: 1,
@@ -19,11 +20,13 @@ const useStyles = makeStyles(theme => ({
     maxHeight: 250,
     backgroundColor: theme.palette.type === 'dark' ? 'hsl(0, 0%, 10%)' : 'hsl(0, 0%, 90%)',
     boxShadow: theme.shadows[4]
-    // border: '1px solid transparent',
-    // borderRightColor: theme.palette.type === 'dark' ? 'hsl(0, 0%, 25%)' : 'hsl(0, 0%, 85%)',
-    // borderBottomColor: theme.palette.type === 'dark' ? 'hsl(0, 0%, 25%)' : 'hsl(0, 0%, 85%)',
-    // borderLeftColor: theme.palette.type === 'dark' ? 'hsl(0, 0%, 25%)' : 'hsl(0, 0%, 85%)',
-    // borderRadius: '3px'
+  },
+  serachTextFieldOptionsInnerSpacer: {
+    display: 'inline-block',
+    height: 0,
+    lineHeight: 0,
+    overflow: 'hidden',
+    whiteSpace: 'pre'
   },
   searchTextFieldItem: {
     padding: theme.spacing(1),
@@ -38,10 +41,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const KEY_THROTTLER = new Throttler(10);
-
 interface SearchTextFieldProps {
-  // open: boolean;
   value: string;
   options: string[];
   disabled?: boolean;
@@ -60,19 +60,20 @@ const SearchTextField: React.FC<SearchTextFieldProps> = ({
   onSelection,
   onClear
 }) => {
+  const theme = useTheme();
   const classes = useStyles();
-
   const [cursor, setCursor] = useState<number>(-1);
   const [precursor, setPrecursor] = useState<string>('');
   const [filteredOptions, setFilteredOptions] = useState<string[]>(options);
   const [open, setOpen] = useState<boolean>(false);
   const element = useRef<HTMLDivElement>();
   const optionsElement = useRef<HTMLDivElement>();
+  const isLTEMedium = useMediaQuery(theme.breakpoints.up('md'));
 
   // Ensure we update options if a new list is provided.
   useEffect(() => setFilteredOptions(options), [options]);
 
-  // Just the the text input element.
+  // Get the the text input element.
   const getInputEl = () => {
     return element.current.querySelector('input');
   };
@@ -81,28 +82,29 @@ const SearchTextField: React.FC<SearchTextFieldProps> = ({
   const _onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value: _value } = event.currentTarget;
     onChange(_value, options);
-
-    //
     filterOptions(_value);
   };
 
   // Handler for KeyDown event on either the text input of the options menu.
   const _onKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
     const { keyCode } = event;
-    console.log(`key[${keyCode}]`);
+    // console.log(`key[${keyCode}]`);
     if (isEnter(keyCode)) {
+      // key[ENTER ]: handler
       if (open) {
-        onOptionSelection(options[cursor]);
+        onOptionSelection(filteredOptions[cursor]);
       } else {
         onSearch(value);
       }
     } else if (isEscape(keyCode)) {
+      // key[ESCAPE]: handler
       if (open) {
         onOptionsClose();
       } else {
         onClear();
       }
     } else if (isArrowUp(keyCode)) {
+      // key[ARROW_UP]: handler
       event.preventDefault();
       if (open) {
         const nextIndex = cursor - 1 > -1 ? cursor - 1 : filteredOptions.length - 1;
@@ -110,19 +112,28 @@ const SearchTextField: React.FC<SearchTextFieldProps> = ({
         scrollTo(nextIndex);
       }
     } else if (isArrowDown(keyCode)) {
+      // key[ARROW_DOWN]: handler
       event.preventDefault();
       if (open) {
         const nextIndex = cursor + 1 < filteredOptions.length ? cursor + 1 : 0;
         setCursor(nextIndex);
         scrollTo(nextIndex);
       } else {
-        setOpen(true);
+        onOptionsOpen();
       }
     } else if (isArrowLeft(keyCode)) {
-      filterOptions(value);
+      // key[ARROW_LEFT]: handler
+      filterOptions(value, -1);
     } else if (isArrowRight(keyCode)) {
-      filterOptions(value);
+      // key[ARROW_RIGHT]: handler
+      filterOptions(value, 1);
     }
+  };
+
+  // Handler for when the options box opens.
+  const onOptionsOpen = () => {
+    setPrecursor(extractPrecursor(value));
+    setOpen(true);
   };
 
   // Handler for when the options box closes.
@@ -137,43 +148,57 @@ const SearchTextField: React.FC<SearchTextFieldProps> = ({
     if (option) {
       const inputEl = getInputEl();
       const thisCursor = inputEl.selectionStart;
-      const nextCursor = thisCursor + option.length;
-      insertText(inputEl, nextCursor, nextCursor, option);
+      insertText(inputEl, thisCursor, thisCursor, option);
       onOptionsClose();
       onChange(inputEl.value, options);
+      if (onSelection) {
+        onSelection(option);
+      }
     }
   };
 
   // Scroll options element to the element at specified position.
   const scrollTo = (position: number) => {
-    console.log(`scollTo:${position}`);
     optionsElement.current
       .querySelector(`[data-searchtextfieldoption-position="${position}"`)
       .scrollIntoView({ block: 'nearest' });
   };
 
-  // Options filter.
+  // Filter the options.
   // Split filter value by single space.
   // Grab nearest group to the left of cursor.
   // Filter entire list with that value if not empty.
-  const filterOptions = (inputValue: string) => {
-    //
-
-    // FIXME -> filter value and precursor not quite keeping up
-    asdfa;
-
+  const filterOptions = (inputValue: string, selectionOffset = 0) => {
     let _options = options;
-    const thisCursor = getInputEl().selectionStart;
-    const _precursor = inputValue.substr(0, thisCursor);
-    const parts = inputValue.substr(0, thisCursor).split(' ');
+
+    // Grab the part of the text before the precursor.
+    const _precursor = extractPrecursor(inputValue, selectionOffset);
+
+    // Split that with a single whitespace.
+    const parts = _precursor.split(' ');
+    // The the part just before the cursor isn't empty,
+    //  then we use that to filter the options.
     if (parts[parts.length - 1] !== '') {
       const filterValue = parts[parts.length - 1];
-      console.log(`precursor: ${precursor}, filterValue: ${filterValue}, cursor: ${thisCursor}`);
       _options = _options.filter(option => option.includes(filterValue));
     }
 
+    // Update states...
     setPrecursor(_precursor);
+
+    // If filtered options is empty, then we return all options..
     setFilteredOptions(_options.length > 0 ? _options : options);
+  };
+
+  const extractPrecursor = (inputValue: string, offset = 0) => {
+    // Extract text up to position of cursor in input element.
+    // with left/right arrow keys, the cursor isn't yet updated to new position
+    //  when the event is received, therefore we use offset.
+    //  +1 for right arrow, -1 for left arrow.
+    const thisCursor = getInputEl().selectionStart + offset;
+
+    // Grab only the part of the text before the cursor.
+    return inputValue.substr(0, thisCursor);
   };
 
   return (
@@ -189,12 +214,12 @@ const SearchTextField: React.FC<SearchTextFieldProps> = ({
         autoFocus
         fullWidth
       />
-      {open ? (
+      {open && isLTEMedium ? (
         <div ref={optionsElement} className={classes.searchTextFieldOptionsCt} tabIndex={-1} onKeyDown={_onKeyDown}>
-          <div style={{ display: 'inline-block', height: 0, lineHeight: 0, overflow: 'hidden', whiteSpace: 'pre' }}>
+          <div className={classes.serachTextFieldOptionsInnerSpacer}>
             <Typography>{precursor}</Typography>
           </div>
-          <div style={{ display: 'inline-block' }} className={classes.searchTextFieldOptionsInner}>
+          <div className={classes.searchTextFieldOptionsInner}>
             {filteredOptions.map((item, index) => (
               <SearchTextOption
                 key={`SearchTextField-item-${index}`}
--- src/components/elements/utils/browser.ts
+++ src/components/elements/utils/browser.ts
@@ -4,8 +4,8 @@ import browser from 'browser-detect';
 export function insertText(input, from, to, text) {
   const browserName = browser().name;
   // execCommand doesn't work in FF.
+  input.focus();
   if (browserName === 'firefox') {
-    console.log('using firefox.');
     _insertTextFF(input, from, to, text);
   } else {
     _insertText(input, from, to, text);
@@ -13,7 +13,7 @@ export function insertText(input, from, to, text) {
 }
 
 function _insertText(input, from, to, text) {
-  const thisCursor = input.selectionStart;
+  // const thisCursor = input.selectionStart;
   // const nextCursor = thisCursor + text.length;
   input.setSelectionRange(from, to);
   document.execCommand('insertText', false, text);
@@ -21,7 +21,6 @@ function _insertText(input, from, to, text) {
 
 function _insertTextFF(input, from, to, text) {
   // UNDO is broken in FF.
-  input.focus();
   const oldText = input.value;
   const prefix = oldText.substring(0, from);
   const suffix = oldText.substring(to);
