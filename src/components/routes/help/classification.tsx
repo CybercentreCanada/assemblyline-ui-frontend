22e05d3530848e81fe6a4275dbc8f5a01fc11643
Steve Garon <steve.garon@cyber.gc.ca>
2020-09-21 19:45:01 +0000

    Added classification help page

--- src/components/routes/help/classification.tsx
+++ src/components/routes/help/classification.tsx
@@ -1,18 +1,299 @@
-import { Box } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
 import useUser from 'commons/components/hooks/useAppUser';
 import PageCenter from 'commons/components/layout/pages/PageCenter';
+import useALContext from 'components/hooks/useALContext';
 import { CustomUser } from 'components/hooks/useMyUser';
 import NotFoundPage from 'components/routes/404_dl';
+import Classification from 'components/visual/Classification';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
-export default function Classification() {
+export default function HelpClassification() {
   const { t } = useTranslation(['helpClassification']);
+  const { classification: c12nDef } = useALContext();
   const { user: currentUser } = useUser<CustomUser>();
 
   return currentUser.c12n_enforcing ? (
     <PageCenter>
-      <Box textAlign="left">{t('Classification')}</Box>
+      <Box textAlign="left" pb={4}>
+        <Typography variant="h5" gutterBottom>
+          {t('desc_title')}
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          {t('desc_1')}
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          {t('desc_2')}
+        </Typography>
+      </Box>
+      <Box textAlign="left" pb={4}>
+        <Typography variant="h5" gutterBottom>
+          {t('submission_title')}
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          {t('submission_desc')}
+        </Typography>
+        <Typography variant="subtitle2" gutterBottom>
+          {t('ex_title')}
+        </Typography>
+        <Box pl={2}>
+          <Typography variant="body2" gutterBottom>
+            {t('assumptions')}
+          </Typography>
+          <ul>
+            <li>
+              <Trans ns="helpClassification" i18nKey="submission_a_1" components={{ bold: <strong /> }} />
+              <Classification inline size="tiny" type="text" c12n={c12nDef ? c12nDef.RESTRICTED : null} format="long" />
+            </li>
+            <li>
+              <Trans ns="helpClassification" i18nKey="submission_a_2" components={{ bold: <strong /> }} />
+              <Classification
+                inline
+                size="tiny"
+                type="text"
+                c12n={c12nDef ? c12nDef.UNRESTRICTED : null}
+                format="long"
+              />
+            </li>
+            <li>
+              <Trans ns="helpClassification" i18nKey="submission_a_3" components={{ bold: <strong /> }} />
+            </li>
+          </ul>
+          <Typography variant="body2" gutterBottom>
+            <Trans ns="helpClassification" i18nKey="submission_a_res_1_p1" components={{ bold: <strong /> }} />
+            <Classification inline size="tiny" type="text" c12n={c12nDef ? c12nDef.UNRESTRICTED : null} format="long" />
+            <Trans ns="helpClassification" i18nKey="submission_a_res_1_p2" components={{ bold: <strong /> }} />
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            <Trans
+              ns="helpClassification"
+              i18nKey="submission_a_res_2_p1"
+              components={{ italic: <i />, bold: <strong /> }}
+            />
+            <Classification inline size="tiny" type="text" c12n={c12nDef ? c12nDef.RESTRICTED : null} format="long" />
+            <Trans
+              ns="helpClassification"
+              i18nKey="submission_a_res_2_p2"
+              components={{ italic: <i />, bold: <strong /> }}
+            />
+          </Typography>
+        </Box>
+      </Box>
+      <Box textAlign="left" pb={4}>
+        <Typography variant="h5" gutterBottom>
+          {t('result_title')}
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          {t('result_desc')}
+        </Typography>
+        <Typography variant="subtitle2" gutterBottom>
+          {t('ex_title')}
+        </Typography>
+        <Box pl={2}>
+          <Typography variant="body2" gutterBottom>
+            {t('assumptions')}
+          </Typography>
+          <ul>
+            <li>
+              <Trans ns="helpClassification" i18nKey="result_a_1" components={{ bold: <strong /> }} />
+              <Classification inline size="tiny" type="text" c12n={c12nDef ? c12nDef.RESTRICTED : null} format="long" />
+            </li>
+            <li>
+              <Trans ns="helpClassification" i18nKey="result_a_2" components={{ bold: <strong /> }} />
+              <Classification
+                inline
+                size="tiny"
+                type="text"
+                c12n={c12nDef ? c12nDef.UNRESTRICTED : null}
+                format="long"
+              />
+            </li>
+            <li>
+              <Trans ns="helpClassification" i18nKey="result_a_3" components={{ bold: <strong /> }} />
+              <Classification
+                inline
+                size="tiny"
+                type="text"
+                c12n={c12nDef ? c12nDef.UNRESTRICTED : null}
+                format="long"
+              />
+            </li>
+            <li>
+              <Trans ns="helpClassification" i18nKey="result_a_4" components={{ bold: <strong /> }} />
+              <Classification inline size="tiny" type="text" c12n={c12nDef ? c12nDef.RESTRICTED : null} format="long" />
+            </li>
+          </ul>
+          <Typography variant="body2" gutterBottom>
+            <Trans ns="helpClassification" i18nKey="result_a_res_1_p1" components={{ bold: <strong /> }} />
+            <Classification inline size="tiny" type="text" c12n={c12nDef ? c12nDef.RESTRICTED : null} format="long" />
+            <Trans ns="helpClassification" i18nKey="result_a_res_1_p2" components={{ bold: <strong /> }} />
+            <Classification inline size="tiny" type="text" c12n={c12nDef ? c12nDef.UNRESTRICTED : null} format="long" />
+            <Trans ns="helpClassification" i18nKey="result_a_res_1_p3" components={{ bold: <strong /> }} />
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            <Trans ns="helpClassification" i18nKey="result_a_res_2_p1" components={{ bold: <strong /> }} />
+            <Classification inline size="tiny" type="text" c12n={c12nDef ? c12nDef.RESTRICTED : null} format="long" />
+            <Trans ns="helpClassification" i18nKey="result_a_res_2_p2" components={{ bold: <strong /> }} />
+          </Typography>
+        </Box>
+      </Box>
+      <Box textAlign="left">
+        <Typography variant="h5" gutterBottom>
+          {t('validation_title')}
+        </Typography>
+      </Box>
+      <Box textAlign="left" pb={1}>
+        <Typography variant="h6" gutterBottom>
+          {t('validation_level')}
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          <Trans ns="helpClassification" i18nKey="validation_level_desc" components={{ bold: <strong /> }} />
+        </Typography>
+        <Typography variant="subtitle2" gutterBottom>
+          {t('validation_level_supported')}
+        </Typography>
+        <ul>
+          {c12nDef
+            ? c12nDef.original_definition.levels.map((lvl, idx) => {
+                return (
+                  <li key={idx}>
+                    <Classification inline size="tiny" type="text" c12n={lvl.name} format="long" /> :: {lvl.description}
+                  </li>
+                );
+              })
+            : [...Array(2)].map((_, i) => {
+                return (
+                  <li>
+                    <Skeleton />
+                  </li>
+                );
+              })}
+        </ul>
+      </Box>
+      <Box textAlign="left" pb={1}>
+        <Typography variant="h6" gutterBottom>
+          {t('validation_required')}
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          <Trans ns="helpClassification" i18nKey="validation_required_desc" components={{ bold: <strong /> }} />
+        </Typography>
+        <Typography variant="subtitle2" gutterBottom>
+          {t('validation_required_supported')}
+        </Typography>
+        <ul>
+          {c12nDef ? (
+            c12nDef.original_definition.required.length !== 0 ? (
+              c12nDef.original_definition.required.map((req, idx) => {
+                return (
+                  <li key={idx}>
+                    <Box display="inline-block" fontWeight={700}>
+                      {req.name}
+                    </Box>{' '}
+                    :: {req.description}
+                  </li>
+                );
+              })
+            ) : (
+              <Typography variant="body2" color="secondary">
+                {t('validation_required_none')}
+              </Typography>
+            )
+          ) : (
+            [...Array(2)].map((_, i) => {
+              return (
+                <li>
+                  <Skeleton />
+                </li>
+              );
+            })
+          )}
+        </ul>
+      </Box>
+      <Box textAlign="left" pb={1}>
+        <Typography variant="h6" gutterBottom>
+          {t('validation_groups')}
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          <Trans ns="helpClassification" i18nKey="validation_groups_desc" components={{ bold: <strong /> }} />
+        </Typography>
+        <Typography variant="subtitle2" gutterBottom>
+          {t('validation_groups_supported')}
+        </Typography>
+        <ul>
+          {c12nDef ? (
+            c12nDef.original_definition.groups.length !== 0 ? (
+              c12nDef.original_definition.groups.map((grp, idx) => {
+                return (
+                  <li key={idx}>
+                    <Box display="inline-block" fontWeight={700}>
+                      {grp.name}
+                    </Box>{' '}
+                    :: {grp.description}
+                  </li>
+                );
+              })
+            ) : (
+              <Typography variant="body2" color="secondary">
+                {t('validation_groups_none')}
+              </Typography>
+            )
+          ) : (
+            [...Array(2)].map((_, i) => {
+              return (
+                <li>
+                  <Skeleton />
+                </li>
+              );
+            })
+          )}
+        </ul>
+        <Typography variant="subtitle2" gutterBottom color="error">
+          <Trans ns="helpClassification" i18nKey="validation_groups_note" components={{ bold: <strong /> }} />
+        </Typography>
+      </Box>
+      <Box textAlign="left" pb={1}>
+        <Typography variant="h6" gutterBottom>
+          {t('validation_subgroups')}
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          <Trans ns="helpClassification" i18nKey="validation_subgroups_desc" components={{ bold: <strong /> }} />
+        </Typography>
+        <Typography variant="subtitle2" gutterBottom>
+          {t('validation_subgroups_supported')}
+        </Typography>
+        <ul>
+          {c12nDef ? (
+            c12nDef.original_definition.subgroups.length !== 0 ? (
+              c12nDef.original_definition.subgroups.map((sgrp, idx) => {
+                return (
+                  <li key={idx}>
+                    <Box display="inline-block" fontWeight={700}>
+                      {sgrp.name}
+                    </Box>{' '}
+                    :: {sgrp.description}
+                  </li>
+                );
+              })
+            ) : (
+              <Typography variant="body2" color="secondary">
+                {t('validation_subgroups_none')}
+              </Typography>
+            )
+          ) : (
+            [...Array(2)].map((_, i) => {
+              return (
+                <li>
+                  <Skeleton />
+                </li>
+              );
+            })
+          )}
+        </ul>
+        <Typography variant="subtitle2" gutterBottom color="error">
+          <Trans ns="helpClassification" i18nKey="validation_subgroups_note" components={{ bold: <strong /> }} />
+        </Typography>
+      </Box>
     </PageCenter>
   ) : (
     <NotFoundPage />
