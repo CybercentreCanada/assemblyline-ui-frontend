3a827a1588883d14d1768a4594e3446674207bb0
Steve Garon <steve.garon@cyber.gc.ca>
2020-10-06 18:18:09 +0000

    Minor visual change in submission list

--- src/components/routes/submissions.tsx
+++ src/components/routes/submissions.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from '@material-ui/core';
+import { CircularProgress, Tooltip, useTheme } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -10,6 +10,7 @@ import Typography from '@material-ui/core/Typography';
 import PageFullWidth from 'commons/components/layout/pages/PageFullWidth';
 import useMyAPI from 'components/hooks/useMyAPI';
 import Classification from 'components/visual/Classification';
+import SubmissionState from 'components/visual/SubmissionState';
 import Verdict from 'components/visual/Verdict';
 import 'moment/locale/fr';
 import React, { useEffect, useState } from 'react';
@@ -19,7 +20,7 @@ import { useHistory } from 'react-router-dom';
 
 export default function Submissions() {
   const { t, i18n } = useTranslation(['submissions']);
-  const [submissions, setSubmissions] = useState([]);
+  const [submissions, setSubmissions] = useState(null);
   const history = useHistory();
   const theme = useTheme();
   const apiCall = useMyAPI();
@@ -36,7 +37,7 @@ export default function Submissions() {
     apiCall({
       method: 'POST',
       url: '/api/v4/search/submission/',
-      body: { query: 'params.submitter:*cyber', rows: 200 },
+      body: { query: '*', rows: 200 },
       onSuccess: api_data => {
         setSubmissions(api_data.api_response.items);
       }
@@ -49,50 +50,56 @@ export default function Submissions() {
       <div style={{ paddingBottom: theme.spacing(8) }}>
         <Typography variant="h4">{t('title')}</Typography>
         <Typography variant="subtitle1" color="secondary">
-          {submissions.length} {t('subtitle')}
+          {submissions !== null && `${submissions.length} ${t('subtitle')}`}
         </Typography>
       </div>
 
-      <TableContainer component={Paper}>
-        <Table size="small" stickyHeader>
-          <TableHead>
-            <TableRow style={{ whiteSpace: 'nowrap' }}>
-              <TableCell>{t('header.starttime')}</TableCell>
-              <TableCell>{t('header.verdict')}</TableCell>
-              <TableCell>{t('header.description')}</TableCell>
-              <TableCell>{t('header.user')}</TableCell>
-              <TableCell>{t('header.numfiles')}</TableCell>
-              <TableCell>{t('header.classification')}</TableCell>
-              <TableCell>{t('header.status')}</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {submissions.map(submission => (
-              <TableRow key={submission.id} onClick={event => handleClick(submission)} hover>
-                <TableCell component="th" scope="row">
-                  <Moment fromNow locale={i18n.language}>
-                    {submission.times.submitted}
-                  </Moment>
-                </TableCell>
-                <TableCell>
-                  <Verdict score={submission.max_score} />
-                </TableCell>
-                <TableCell
-                  style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '200px' }}
-                >
-                  {submission.params.description}
-                </TableCell>
-                <TableCell>{submission.params.submitter}</TableCell>
-                <TableCell>{submission.file_count}</TableCell>
-                <TableCell>
-                  <Classification type="text" size="tiny" c12n={submission.classification} format="short" />
-                </TableCell>
-                <TableCell>{submission.state}</TableCell>
+      {submissions !== null ? (
+        <TableContainer component={Paper}>
+          <Table size="small" stickyHeader>
+            <TableHead>
+              <TableRow style={{ whiteSpace: 'nowrap' }}>
+                <TableCell>{t('header.starttime')}</TableCell>
+                <TableCell>{t('header.verdict')}</TableCell>
+                <TableCell>{t('header.description')}</TableCell>
+                <TableCell>{t('header.user')}</TableCell>
+                <TableCell>{t('header.numfiles')}</TableCell>
+                <TableCell>{t('header.classification')}</TableCell>
+                <TableCell>{t('header.status')}</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {submissions.map(submission => (
+                <TableRow key={submission.id} onClick={event => handleClick(submission)} hover>
+                  <TableCell style={{ whiteSpace: 'nowrap' }}>
+                    <Tooltip title={submission.times.submitted}>
+                      <Moment fromNow locale={i18n.language}>
+                        {submission.times.submitted}
+                      </Moment>
+                    </Tooltip>
+                  </TableCell>
+                  <TableCell>
+                    <Verdict score={submission.max_score} />
+                  </TableCell>
+                  <TableCell style={{ wordBreak: 'break-all' }}>{submission.params.description}</TableCell>
+                  <TableCell style={{ whiteSpace: 'nowrap' }}>{submission.params.submitter}</TableCell>
+                  <TableCell>{submission.file_count}</TableCell>
+                  <TableCell>
+                    <Classification type="text" size="tiny" c12n={submission.classification} format="short" />
+                  </TableCell>
+                  <TableCell style={{ textAlign: 'center' }}>
+                    <SubmissionState state={submission.state} error_count={submission.error_count} />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <div style={{ width: '100%', textAlign: 'center' }}>
+          <CircularProgress />
+        </div>
+      )}
     </PageFullWidth>
   );
 }
