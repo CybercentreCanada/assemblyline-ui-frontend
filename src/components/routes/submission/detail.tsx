b8b9ceb653f11c8028791c2a7bc47c3aed1cf809
Steve Garon <steve.garon@cyber.gc.ca>
2020-10-15 20:22:43 +0000

    Working on detail page

--- src/components/routes/submission/detail.tsx
+++ src/components/routes/submission/detail.tsx
@@ -1,12 +1,17 @@
-import { Grid, IconButton, Typography, useTheme } from '@material-ui/core';
+import { Grid, IconButton, Link as MaterialLink, Tooltip, Typography, useTheme } from '@material-ui/core';
 import ChromeReaderModeOutlinedIcon from '@material-ui/icons/ChromeReaderModeOutlined';
+import CloudDownloadOutlinedIcon from '@material-ui/icons/CloudDownloadOutlined';
+import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
+import ReplayOutlinedIcon from '@material-ui/icons/ReplayOutlined';
 import { Skeleton } from '@material-ui/lab';
 import PageCenter from 'commons/components/layout/pages/PageCenter';
 import useMyAPI from 'components/hooks/useMyAPI';
+import useMySnackbar from 'components/hooks/useMySnackbar';
 import Classification from 'components/visual/Classification';
+import ConfirmationDialog from 'components/visual/ConfirmationDialog';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 
 type ParamProps = {
   id: string;
@@ -17,9 +22,43 @@ export default function SubmissionDetail() {
   const { id } = useParams<ParamProps>();
   const theme = useTheme();
   const [submission, setSubmission] = useState(null);
+  const [deleteDialog, setDeleteDialog] = useState(false);
   const apiCall = useMyAPI();
   const sp2 = theme.spacing(2);
   const sp4 = theme.spacing(4);
+  const { showSuccessMessage } = useMySnackbar();
+  const history = useHistory();
+
+  const resubmit = () => {
+    if (submission != null) {
+      apiCall({
+        url: `/api/v4/submit/resubmit/${submission.sid}/`,
+        onSuccess: api_data => {
+          showSuccessMessage(t('submit.success'));
+          setSubmission(null);
+          setTimeout(() => {
+            history.push(`/submission/detail/${api_data.api_response.sid}`);
+          }, 500);
+        }
+      });
+    }
+  };
+
+  const deleteSubmission = () => {
+    if (submission != null) {
+      apiCall({
+        method: 'DELETE',
+        url: `/api/v4/submission/${submission.sid}/`,
+        onSuccess: api_data => {
+          showSuccessMessage(t('delete.success'));
+          setDeleteDialog(false);
+          setTimeout(() => {
+            history.push('/submissions');
+          }, 500);
+        }
+      });
+    }
+  };
 
   useEffect(() => {
     apiCall({
@@ -29,17 +68,27 @@ export default function SubmissionDetail() {
       }
     });
     // eslint-disable-next-line
-  }, []);
+  }, [id]);
 
   return (
     <PageCenter>
+      <ConfirmationDialog
+        open={deleteDialog}
+        handleClose={() => setDeleteDialog(false)}
+        handleAccept={deleteSubmission}
+        title={t('delete.title')}
+        cancelText={t('delete.cancelText')}
+        acceptText={t('delete.acceptText')}
+        text={t('delete.text')}
+      />
+
       <div style={{ textAlign: 'left' }}>
         <div style={{ paddingBottom: sp4, paddingTop: sp2 }}>
           <Classification size="tiny" c12n={submission ? submission.classification : null} />
         </div>
         <div style={{ paddingBottom: sp4 }}>
-          <Grid container>
-            <Grid item xs={12} sm>
+          <Grid container alignItems="center">
+            <Grid item xs>
               <div>
                 <Typography variant="h4">{t('title')}</Typography>
                 <Typography variant="caption">
@@ -49,11 +98,26 @@ export default function SubmissionDetail() {
             </Grid>
             <Grid item xs={12} sm>
               <div style={{ textAlign: 'right' }}>
-                <Link to={`/submission/report/${submission ? submission.sid : id}`}>
-                  <IconButton>
+                <Tooltip title={t('delete')}>
+                  <IconButton onClick={() => setDeleteDialog(true)}>
+                    <DeleteForeverOutlinedIcon color="error" />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title={t('download')}>
+                  <IconButton component={MaterialLink} href={`/api/v4/bundle/${submission ? submission.sid : id}/`}>
+                    <CloudDownloadOutlinedIcon />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title={t('resubmit')}>
+                  <IconButton onClick={resubmit}>
+                    <ReplayOutlinedIcon />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title={t('report_view')}>
+                  <IconButton component={Link} to={`/submission/report/${submission ? submission.sid : id}`}>
                     <ChromeReaderModeOutlinedIcon />
                   </IconButton>
-                </Link>
+                </Tooltip>
               </div>
             </Grid>
           </Grid>
