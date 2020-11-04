99fc1c2b3225dd6c63d06270b36a535cb2b7a564
Steve Garon <steve.garon@cyber.gc.ca>
2020-11-04 14:41:16 +0000

    Simplify PageFullScreen

--- src/commons/components/layout/pages/PageFullScreen.tsx
+++ src/commons/components/layout/pages/PageFullScreen.tsx
@@ -1,59 +1,37 @@
-import { IconButton, makeStyles, useScrollTrigger } from '@material-ui/core';
+import { IconButton, makeStyles, useTheme } from '@material-ui/core';
 import FullscreenIcon from '@material-ui/icons/Fullscreen';
 import FullscreenExitIcon from '@material-ui/icons/FullscreenExit';
+import useAppBarHeight from 'commons/components/hooks/useAppBarHeight';
 import useAppLayout from 'commons/components/hooks/useAppLayout';
 import useFullscreenStatus from 'commons/components/hooks/useFullscreenStatus';
 import React from 'react';
 
-const useStyles = (isFullscreen, topbarShown) => {
-  return makeStyles(theme => ({
-    root: {
-      backgroundColor: isFullscreen ? theme.palette.background.default : theme.palette.background.default,
-      overflow: 'auto'
-    },
-    page: {
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      marginBottom: isFullscreen ? theme.spacing(2) : 'auto',
-      marginTop: isFullscreen ? theme.spacing(3) : theme.spacing(6),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        paddingLeft: isFullscreen ? theme.spacing(2) : 0,
-        paddingRight: isFullscreen ? theme.spacing(2) : 0
-      }
-    },
-    toggle: {
-      position: 'fixed',
-      right: isFullscreen ? '30px' : '20px',
-      top: isFullscreen || !topbarShown ? '20px' : '84px',
-      zIndex: theme.zIndex.appBar + 1,
-      transition: theme.transitions.create('top', {
-        easing: theme.transitions.easing.easeInOut,
-        duration: '0.15s'
-      }),
-      [theme.breakpoints.down('xs')]: {
-        right: '16px',
-        top: isFullscreen || !topbarShown ? '8px' : '60px'
-      }
-    }
-  }))();
-};
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: theme.palette.background.default
+  },
+  toggle: {
+    float: 'right',
+    paddingTop: theme.spacing(2),
+    position: 'sticky',
+    right: theme.spacing(2),
+    zIndex: theme.zIndex.appBar + 1
+  }
+}));
 
-function MoveOnScroll({ children, enabled, isFullscreen }) {
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0
-  });
-
-  const classes = useStyles(isFullscreen, !trigger || !enabled);
-  return <div className={classes.toggle}>{children}</div>;
+interface PageFullscreenProps {
+  children: React.ReactNode;
+  margin?: number;
 }
-
-export default function PageFullscreen({ children }) {
+const PageFullscreen: React.FC<PageFullscreenProps> = ({ children, margin = 2 }) => {
   const maximizableElement = React.useRef(null);
+  const appBarHeight = useAppBarHeight();
+  const { currentLayout, autoHideAppbar } = useAppLayout();
   let isFullscreen;
   let setIsFullscreen;
   let fullscreenSupported;
+
+  const barWillHide = currentLayout !== 'top' && autoHideAppbar;
 
   try {
     [isFullscreen, setIsFullscreen] = useFullscreenStatus(maximizableElement);
@@ -63,9 +41,8 @@ export default function PageFullscreen({ children }) {
     setIsFullscreen = undefined;
   }
 
-  const classes = useStyles(isFullscreen, true);
-  const { autoHideAppbar, currentLayout } = useAppLayout();
-  const isTopLayout = currentLayout === 'top';
+  const classes = useStyles();
+  const theme = useTheme();
 
   const handleEnterFullscreen = () => {
     setIsFullscreen();
@@ -77,20 +54,20 @@ export default function PageFullscreen({ children }) {
 
   return (
     <div ref={maximizableElement} className={classes.root}>
-      <div className={classes.page}>
-        <MoveOnScroll enabled={!isTopLayout && autoHideAppbar} isFullscreen={isFullscreen}>
-          {fullscreenSupported ? null : isFullscreen ? (
-            <IconButton onClick={handleExitFullscreen}>
-              <FullscreenExitIcon />
-            </IconButton>
-          ) : (
-            <IconButton onClick={handleEnterFullscreen}>
-              <FullscreenIcon />
-            </IconButton>
-          )}
-        </MoveOnScroll>
-        {children}
+      <div className={classes.toggle} style={{ top: barWillHide ? 0 : appBarHeight }}>
+        {fullscreenSupported ? null : isFullscreen ? (
+          <IconButton onClick={handleExitFullscreen}>
+            <FullscreenExitIcon />
+          </IconButton>
+        ) : (
+          <IconButton onClick={handleEnterFullscreen}>
+            <FullscreenIcon />
+          </IconButton>
+        )}
       </div>
+      <div style={{ margin: theme.spacing(margin) }}>{children}</div>
     </div>
   );
-}
+};
+
+export default PageFullscreen;
