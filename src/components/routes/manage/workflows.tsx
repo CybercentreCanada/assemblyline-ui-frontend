db6956a3e6ae4be174c170df280902917f502c5b
Steve Garon <steve.garon@cyber.gc.ca>
2020-11-16 18:14:49 +0000

    Added workflow list/search

--- src/components/routes/manage/workflows.tsx
+++ src/components/routes/manage/workflows.tsx
@@ -1,13 +1,150 @@
-import PageCenter from 'commons/components/layout/pages/PageCenter';
-import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import PageFullWidth from 'commons/components/layout/pages/PageFullWidth';
+import PageHeader from 'commons/components/layout/pages/PageHeader';
+import SearchBar from 'components/elements/search/search-bar';
+import SimpleSearchQuery from 'components/elements/search/simple-search-query';
+import useAppContext from 'components/hooks/useAppContext';
+import useMyAPI from 'components/hooks/useMyAPI';
+import SearchPager from 'components/visual/SearchPager';
+import WorkflowTable from 'components/visual/SearchResult/workflow';
+import 'moment/locale/fr';
+import React, { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useHistory, useLocation } from 'react-router-dom';
+
+const PAGE_SIZE = 25;
+const DEFAULT_SUGGESTION = ['OR', 'AND', 'NOT', 'TO', 'now', 'd', 'M', 'y', 'h', 'm'];
+
+const useStyles = makeStyles(theme => ({
+  searchresult: {
+    fontStyle: 'italic',
+    paddingTop: theme.spacing(0.5),
+    display: 'flex',
+    flexWrap: 'wrap'
+  }
+}));
+
+type SearchResults = {
+  items: any[];
+  offset: number;
+  rows: number;
+  total: number;
+};
 
 export default function Workflows() {
   const { t } = useTranslation(['manageWorkflows']);
+  const [pageSize] = useState(PAGE_SIZE);
+  const [searching, setSearching] = useState(false);
+  const { indexes } = useAppContext();
+  const [workflowResults, setWorkflowResults] = useState<SearchResults>(null);
+  const location = useLocation();
+  const [query, setQuery] = useState<SimpleSearchQuery>(null);
+  const history = useHistory();
+  const theme = useTheme();
+  const apiCall = useMyAPI();
+  const classes = useStyles();
+  const [suggestions] = useState([
+    ...Object.keys(indexes.workflow).filter(name => {
+      return indexes.workflow[name].indexed;
+    }),
+    ...DEFAULT_SUGGESTION
+  ]);
+  const filterValue = useRef<string>('');
+
+  useEffect(() => {
+    setQuery(new SimpleSearchQuery(location.search, `query=*&rows=${pageSize}&offset=0`));
+  }, [location.pathname, location.search, pageSize]);
+
+  useEffect(() => {
+    if (query) {
+      query.set('rows', PAGE_SIZE);
+      query.set('offset', 0);
+      setSearching(true);
+      apiCall({
+        method: 'POST',
+        url: '/api/v4/search/workflow/',
+        body: query.getParams(),
+        onSuccess: api_data => {
+          setWorkflowResults(api_data.api_response);
+        },
+        onFinalize: () => {
+          setSearching(false);
+        }
+      });
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  const onClear = () => {
+    history.push(location.pathname);
+  };
+
+  const onSearch = () => {
+    if (filterValue.current !== '') {
+      query.set('query', filterValue.current);
+      history.push(`${location.pathname}?${query.toString()}`);
+    } else {
+      onClear();
+    }
+  };
+
+  const onFilterValueChange = (inputValue: string) => {
+    filterValue.current = inputValue;
+  };
 
   return (
-    <PageCenter margin={4}>
-      <div style={{ textAlign: 'left' }}>{t('Workflows')}</div>
-    </PageCenter>
+    <PageFullWidth margin={4}>
+      <div style={{ paddingBottom: theme.spacing(2) }}>
+        <Typography variant="h4">{t('title')}</Typography>
+      </div>
+
+      <PageHeader isSticky>
+        <div style={{ paddingTop: theme.spacing(1) }}>
+          <SearchBar
+            initValue={query ? query.get('query', '') : ''}
+            placeholder={t('filter')}
+            searching={searching}
+            suggestions={suggestions}
+            onValueChange={onFilterValueChange}
+            onClear={onClear}
+            onSearch={onSearch}
+          >
+            {workflowResults !== null && (
+              <div className={classes.searchresult}>
+                {workflowResults.total !== 0 && (
+                  <Typography variant="subtitle1" color="secondary" style={{ flexGrow: 1 }}>
+                    {searching ? (
+                      <span>{t('searching')}</span>
+                    ) : (
+                      <span>
+                        {workflowResults.total}&nbsp;
+                        {query.get('query')
+                          ? t(`filtered${workflowResults.total === 1 ? '' : 's'}`)
+                          : t(`total${workflowResults.total === 1 ? '' : 's'}`)}
+                      </span>
+                    )}
+                  </Typography>
+                )}
+
+                <SearchPager
+                  total={workflowResults.total}
+                  setResults={setWorkflowResults}
+                  pageSize={pageSize}
+                  index="workflow"
+                  query={query}
+                  setSearching={setSearching}
+                />
+              </div>
+            )}
+          </SearchBar>
+        </div>
+      </PageHeader>
+
+      <div style={{ paddingTop: theme.spacing(2), paddingLeft: theme.spacing(0.5), paddingRight: theme.spacing(0.5) }}>
+        <WorkflowTable workflowResults={workflowResults} />
+      </div>
+    </PageFullWidth>
   );
 }
---
+++ src/components/visual/SearchResult/workflow.tsx
@@ -0,0 +1,105 @@
+import Paper from '@material-ui/core/Paper';
+import TableContainer from '@material-ui/core/TableContainer';
+import { AlertTitle, Skeleton } from '@material-ui/lab';
+import Classification from 'components/visual/Classification';
+import 'moment/locale/fr';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Moment from 'react-moment';
+import { Link } from 'react-router-dom';
+import { DivTable, DivTableBody, DivTableCell, DivTableHead, DivTableRow, LinkRow } from '../DivTable';
+import InformativeAlert from '../InformativeAlert';
+
+export type WorkflowResult = {
+  classification: string;
+  creation_date: string;
+  creator: string;
+  edited_by: string;
+  hit_count: string;
+  id: string;
+  last_edit: string;
+  last_seen: string;
+  name: string;
+  priority: string;
+  query: string;
+  status: string;
+  workflow_id: string;
+};
+
+type SearchResults = {
+  items: WorkflowResult[];
+  rows: number;
+  offset: number;
+  total: number;
+};
+
+type WorkflowTableProps = {
+  workflowResults: SearchResults;
+};
+
+const WrappedWorflowTable: React.FC<WorkflowTableProps> = ({ workflowResults }) => {
+  const { t, i18n } = useTranslation(['search']);
+
+  return workflowResults ? (
+    workflowResults.total !== 0 ? (
+      <TableContainer component={Paper}>
+        <DivTable>
+          <DivTableHead>
+            <DivTableRow>
+              <DivTableCell>{t('header.created')}</DivTableCell>
+              <DivTableCell>{t('header.lasttimeseen')}</DivTableCell>
+              <DivTableCell>{t('header.name')}</DivTableCell>
+              <DivTableCell>{t('header.priority')}</DivTableCell>
+              <DivTableCell>{t('header.status')}</DivTableCell>
+              <DivTableCell>{t('header.hit_count')}</DivTableCell>
+              <DivTableCell>{t('header.classification')}</DivTableCell>
+            </DivTableRow>
+          </DivTableHead>
+          <DivTableBody>
+            {workflowResults.items.map(workflow => (
+              <LinkRow
+                key={workflow.workflow_id}
+                component={Link}
+                to={`/manage/workflow/${workflow.workflow_id}`}
+                hover
+                style={{ textDecoration: 'none' }}
+              >
+                <DivTableCell>
+                  <Moment fromNow locale={i18n.language}>
+                    {workflow.creation_date}
+                  </Moment>
+                </DivTableCell>
+                <DivTableCell>
+                  {workflow.last_seen && (
+                    <Moment fromNow locale={i18n.language}>
+                      {workflow.last_seen}
+                    </Moment>
+                  )}
+                </DivTableCell>
+                <DivTableCell>{workflow.name}</DivTableCell>
+                <DivTableCell>{workflow.priority}</DivTableCell>
+                <DivTableCell>{workflow.status}</DivTableCell>
+                <DivTableCell>{workflow.hit_count}</DivTableCell>
+                <DivTableCell>
+                  <Classification type="text" size="tiny" c12n={workflow.classification} format="short" />
+                </DivTableCell>
+              </LinkRow>
+            ))}
+          </DivTableBody>
+        </DivTable>
+      </TableContainer>
+    ) : (
+      <div style={{ width: '100%' }}>
+        <InformativeAlert>
+          <AlertTitle>{t('no_workflows_title')}</AlertTitle>
+          {t('no_results_desc')}
+        </InformativeAlert>
+      </div>
+    )
+  ) : (
+    <Skeleton variant="rect" style={{ height: '6rem', borderRadius: '4px' }} />
+  );
+};
+
+const WorflowTable = React.memo(WrappedWorflowTable);
+export default WorflowTable;
