519fdefc6e10e0d890371c012b422ad6a8bd42ed
Steve Garon <steve.garon@cyber.gc.ca>
2020-11-17 20:53:10 +0000

    Started working on the dashboard

--- src/components/routes/dashboard.tsx
+++ src/components/routes/dashboard.tsx
@@ -24,9 +24,86 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function Dashboard() {
+const IngestCard = () => {
   const { t } = useTranslation(['dashboard']);
   const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <Typography variant="h4" className={classes.watermark}>
+        {t('ingest')}
+      </Typography>
+    </Card>
+  );
+};
+
+const DispatcherCard = () => {
+  const { t } = useTranslation(['dashboard']);
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <Typography variant="h4" className={classes.watermark}>
+        {t('dispatcher')}
+      </Typography>
+    </Card>
+  );
+};
+
+const ExpiryCard = () => {
+  const { t } = useTranslation(['dashboard']);
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <Typography variant="h4" className={classes.watermark}>
+        {t('expiry')}
+      </Typography>
+    </Card>
+  );
+};
+
+const AlerterCard = () => {
+  const { t } = useTranslation(['dashboard']);
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <Typography variant="h4" className={classes.watermark}>
+        {t('alerter')}
+      </Typography>
+    </Card>
+  );
+};
+
+const ScalerResourcesCard = () => {
+  const { t } = useTranslation(['dashboard']);
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <Typography variant="h4" className={classes.watermark}>
+        {t('resources')}
+      </Typography>
+    </Card>
+  );
+};
+
+const ServiceCard = ({ service = null }) => {
+  const { t } = useTranslation(['dashboard']);
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <Typography variant="h4" className={classes.watermark}>
+        {t(service)}
+      </Typography>
+    </Card>
+  );
+};
+
+const Dashboard = () => {
+  const { t } = useTranslation(['dashboard']);
 
   const cards = [];
   for (let x = 0; x < 12; x++) {
@@ -39,16 +116,17 @@ function Dashboard() {
         {t('title')}
       </Typography>
       <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', width: '100%' }}>
+        <IngestCard />
+        <DispatcherCard />
+        <ExpiryCard />
+        <AlerterCard />
+        <ScalerResourcesCard />
         {cards.map((a, i) => (
-          <Card key={i} className={classes.card}>
-            <Typography variant="h4" className={classes.watermark}>
-              # {a.id}
-            </Typography>
-          </Card>
+          <ServiceCard key={i} service={`service_${a.id}`} />
         ))}
       </div>
     </PageFullscreen>
   );
-}
+};
 
 export default Dashboard;
