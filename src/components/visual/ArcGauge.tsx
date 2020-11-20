9f2478813c0a8255ead54b738b1964e87ff7a66d
Steve Garon <steve.garon@cyber.gc.ca>
2020-11-20 16:57:49 +0000

    Added ArcGauge component

---
+++ src/components/visual/ArcGauge.tsx
@@ -0,0 +1,96 @@
+import { makeStyles, useTheme } from '@material-ui/core';
+import { scaleLinear } from 'd3-scale';
+import { arc } from 'd3-shape';
+import React from 'react';
+
+const useStyles = makeStyles(theme => ({
+  caption: {
+    color: theme.palette.text.secondary,
+    fontSize: 'x-small',
+    fontWeight: 500,
+    textAlign: 'center',
+    width: '100%'
+  },
+  graph: {
+    gridColumn: '1 / 1',
+    gridRow: '1 / 1'
+  },
+  outer: {
+    display: 'grid',
+    gridTemplate: '1fr / 1fr',
+    placeItems: 'end'
+  },
+  text: {
+    color: theme.palette.text.secondary,
+    fontWeight: 900,
+    gridColumn: '1 / 1',
+    gridRow: '1 / 1',
+    textAlign: 'center',
+    width: '100%'
+  },
+  title: {
+    color: theme.palette.text.secondary,
+    fontWeight: 500,
+    textAlign: 'center',
+    width: '100%'
+  }
+}));
+
+type ArcGaugeProps = {
+  pctValue: number;
+  caption?: string;
+  colorBack?: string;
+  fontSize?: string;
+  title?: string;
+  width?: string;
+};
+
+const ArcGauge: React.FC<ArcGaugeProps> = ({
+  pctValue,
+  caption = '',
+  colorBack = '#68686815',
+  fontSize = '22px',
+  title = '',
+  width = '100px'
+}) => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const colorFilled = theme.palette.type === 'dark' ? theme.palette.primary.light : theme.palette.primary.dark;
+
+  const backgroundArc = arc()
+    .innerRadius(0.7)
+    .outerRadius(0.95)
+    .startAngle(-Math.PI / 2)
+    .endAngle(Math.PI / 2)
+    .cornerRadius(0)();
+  const percentScale = scaleLinear().domain([0, 100]).range([0, 1]);
+
+  const angleScale = scaleLinear()
+    .domain([0, 1])
+    .range([-Math.PI / 2, Math.PI / 2])
+    .clamp(true);
+  const filledArc = arc()
+    .innerRadius(0.7)
+    .outerRadius(0.95)
+    .startAngle(-Math.PI / 2)
+    .endAngle(angleScale(percentScale(pctValue)))
+    .cornerRadius(0)();
+
+  return (
+    <div>
+      {title && <div className={classes.title}>{title}</div>}
+      <div className={classes.outer} style={{ fontSize, lineHeight: fontSize, width }}>
+        <span className={classes.text}>{`${pctValue}%`}</span>
+        <svg className={classes.graph} viewBox={[-1, -1, 2, 1].join(' ')}>
+          <g>
+            <path d={backgroundArc} fill={colorBack} />
+            <path d={filledArc} fill={colorFilled} />
+          </g>
+        </svg>
+      </div>
+      {caption && <div className={classes.caption}>{caption}</div>}
+    </div>
+  );
+};
+
+export default ArcGauge;
