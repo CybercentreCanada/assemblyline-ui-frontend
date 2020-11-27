5b905711d898cdae376c28ab9cb4fb8e2a85a2c2
Steve Garon <steve.garon@cyber.gc.ca>
2020-11-27 20:50:42 +0000

    Added error viewer

--- src/components/routes/admin/error_viewer.tsx
+++ src/components/routes/admin/error_viewer.tsx
@@ -1,18 +1,332 @@
+import {
+  Card,
+  Drawer,
+  Grid,
+  IconButton,
+  makeStyles,
+  Tooltip,
+  Typography,
+  useMediaQuery,
+  useTheme
+} from '@material-ui/core';
+import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
+import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
+import PanToolOutlinedIcon from '@material-ui/icons/PanToolOutlined';
+import ReportProblemOutlinedIcon from '@material-ui/icons/ReportProblemOutlined';
 import useUser from 'commons/components/hooks/useAppUser';
-import PageCenter from 'commons/components/layout/pages/PageCenter';
+import useClipboard from 'commons/components/hooks/useClipboard';
+import PageFullWidth from 'commons/components/layout/pages/PageFullWidth';
+import PageHeader from 'commons/components/layout/pages/PageHeader';
+import SearchBar from 'components/elements/search/search-bar';
+import SimpleSearchQuery from 'components/elements/search/simple-search-query';
+import useMyAPI from 'components/hooks/useMyAPI';
 import { CustomUser } from 'components/hooks/useMyUser';
-import React from 'react';
+import SearchPager from 'components/visual/SearchPager';
+import ErrorsTable from 'components/visual/SearchResult/errors';
+import 'moment/locale/fr';
+import React, { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Redirect } from 'react-router-dom';
+import { BsClipboard } from 'react-icons/bs';
+import Moment from 'react-moment';
+import { Redirect, useHistory, useLocation } from 'react-router-dom';
+
+const PAGE_SIZE = 25;
+const DEFAULT_SUGGESTION = ['OR', 'AND', 'NOT', 'TO', 'now', 'd', 'M', 'y', 'h', 'm'];
+
+const useStyles = makeStyles(theme => ({
+  searchresult: {
+    fontStyle: 'italic',
+    paddingTop: theme.spacing(0.5),
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  drawerPaper: {
+    width: '80%',
+    maxWidth: '800px',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%'
+    }
+  },
+  clipboardIcon: {
+    marginLeft: theme.spacing(1),
+    '&:hover': {
+      cursor: 'pointer',
+      transform: 'scale(1.1)'
+    }
+  }
+}));
 
 export default function ErrorViewer() {
-  const { t } = useTranslation(['adminErrorViewer']);
+  const { t, i18n } = useTranslation(['adminErrorViewer']);
+  const [pageSize] = useState(PAGE_SIZE);
+  const [searching, setSearching] = useState(false);
+  const [errorResults, setErrorResults] = useState(null);
+  const classes = useStyles();
+  const history = useHistory();
+  const [query, setQuery] = useState<SimpleSearchQuery>(null);
+  const theme = useTheme();
+  const apiCall = useMyAPI();
+  const [error, setError] = useState(null);
   const { user: currentUser } = useUser<CustomUser>();
+  const [suggestions, setSuggestions] = useState(DEFAULT_SUGGESTION);
+  const location = useLocation();
+  const upMD = useMediaQuery(theme.breakpoints.up('md'));
+  const filterValue = useRef<string>('');
+  const { copy } = useClipboard();
+
+  const closeDrawer = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    setQuery(new SimpleSearchQuery(location.search, `rows=${pageSize}&offset=0`));
+  }, [location.search, pageSize]);
+
+  useEffect(() => {
+    if (query && currentUser.is_admin) {
+      query.set('rows', pageSize);
+      query.set('offset', 0);
+      setSearching(true);
+      apiCall({
+        url: `/api/v4/error/list/?${query.toString()}`,
+        onSuccess: api_data => {
+          setErrorResults(api_data.api_response);
+        },
+        onFinalize: () => {
+          setSearching(false);
+        }
+      });
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  useEffect(() => {
+    apiCall({
+      url: '/api/v4/search/fields/error/',
+      onSuccess: api_data => {
+        setSuggestions([
+          ...Object.keys(api_data.api_response).filter(name => {
+            return api_data.api_response[name].indexed;
+          }),
+          ...DEFAULT_SUGGESTION
+        ]);
+      }
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onClear = () => {
+    history.push(location.pathname);
+  };
+
+  const onSearch = () => {
+    if (filterValue.current !== '') {
+      query.set('query', filterValue.current);
+      history.push(`${location.pathname}?${query.toString()}`);
+    } else {
+      onClear();
+    }
+  };
+
+  const onFilterValueChange = (inputValue: string) => {
+    filterValue.current = inputValue;
+  };
+
+  const errorMap = {
+    'MAX DEPTH REACHED': <PanToolOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    'MAX RETRY REACHED': <PanToolOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    EXCEPTION: <ReportProblemOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    'TASK PRE-EMPTED': <CancelOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    'SERVICE DOWN': <CancelOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    'SERVICE BUSY': <CancelOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    'MAX FILES REACHED': <PanToolOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    UNKNOWN: <ReportProblemOutlinedIcon style={{ color: theme.palette.action.active }} />
+  };
 
   return currentUser.is_admin ? (
-    <PageCenter margin={4} width="100%">
-      <div>{t('Error Viewer')}</div>
-    </PageCenter>
+    <PageFullWidth margin={4}>
+      <Drawer anchor="right" classes={{ paper: classes.drawerPaper }} open={error !== null} onClose={closeDrawer}>
+        <div id="drawerTop" style={{ padding: theme.spacing(1) }}>
+          <IconButton onClick={closeDrawer}>
+            <CloseOutlinedIcon />
+          </IconButton>
+        </div>
+        {error && (
+          <div style={{ paddingLeft: theme.spacing(2), paddingRight: theme.spacing(2) }}>
+            <Grid container spacing={1} style={{ paddingBottom: theme.spacing(1) }}>
+              <Grid item xs={6} sm={8}>
+                <Typography variant="h5">{error.response.service_name}</Typography>
+                <Typography variant="caption">
+                  {`${error.response.service_version}${
+                    error.response.service_tool_version && ` (${error.response.service_tool_version})`
+                  }`}
+                </Typography>
+              </Grid>
+              <Grid item xs={6} sm={4}>
+                <div style={{ display: 'inline-block', textAlign: 'start' }}>
+                  <Typography component="div" variant="body1">
+                    <Moment fromNow locale={i18n.language}>
+                      {error.created}
+                    </Moment>
+                  </Typography>
+                  <Typography component="div" variant="caption">
+                    <Moment format="YYYY-MM-DD HH:mm:ss">{error.created}</Moment>
+                  </Typography>
+                </div>
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={1} style={{ paddingBottom: theme.spacing(1) }}>
+              <Grid item xs={6} sm={8}>
+                <span style={{ verticalAlign: 'middle' }}>{errorMap[error.type]}&nbsp;</span>
+                <span style={{ verticalAlign: 'middle' }}>{error.type}</span>
+              </Grid>
+              <Grid item xs={6} sm={4} style={{ alignSelf: 'center' }}>
+                <span style={{ verticalAlign: 'middle' }}>{error.response.status}</span>
+              </Grid>
+            </Grid>
+
+            <div style={{ marginBottom: theme.spacing(1) }}>
+              <label>{t('message')}</label>
+              <Card variant="outlined">
+                <pre
+                  style={{
+                    paddingLeft: theme.spacing(1),
+                    paddingRight: theme.spacing(1),
+                    whiteSpace: 'pre-wrap',
+                    minHeight: '10rem'
+                  }}
+                >
+                  {error.response.message}
+                </pre>
+              </Card>
+            </div>
+
+            {error.response.service_debug_info && (
+              <div style={{ marginBottom: theme.spacing(1) }}>
+                <label>{t('debug_info')}</label>
+                <Card variant="outlined">
+                  <pre
+                    style={{
+                      paddingLeft: theme.spacing(1),
+                      paddingRight: theme.spacing(1),
+                      whiteSpace: 'pre-wrap'
+                    }}
+                  >
+                    {error.response.service_debug_info}
+                  </pre>
+                </Card>
+              </div>
+            )}
+
+            <div style={{ marginBottom: theme.spacing(1) }}>
+              <label>{t('file_info')}</label>
+              <div style={{ wordBreak: 'break-all' }}>
+                {error.sha256}
+                <BsClipboard className={classes.clipboardIcon} onClick={() => copy(error.sha256, 'drawerTop')} />
+              </div>
+            </div>
+
+            {/* <textarea style={{ width: '100%', height: '100%' }}>{JSON.stringify(error)}</textarea> */}
+          </div>
+        )}
+      </Drawer>
+
+      <div style={{ paddingBottom: theme.spacing(2) }}>
+        <Typography variant="h4">{t('title')}</Typography>
+      </div>
+
+      <PageHeader isSticky>
+        <div style={{ paddingTop: theme.spacing(1) }}>
+          <SearchBar
+            initValue={query ? query.get('query', '') : ''}
+            placeholder={t('filter')}
+            searching={searching}
+            suggestions={suggestions}
+            onValueChange={onFilterValueChange}
+            onClear={onClear}
+            onSearch={onSearch}
+            buttons={[
+              {
+                icon: (
+                  <Tooltip title={t('exception')}>
+                    <ReportProblemOutlinedIcon fontSize={upMD ? 'default' : 'small'} />
+                  </Tooltip>
+                ),
+                props: {
+                  onClick: () => {
+                    query.set('query', 'type:EXCEPTION OR type:UNKNOWN');
+                    history.push(`${location.pathname}?${query.toString()}`);
+                  }
+                }
+              },
+              {
+                icon: (
+                  <Tooltip title={t('canceled')}>
+                    <CancelOutlinedIcon fontSize={upMD ? 'default' : 'small'} />
+                  </Tooltip>
+                ),
+                props: {
+                  onClick: () => {
+                    query.set('query', 'type:SERVICE* OR type:TASK*');
+                    history.push(`${location.pathname}?${query.toString()}`);
+                  }
+                }
+              },
+              {
+                icon: (
+                  <Tooltip title={t('maxed')}>
+                    <PanToolOutlinedIcon fontSize={upMD ? 'default' : 'small'} />
+                  </Tooltip>
+                ),
+                props: {
+                  onClick: () => {
+                    query.set('query', 'type:MAX*');
+                    history.push(`${location.pathname}?${query.toString()}`);
+                  }
+                }
+              }
+            ]}
+          >
+            {errorResults !== null && (
+              <div className={classes.searchresult}>
+                {errorResults.total !== 0 && (
+                  <Typography variant="subtitle1" color="secondary" style={{ flexGrow: 1 }}>
+                    {searching ? (
+                      <span>{t('searching')}</span>
+                    ) : (
+                      <span>
+                        {errorResults.total}&nbsp;
+                        {query.get('query')
+                          ? t(`filtered${errorResults.total === 1 ? '' : 's'}`)
+                          : t(`total${errorResults.total === 1 ? '' : 's'}`)}
+                      </span>
+                    )}
+                  </Typography>
+                )}
+
+                <SearchPager
+                  method="GET"
+                  url="/api/v4/error/list/"
+                  total={errorResults.total}
+                  setResults={setErrorResults}
+                  pageSize={pageSize}
+                  index="user"
+                  query={query}
+                  setSearching={setSearching}
+                />
+              </div>
+            )}
+          </SearchBar>
+        </div>
+      </PageHeader>
+
+      <div style={{ paddingTop: theme.spacing(2), paddingLeft: theme.spacing(0.5), paddingRight: theme.spacing(0.5) }}>
+        <ErrorsTable errorResults={errorResults} onClick={setError} />
+      </div>
+    </PageFullWidth>
   ) : (
     <Redirect to="/forbidden" />
   );
---
+++ src/components/visual/SearchResult/errors.tsx
@@ -0,0 +1,105 @@
+import { Tooltip, useTheme } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import TableContainer from '@material-ui/core/TableContainer';
+import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
+import PanToolOutlinedIcon from '@material-ui/icons/PanToolOutlined';
+import ReportProblemOutlinedIcon from '@material-ui/icons/ReportProblemOutlined';
+import { AlertTitle, Skeleton } from '@material-ui/lab';
+import { DivTable, DivTableBody, DivTableCell, DivTableHead, DivTableRow } from 'components/visual/DivTable';
+import 'moment/locale/fr';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Moment from 'react-moment';
+import InformativeAlert from '../InformativeAlert';
+
+export type ErrorResult = {
+  created: string;
+  id: string;
+  response: {
+    message: string;
+    service_debug_info: string;
+    service_name: string;
+    service_tool_version: string;
+    service_version: string;
+    status: string;
+  };
+  sha256: string;
+  type: string;
+};
+
+type SearchResults = {
+  items: ErrorResult[];
+  rows: number;
+  offset: number;
+  total: number;
+};
+
+type ErrorsTableProps = {
+  errorResults: SearchResults;
+  onClick: (error: ErrorResult) => void;
+};
+
+const WrappedErrorsTable: React.FC<ErrorsTableProps> = ({ errorResults, onClick }) => {
+  const { t, i18n } = useTranslation(['adminErrorViewer']);
+  const theme = useTheme();
+
+  const errorMap = {
+    'MAX DEPTH REACHED': <PanToolOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    'MAX RETRY REACHED': <PanToolOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    EXCEPTION: <ReportProblemOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    'TASK PRE-EMPTED': <CancelOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    'SERVICE DOWN': <CancelOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    'SERVICE BUSY': <CancelOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    'MAX FILES REACHED': <PanToolOutlinedIcon style={{ color: theme.palette.action.active }} />,
+    UNKNOWN: <ReportProblemOutlinedIcon style={{ color: theme.palette.action.active }} />
+  };
+
+  return errorResults ? (
+    errorResults.total !== 0 ? (
+      <TableContainer component={Paper}>
+        <DivTable size="small">
+          <DivTableHead>
+            <DivTableRow style={{ whiteSpace: 'nowrap' }}>
+              <DivTableCell>{t('header.time')}</DivTableCell>
+              <DivTableCell>{t('header.service')}</DivTableCell>
+              <DivTableCell>{t('header.message')}</DivTableCell>
+              <DivTableCell>{t('header.type')}</DivTableCell>
+            </DivTableRow>
+          </DivTableHead>
+          <DivTableBody>
+            {errorResults.items.map(error => (
+              <DivTableRow key={error.id} hover onClick={() => onClick(error)} style={{ cursor: 'pointer' }}>
+                <DivTableCell>
+                  <Tooltip title={error.created}>
+                    <Moment fromNow locale={i18n.language}>
+                      {error.created}
+                    </Moment>
+                  </Tooltip>
+                </DivTableCell>
+                <DivTableCell>{error.response.service_name}</DivTableCell>
+                <DivTableCell>{error.response.message}</DivTableCell>
+                <DivTableCell>
+                  <Tooltip title={error.type}>
+                    <span>{errorMap[error.type]}</span>
+                  </Tooltip>
+                </DivTableCell>
+              </DivTableRow>
+            ))}
+          </DivTableBody>
+        </DivTable>
+      </TableContainer>
+    ) : (
+      <div style={{ width: '100%' }}>
+        <InformativeAlert>
+          <AlertTitle>{t('no_errors_title')}</AlertTitle>
+          {t('no_results_desc')}
+        </InformativeAlert>
+      </div>
+    )
+  ) : (
+    <Skeleton variant="rect" style={{ height: '6rem', borderRadius: '4px' }} />
+  );
+};
+
+const ErrorsTable = React.memo(WrappedErrorsTable);
+export default ErrorsTable;
