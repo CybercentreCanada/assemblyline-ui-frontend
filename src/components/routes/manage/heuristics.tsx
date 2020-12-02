77b1cf74dc4ea544e63c1cc566d71f3844e067e0
Steve Garon <steve.garon@cyber.gc.ca>
2020-12-02 14:09:22 +0000

    Use global drawer in heuristics

--- src/components/routes/manage/heuristics.tsx
+++ src/components/routes/manage/heuristics.tsx
@@ -1,16 +1,16 @@
-import { Drawer, IconButton, makeStyles, useTheme } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
-import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
 import PageFullWidth from 'commons/components/layout/pages/PageFullWidth';
 import PageHeader from 'commons/components/layout/pages/PageHeader';
 import SearchBar from 'components/elements/search/search-bar';
 import SimpleSearchQuery from 'components/elements/search/simple-search-query';
 import useAppContext from 'components/hooks/useAppContext';
+import useDrawer from 'components/hooks/useDrawer';
 import useMyAPI from 'components/hooks/useMyAPI';
 import SearchPager from 'components/visual/SearchPager';
 import HeuristicsTable from 'components/visual/SearchResult/heuristics';
 import 'moment/locale/fr';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHistory, useLocation } from 'react-router-dom';
 import HeuristicDetail from './heuristic_detail';
@@ -45,8 +45,6 @@ export default function Heuristics() {
   const { t } = useTranslation(['manageHeuristics']);
   const [pageSize] = useState(PAGE_SIZE);
   const [searching, setSearching] = useState(false);
-  const [drawer, setDrawer] = useState(false);
-  const [heurid, setHeurid] = useState(null);
   const { indexes } = useAppContext();
   const [heuristicResults, setHeuristicResults] = useState<SearchResults>(null);
   const location = useLocation();
@@ -55,6 +53,7 @@ export default function Heuristics() {
   const theme = useTheme();
   const apiCall = useMyAPI();
   const classes = useStyles();
+  const { setGlobalDrawer } = useDrawer();
   const [suggestions] = useState([
     ...Object.keys(indexes.heuristic).filter(name => {
       return indexes.heuristic[name].indexed;
@@ -62,10 +61,6 @@ export default function Heuristics() {
     ...DEFAULT_SUGGESTION
   ]);
   const filterValue = useRef<string>('');
-
-  const closeDrawer = () => {
-    setDrawer(false);
-  };
 
   useEffect(() => {
     setQuery(new SimpleSearchQuery(location.search, `query=*&rows=${pageSize}&offset=0`));
@@ -92,86 +87,103 @@ export default function Heuristics() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query]);
 
-  const onClear = () => {
-    history.push(location.pathname);
-  };
+  const onClear = useCallback(
+    () => {
+      history.push(location.pathname);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [location.pathname]
+  );
 
-  const onSearch = () => {
-    if (filterValue.current !== '') {
-      query.set('query', filterValue.current);
-      history.push(`${location.pathname}?${query.toString()}`);
-    } else {
-      onClear();
-    }
-  };
+  const onSearch = useCallback(
+    () => {
+      if (filterValue.current !== '') {
+        query.set('query', filterValue.current);
+        history.push(`${location.pathname}?${query.toString()}`);
+      } else {
+        onClear();
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [query, location.pathname, onClear]
+  );
 
   const onFilterValueChange = (inputValue: string) => {
     filterValue.current = inputValue;
   };
 
-  const setHeuristicID = (heur_id: string) => {
-    setDrawer(true);
-    setHeurid(heur_id);
-  };
+  const setHeuristicID = useCallback(
+    (heur_id: string) => {
+      setGlobalDrawer(<HeuristicDetail heur_id={heur_id} />);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
 
   return (
     <PageFullWidth margin={4}>
-      <Drawer anchor="right" classes={{ paper: classes.drawerPaper }} open={drawer} onClose={closeDrawer}>
-        <div id="drawerTop" style={{ padding: theme.spacing(1) }}>
-          <IconButton onClick={closeDrawer}>
-            <CloseOutlinedIcon />
-          </IconButton>
-        </div>
-        <div style={{ paddingLeft: theme.spacing(2), paddingRight: theme.spacing(2) }}>
-          <HeuristicDetail heur_id={heurid} />
-        </div>
-      </Drawer>
+      {useMemo(() => {
+        return (
+          <>
+            <div style={{ paddingBottom: theme.spacing(2) }}>
+              <Typography variant="h4">{t('title')}</Typography>
+            </div>
 
-      <div style={{ paddingBottom: theme.spacing(2) }}>
-        <Typography variant="h4">{t('title')}</Typography>
-      </div>
+            <PageHeader isSticky>
+              <div style={{ paddingTop: theme.spacing(1) }}>
+                <SearchBar
+                  initValue={query ? query.get('query', '') : ''}
+                  placeholder={t('filter')}
+                  searching={searching}
+                  suggestions={suggestions}
+                  onValueChange={onFilterValueChange}
+                  onClear={onClear}
+                  onSearch={onSearch}
+                >
+                  {heuristicResults !== null && (
+                    <div className={classes.searchresult}>
+                      {heuristicResults.total !== 0 && (
+                        <Typography variant="subtitle1" color="secondary" style={{ flexGrow: 1 }}>
+                          {searching ? (
+                            <span>{t('searching')}</span>
+                          ) : (
+                            <span>
+                              {heuristicResults.total}&nbsp;
+                              {query.get('query')
+                                ? t(`filtered${heuristicResults.total === 1 ? '' : 's'}`)
+                                : t(`total${heuristicResults.total === 1 ? '' : 's'}`)}
+                            </span>
+                          )}
+                        </Typography>
+                      )}
 
-      <PageHeader isSticky>
-        <div style={{ paddingTop: theme.spacing(1) }}>
-          <SearchBar
-            initValue={query ? query.get('query', '') : ''}
-            placeholder={t('filter')}
-            searching={searching}
-            suggestions={suggestions}
-            onValueChange={onFilterValueChange}
-            onClear={onClear}
-            onSearch={onSearch}
-          >
-            {heuristicResults !== null && (
-              <div className={classes.searchresult}>
-                {heuristicResults.total !== 0 && (
-                  <Typography variant="subtitle1" color="secondary" style={{ flexGrow: 1 }}>
-                    {searching ? (
-                      <span>{t('searching')}</span>
-                    ) : (
-                      <span>
-                        {heuristicResults.total}&nbsp;
-                        {query.get('query')
-                          ? t(`filtered${heuristicResults.total === 1 ? '' : 's'}`)
-                          : t(`total${heuristicResults.total === 1 ? '' : 's'}`)}
-                      </span>
-                    )}
-                  </Typography>
-                )}
-
-                <SearchPager
-                  total={heuristicResults.total}
-                  setResults={setHeuristicResults}
-                  pageSize={pageSize}
-                  index="heuristic"
-                  query={query}
-                  setSearching={setSearching}
-                />
+                      <SearchPager
+                        total={heuristicResults.total}
+                        setResults={setHeuristicResults}
+                        pageSize={pageSize}
+                        index="heuristic"
+                        query={query}
+                        setSearching={setSearching}
+                      />
+                    </div>
+                  )}
+                </SearchBar>
               </div>
-            )}
-          </SearchBar>
-        </div>
-      </PageHeader>
+            </PageHeader>
+          </>
+        );
+      }, [
+        classes.searchresult,
+        heuristicResults,
+        onClear,
+        onSearch,
+        pageSize,
+        query,
+        searching,
+        suggestions,
+        t,
+        theme
+      ])}
 
       <div style={{ paddingTop: theme.spacing(2), paddingLeft: theme.spacing(0.5), paddingRight: theme.spacing(0.5) }}>
         <HeuristicsTable heuristicResults={heuristicResults} setHeuristicID={setHeuristicID} />
