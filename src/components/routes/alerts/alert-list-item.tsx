a58ede0480da9fcf915fe2c3f1c3ae1b51af834a
Steve Garon <steve.garon@cyber.gc.ca>
2020-12-07 14:51:58 +0000

    Cleaning up list items

--- src/components/routes/alerts/alert-list-item.tsx
+++ src/components/routes/alerts/alert-list-item.tsx
@@ -1,174 +1,87 @@
-import { Grid, Typography, useMediaQuery, useTheme } from '@material-ui/core';
+import { Grid, makeStyles, useTheme } from '@material-ui/core';
 import PersonIcon from '@material-ui/icons/Person';
 import { ChipList } from 'components/elements/mui/chips';
 import { AlertItem } from 'components/routes/alerts/hooks/useAlerts';
-import Classification from 'components/visual/Classification';
-import CustomChip from 'components/visual/CustomChip';
 import Verdict from 'components/visual/Verdict';
-import { formatDistanceToNowStrict } from 'date-fns';
+import 'moment/locale/fr';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Moment from 'react-moment';
 import AlertPriority from './alert-priority';
+import AlertStatus from './alert-status';
 
 type AlertListItemProps = {
   item: AlertItem;
 };
 
-const AlertListItem: React.FC<AlertListItemProps> = props => {
+const useStyles = makeStyles(theme => ({
+  file_info: {
+    whiteSpace: 'nowrap',
+    overflowX: 'hidden',
+    textOverflow: 'ellipsis'
+  }
+}));
+const AlertListItem: React.FC<AlertListItemProps> = ({ item }) => {
   const theme = useTheme();
-  const isDownSM = useMediaQuery(theme.breakpoints.down('sm'));
-  const isDownXS = useMediaQuery(theme.breakpoints.down('xs'));
-  if (isDownXS) {
-    return <AlertItemXSmall {...props} />;
-  }
-  if (isDownSM) {
-    return <AlertItemSmall {...props} />;
-  }
-  return <AlertItemDefault {...props} />;
-};
+  const { i18n } = useTranslation();
+  const classes = useStyles();
 
-//
-const AlertItemDefault: React.FC<AlertListItemProps> = ({ item }) => {
-  const theme = useTheme();
   return (
     <div style={{ padding: theme.spacing(2) }}>
-      <Grid container style={{ alignItems: 'center', paddingBottom: theme.spacing(1) }}>
-        <Grid item xs={4}>
+      <Grid container spacing={1}>
+        <Grid item xs={8} className={classes.file_info}>
           <AlertPriority name={item.priority} />
-          <span style={{ marginLeft: theme.spacing(1), marginRight: theme.spacing(1) }}>{item.file.name}</span>
-          <Typography variant="caption">
-            {item.file.size}({(item.file.size / 1024).toFixed(2)}Kb)
-          </Typography>
+          <span style={{ marginLeft: theme.spacing(1) }}>{item.group_count}x</span>
+          <span style={{ marginLeft: theme.spacing(1) }}>{item.file.name}</span>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={2}>
+          {item.owner ? (
+            <>
+              <PersonIcon />
+              &nbsp; {item.owner}
+            </>
+          ) : item.hint_owner ? (
+            <>
+              <PersonIcon />
+              &nbsp; assigned
+            </>
+          ) : null}
+        </Grid>
+        <Grid item xs={2} style={{ textAlign: 'right' }}>
           <Verdict score={item.al.score} />
         </Grid>
         <Grid item xs={2}>
-          <div style={{ display: 'inline-block' }}>
-            <Classification c12n={item.classification} type="pill" size="tiny" format="short" />
-          </div>
+          <AlertStatus name={item.status} />
         </Grid>
-        <Grid item xs={2}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div style={{ flexGrow: 1 }} />
-            {formatDistanceToNowStrict(new Date(item.reporting_ts))}
-          </div>
-        </Grid>
-      </Grid>
-      <Grid container style={{ alignItems: 'center' }}>
-        <Grid item xs={4}>
-          <ChipList items={item.label.map(label => ({ label, size: 'small', variant: 'outlined' }))} />
-        </Grid>
-        <Grid item xs={4}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <CustomChip size="small" variant="outlined" label={item.status} />
-          </div>
-        </Grid>
-        <Grid item xs={2}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            {item.owner ? (
-              <>
-                <PersonIcon />
-                &nbsp; {item.owner}
-              </>
-            ) : item.hint_owner ? (
-              <>
-                <PersonIcon />
-                &nbsp; assigned
-              </>
-            ) : null}
-          </div>
-        </Grid>
-        <Grid item xs={2}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div style={{ flexGrow: 1 }} />
-            <Typography>{item.group_count}x</Typography>
-          </div>
-        </Grid>
-      </Grid>
-    </div>
-  );
-};
-
-const AlertItemSmall: React.FC<AlertListItemProps> = ({ item }) => {
-  const theme = useTheme();
-  return (
-    <div style={{ padding: theme.spacing(2) }}>
-      <Grid container style={{ alignItems: 'center' }}>
-        <Grid item xs={8}>
-          <AlertPriority name={item.priority} />
-          <span style={{ marginLeft: theme.spacing(1), marginRight: theme.spacing(1) }}>{item.file.name}</span>
-          <Typography variant="caption">
-            {item.file.size}({(item.file.size / 1024).toFixed(2)}Kb)
-          </Typography>
-        </Grid>
-        <Grid item xs={2}>
-          <CustomChip label={item.status} variant="outlined" />
-        </Grid>
-        <Grid item xs={2}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div style={{ flexGrow: 1 }} />
-            {formatDistanceToNowStrict(new Date(item.reporting_ts))}
-          </div>
-        </Grid>
-      </Grid>
-      <Grid container style={{ alignItems: 'center' }}>
-        <Grid item xs={8}>
-          <div>
-            <Verdict score={item.al.score} />
-          </div>
-        </Grid>
-        <Grid item xs={2}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <PersonIcon />
-            &nbsp;{item.owner ? item.owner : item.hint_owner ? 'assigned' : 'none'}
-          </div>
-        </Grid>
-        <Grid item xs={2}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div style={{ flexGrow: 1 }} />
-            <Typography>{item.group_count}x</Typography>
-          </div>
-        </Grid>
-      </Grid>
-    </div>
-  );
-};
-
-const AlertItemXSmall: React.FC<AlertListItemProps> = ({ item }) => {
-  const theme = useTheme();
-  return (
-    <div style={{ padding: theme.spacing(2) }}>
-      <Grid container style={{ alignItems: 'center' }}>
-        <Grid item xs={10}>
-          <AlertPriority name={item.priority} />
-          <span style={{ marginLeft: theme.spacing(1), marginRight: theme.spacing(1) }}>{item.file.name}</span>
-        </Grid>
-        <Grid item xs={2}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div style={{ flexGrow: 1 }} />
-            {formatDistanceToNowStrict(new Date(item.reporting_ts))}
-          </div>
-        </Grid>
-      </Grid>
-      <Grid container style={{ alignItems: 'center' }}>
         <Grid item xs={6}>
-          <div>
-            <Verdict score={item.al.score} />
-          </div>
-        </Grid>
-        <Grid item xs={4}>
-          <CustomChip label={item.status} variant="outlined" />
+          <ChipList
+            items={item.label
+              .map(label => ({ label, variant: 'outlined' as 'outlined' }))
+              .concat(
+                item.al.attrib.map(label => ({
+                  label,
+                  color: 'error',
+                  variant: 'outlined' as 'outlined'
+                }))
+              )}
+          />
         </Grid>
         <Grid item xs={2}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div style={{ flexGrow: 1 }} />
-            <Typography>{item.group_count}x</Typography>
-          </div>
+          <ChipList
+            items={[
+              { label: `${item.al.av.length}x AVs`, variant: 'outlined' },
+              { label: `${item.al.domain.length + item.al.ip.length}x IOCs`, variant: 'outlined' }
+            ]}
+          />
+        </Grid>
+        <Grid item xs={2} style={{ textAlign: 'right' }}>
+          <Moment fromNow locale={i18n.language}>
+            {item.reporting_ts}
+          </Moment>
         </Grid>
       </Grid>
     </div>
   );
 };
 
-//
 export default AlertListItem;
