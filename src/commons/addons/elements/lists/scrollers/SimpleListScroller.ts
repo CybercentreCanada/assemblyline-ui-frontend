cd54a799c24e5f7a5db2540b0b1478a474dacecc
Steve Garon <steve.garon@cyber.gc.ca>
2020-12-09 15:46:48 +0000

    Pin to middle while scrolling up and pin to bottom when scrolling down

--- src/commons/addons/elements/lists/scrollers/SimpleListScroller.ts
+++ src/commons/addons/elements/lists/scrollers/SimpleListScroller.ts
@@ -1,24 +1,35 @@
 import ListScroller from './ListScroller';
 
-function elementInViewport(el, offset, listElement) {
-  const top = el.offsetTop;
-  const height = el.offsetHeight;
-  const listElementRect = listElement.getBoundingClientRect();
-
-  return top >= -listElementRect.top + offset && top + height <= -listElementRect.top + window.innerHeight;
-}
-
 export default class SimpleListScroller implements ListScroller {
   constructor(private scrollTarget: HTMLElement, private listElement: HTMLElement, private rowHeight?: number) {}
 
   public scrollTo(position: number) {
     const scrollElement = this.scrollElement(position);
+    if (scrollElement === null) return;
     const offset = this.offset();
-    if (offset > 0 && !elementInViewport(scrollElement, offset, this.listElement)) {
-      scrollElement.scrollIntoView({ block: 'center', behavior: 'smooth' });
+    if (offset > 0) {
+      const block = this.getScrollBlock(scrollElement, offset);
+      if (block) scrollElement.scrollIntoView({ block });
     } else {
-      scrollElement.scrollIntoView({ block: 'nearest', behavior: 'smooth' });
+      scrollElement.scrollIntoView({ block: 'nearest' });
     }
+  }
+
+  private getScrollBlock(el: HTMLElement, offset: number) {
+    const top = el.offsetTop;
+    const height = el.offsetHeight;
+    const listElementRect = this.listElement.getBoundingClientRect();
+    const stElementRect = this.scrollTarget.getBoundingClientRect();
+
+    if (offset - listElementRect.top !== 0 && top < -listElementRect.top + stElementRect.height / 2) {
+      return 'center';
+    }
+
+    if (top + height > -listElementRect.top + stElementRect.height) {
+      return 'end';
+    }
+
+    return null;
   }
 
   private scrollElement(position: number): HTMLElement {
