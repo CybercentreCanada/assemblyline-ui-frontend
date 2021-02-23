efc49fd8b156c64a88e9e886d271ca7fc621570a
Steve Garon <steve.garon@cyber.gc.ca>
2021-02-23 19:30:46 +0000

    Added general tab to service management

--- src/components/routes/admin/service_detail.tsx
+++ src/components/routes/admin/service_detail.tsx
@@ -1,14 +1,28 @@
-import { Grid, IconButton, Tooltip, Typography, useTheme } from '@material-ui/core';
+import {
+  Button,
+  CircularProgress,
+  Grid,
+  IconButton,
+  makeStyles,
+  Paper,
+  Tab,
+  Tooltip,
+  Typography,
+  useTheme
+} from '@material-ui/core';
 import RemoveCircleOutlineOutlinedIcon from '@material-ui/icons/RemoveCircleOutlineOutlined';
-import { Skeleton } from '@material-ui/lab';
+import { Skeleton, TabContext, TabList, TabPanel } from '@material-ui/lab';
 import PageCenter from 'commons/components/layout/pages/PageCenter';
 import useALContext from 'components/hooks/useALContext';
 import useMyAPI from 'components/hooks/useMyAPI';
 import useMySnackbar from 'components/hooks/useMySnackbar';
 import ConfirmationDialog from 'components/visual/ConfirmationDialog';
+import Empty from 'components/visual/Empty';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Redirect, useHistory, useParams } from 'react-router-dom';
+import UnderConstruction from '../under_construction';
+import ServiceGeneral from './service_detail/general';
 
 type ServiceProps = {
   name?: string | null;
@@ -19,17 +33,142 @@ type ParamProps = {
   svc: string;
 };
 
+type Volume = {
+  capacity: string;
+  mount_path: string;
+  storage_class: string;
+};
+
+type Environment = {
+  name: string;
+  value: string;
+};
+
+type Container = {
+  allow_internet_access: boolean;
+  command: string;
+  cpu_cores: number;
+  environment: Environment[];
+  image: string;
+  ports: string[];
+  ram_mb: number;
+  ram_mb_min: number;
+  registry_password: string;
+  registry_username: string;
+};
+
+type SubmissionParams = {
+  default: string | boolean | number;
+  name: string;
+  type: 'int' | 'bool' | 'str' | 'list';
+  value: string | boolean | number;
+  list?: string[];
+};
+
+type Source = {
+  ca_cert: string;
+  default_classification: string;
+  headers: Environment[];
+  name: string;
+  password: string;
+  pattern: string;
+  private_key: string;
+  proxy: string;
+  ssl_ignore_errors: boolean;
+  uri: string;
+  username: string;
+};
+
+type UpdateConfig = {
+  generate_signatures: boolean;
+  method: 'run' | 'build';
+  run_options: Container;
+  sources: Source[];
+  update_interval_seconds: number;
+  wait_for_update: boolean;
+};
+
+export type ServiceDetail = {
+  accepts: string;
+  category: string;
+  config: {
+    [name: string]: string;
+  };
+  default_result_classification: string;
+  dependencies: {
+    [name: string]: {
+      container: Container;
+      volumes: {
+        [name: string]: Volume;
+      };
+    };
+  };
+  description: string;
+  disable_cache: boolean;
+  docker_config: Container;
+  enabled: boolean;
+  is_external: boolean;
+  licence_count: number;
+  name: string;
+  rejects: string;
+  stage: string;
+  submission_params: SubmissionParams[];
+  timeout: number;
+  update_channel: 'dev' | 'beta' | 'rc' | 'stable';
+  update_config: UpdateConfig;
+  version: string;
+};
+
+export type ServiceConstants = {
+  categories: string[];
+  stages: string[];
+};
+
+const useStyles = makeStyles(() => ({
+  buttonProgress: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12
+  }
+}));
+
 function Service({ name, onDeleted }: ServiceProps) {
   const { svc } = useParams<ParamProps>();
   const { t } = useTranslation(['adminServices']);
-  const [service, setService] = useState(null);
+  const [service, setService] = useState<ServiceDetail>(null);
+  const [constants, setConstants] = useState<ServiceConstants>(null);
+  const [versions, setVersions] = useState<string[]>(null);
+  const [tab, setTab] = useState('general');
+  const [modified, setModified] = useState(false);
+  const [buttonLoading, setButtonLoading] = useState(false);
   const theme = useTheme();
   const [deleteDialog, setDeleteDialog] = useState(false);
   const { user: currentUser } = useALContext();
   const { showSuccessMessage } = useMySnackbar();
   const history = useHistory();
+  const classes = useStyles();
 
   const apiCall = useMyAPI();
+
+  function saveService() {
+    apiCall({
+      url: `/api/v4/service/${name || svc}/`,
+      method: 'POST',
+      body: service,
+      onSuccess: () => {
+        setModified(false);
+        showSuccessMessage(t('save.success'));
+      },
+      onEnter: () => setButtonLoading(true),
+      onExit: () => setButtonLoading(false)
+    });
+  }
+
+  const onChangeTab = (event, newValue) => {
+    setTab(newValue);
+  };
 
   const closeDialog = () => {
     setDeleteDialog(false);
@@ -59,6 +198,18 @@ function Service({ name, onDeleted }: ServiceProps) {
         url: `/api/v4/service/${name || svc}/`,
         onSuccess: api_data => {
           setService(api_data.api_response);
+        }
+      });
+      apiCall({
+        url: '/api/v4/service/constants/',
+        onSuccess: api_data => {
+          setConstants(api_data.api_response);
+        }
+      });
+      apiCall({
+        url: `/api/v4/service/versions/${name || svc}/`,
+        onSuccess: api_data => {
+          setVersions(api_data.api_response);
         }
       });
     }
@@ -108,8 +259,63 @@ function Service({ name, onDeleted }: ServiceProps) {
           </Grid>
         </Grid>
 
-        {service && JSON.stringify(service)}
+        {service ? (
+          <TabContext value={tab}>
+            <Paper
+              square
+              style={{ backgroundColor: name ? theme.palette.background.default : theme.palette.background.paper }}
+            >
+              <TabList onChange={onChangeTab} indicatorColor="primary" textColor="primary">
+                <Tab label={t('tab.general')} value="general" />
+                <Tab label={t('tab.docker')} value="docker" />
+                {service.update_config ? <Tab label={t('tab.updater')} value="updater" /> : <Empty />}
+                <Tab label={t('tab.params')} value="params" />
+              </TabList>
+            </Paper>
+            <TabPanel value="general" style={{ paddingLeft: 0, paddingRight: 0 }}>
+              <ServiceGeneral
+                service={service}
+                setService={setService}
+                setModified={setModified}
+                constants={constants}
+                versions={versions}
+              />
+            </TabPanel>
+            <TabPanel value="docker">
+              <UnderConstruction page="Docker Tab" />
+            </TabPanel>
+            <TabPanel value="updater">
+              <UnderConstruction page="Updater Tab" />
+            </TabPanel>
+            <TabPanel value="params">
+              <UnderConstruction page="Params Tab" />
+            </TabPanel>
+          </TabContext>
+        ) : (
+          <Skeleton variant="rect" height="10rem" />
+        )}
       </div>
+
+      {service && modified ? (
+        <div
+          style={{
+            paddingTop: theme.spacing(1),
+            paddingBottom: theme.spacing(1),
+            position: 'fixed',
+            bottom: 0,
+            left: 0,
+            width: '100%',
+            zIndex: theme.zIndex.drawer - 1,
+            backgroundColor: theme.palette.background.default,
+            boxShadow: theme.shadows[4]
+          }}
+        >
+          <Button variant="contained" color="primary" disabled={buttonLoading || !modified} onClick={saveService}>
+            {t('save')}
+            {buttonLoading && <CircularProgress size={24} className={classes.buttonProgress} />}
+          </Button>
+        </div>
+      ) : null}
     </PageCenter>
   );
 }
---
+++ src/components/routes/admin/service_detail/general.tsx
@@ -0,0 +1,298 @@
+import {
+  FormControlLabel,
+  Grid,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  TextField,
+  Typography,
+  useTheme
+} from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
+import useALContext from 'components/hooks/useALContext';
+import Classification from 'components/visual/Classification';
+import 'moment/locale/fr';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { ServiceConstants, ServiceDetail } from '../service_detail';
+
+type ServiceGeneralProps = {
+  service: ServiceDetail;
+  constants: ServiceConstants;
+  versions: string[];
+  setService: (value: ServiceDetail) => void;
+  setModified: (value: boolean) => void;
+};
+
+const ServiceGeneral = ({ service, constants, versions, setService, setModified }: ServiceGeneralProps) => {
+  const { t } = useTranslation(['adminServices']);
+  const theme = useTheme();
+  const { c12nDef } = useALContext();
+
+  const handleDescriptionChange = event => {
+    setModified(true);
+    setService({ ...service, description: event.target.value });
+  };
+
+  const handleVersionChange = event => {
+    setModified(true);
+    setService({ ...service, version: event.target.value });
+  };
+
+  const handleStageChange = event => {
+    setModified(true);
+    setService({ ...service, stage: event.target.value });
+  };
+
+  const handleCategoryChange = event => {
+    setModified(true);
+    setService({ ...service, category: event.target.value });
+  };
+
+  const handleAcceptChange = event => {
+    setModified(true);
+    setService({ ...service, accepts: event.target.value });
+  };
+
+  const handleRejectChange = event => {
+    setModified(true);
+    setService({ ...service, rejects: event.target.value });
+  };
+
+  const handleTimeoutChange = event => {
+    setModified(true);
+    setService({ ...service, timeout: event.target.value });
+  };
+
+  const handleLicenceChange = event => {
+    setModified(true);
+    setService({ ...service, licence_count: event.target.value });
+  };
+
+  const handleExternalToggle = () => {
+    setModified(true);
+    setService({ ...service, is_external: !service.is_external });
+  };
+
+  const handleCacheToggle = () => {
+    setModified(true);
+    setService({ ...service, disable_cache: !service.disable_cache });
+  };
+
+  const setClassification = default_result_classification => {
+    setModified(true);
+    setService({ ...service, default_result_classification });
+  };
+
+  return (
+    <div>
+      {c12nDef.enforce && (
+        <div style={{ paddingBottom: theme.spacing(4) }}>
+          <Classification
+            type="picker"
+            c12n={service ? service.default_result_classification : null}
+            setClassification={setClassification}
+          />
+        </div>
+      )}
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="subtitle2">{t('general.name')}</Typography>
+          {service ? (
+            <TextField fullWidth size="small" margin="dense" variant="outlined" disabled value={service.name} />
+          ) : (
+            <Skeleton style={{ height: '2.5rem' }} />
+          )}
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="subtitle2">{t('general.version')}</Typography>
+          {service ? (
+            <Select
+              id="version"
+              fullWidth
+              value={service.version}
+              onChange={handleVersionChange}
+              variant="outlined"
+              margin="dense"
+              style={{ marginTop: theme.spacing(1), marginBottom: theme.spacing(0.5) }}
+            >
+              {versions ? (
+                versions.map((v, i) => {
+                  return (
+                    <MenuItem key={i} value={v}>
+                      {v}
+                    </MenuItem>
+                  );
+                })
+              ) : (
+                <MenuItem value={service.version}>{service.version}</MenuItem>
+              )}
+            </Select>
+          ) : (
+            <Skeleton style={{ height: '2.5rem' }} />
+          )}
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="subtitle2">{t('general.description')}</Typography>
+          {service ? (
+            <TextField
+              fullWidth
+              size="small"
+              multiline
+              rows={4}
+              margin="dense"
+              variant="outlined"
+              onChange={handleDescriptionChange}
+              value={service.description}
+            />
+          ) : (
+            <Skeleton style={{ height: '2.5rem' }} />
+          )}
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="subtitle2">{t('general.stage')}</Typography>
+          {service ? (
+            <Select
+              id="stage"
+              fullWidth
+              value={service.stage}
+              onChange={handleStageChange}
+              variant="outlined"
+              margin="dense"
+              style={{ marginTop: theme.spacing(1), marginBottom: theme.spacing(0.5) }}
+            >
+              {constants ? (
+                constants.stages.map((s, i) => {
+                  return (
+                    <MenuItem key={i} value={s}>
+                      {s}
+                    </MenuItem>
+                  );
+                })
+              ) : (
+                <MenuItem value={service.stage}>{service.stage}</MenuItem>
+              )}
+            </Select>
+          ) : (
+            <Skeleton style={{ height: '2.5rem' }} />
+          )}
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="subtitle2">{t('general.category')}</Typography>
+          {service ? (
+            <Select
+              id="category"
+              fullWidth
+              value={service.category}
+              onChange={handleCategoryChange}
+              variant="outlined"
+              margin="dense"
+              style={{ marginTop: theme.spacing(1), marginBottom: theme.spacing(0.5) }}
+            >
+              {constants ? (
+                constants.categories.map((c, i) => {
+                  return (
+                    <MenuItem key={i} value={c}>
+                      {c}
+                    </MenuItem>
+                  );
+                })
+              ) : (
+                <MenuItem value={service.category}>{service.category}</MenuItem>
+              )}
+            </Select>
+          ) : (
+            <Skeleton style={{ height: '2.5rem' }} />
+          )}
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="subtitle2">{t('general.accept')}</Typography>
+          {service ? (
+            <TextField
+              fullWidth
+              size="small"
+              margin="dense"
+              variant="outlined"
+              onChange={handleAcceptChange}
+              value={service.accepts}
+            />
+          ) : (
+            <Skeleton style={{ height: '2.5rem' }} />
+          )}
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="subtitle2">{t('general.reject')}</Typography>
+          {service ? (
+            <TextField
+              fullWidth
+              size="small"
+              margin="dense"
+              variant="outlined"
+              onChange={handleRejectChange}
+              value={service.rejects}
+            />
+          ) : (
+            <Skeleton style={{ height: '2.5rem' }} />
+          )}
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="subtitle2">{t('general.timeout')}</Typography>
+          {service ? (
+            <TextField
+              fullWidth
+              type="number"
+              size="small"
+              margin="dense"
+              variant="outlined"
+              onChange={handleTimeoutChange}
+              value={service.timeout}
+            />
+          ) : (
+            <Skeleton style={{ height: '2.5rem' }} />
+          )}
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="subtitle2">{t('general.licence')}</Typography>
+          {service ? (
+            <TextField
+              fullWidth
+              type="number"
+              size="small"
+              margin="dense"
+              variant="outlined"
+              onChange={handleLicenceChange}
+              value={service.licence_count}
+            />
+          ) : (
+            <Skeleton style={{ height: '2.5rem' }} />
+          )}
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="subtitle2">{t('general.location')}</Typography>
+          {service ? (
+            <RadioGroup value={service.is_external} onChange={handleExternalToggle}>
+              <FormControlLabel value control={<Radio />} label={t('general.location.external')} />
+              <FormControlLabel value={false} control={<Radio />} label={t('general.location.internal')} />
+            </RadioGroup>
+          ) : (
+            <Skeleton style={{ height: '2.5rem' }} />
+          )}
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="subtitle2">{t('general.caching')}</Typography>
+          {service ? (
+            <RadioGroup value={service.disable_cache} onChange={handleCacheToggle}>
+              <FormControlLabel value={false} control={<Radio />} label={t('general.caching.enabled')} />
+              <FormControlLabel value control={<Radio />} label={t('general.caching.disabled')} />
+            </RadioGroup>
+          ) : (
+            <Skeleton style={{ height: '2.5rem' }} />
+          )}
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default ServiceGeneral;
