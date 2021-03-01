c859abf2ef5f19e74a61acec71d919f99741986c
Steve Garon <steve.garon@cyber.gc.ca>
2021-03-01 04:55:34 +0000

    Extacted container card and get container edit dialog ready.

---
+++ src/components/routes/admin/service_detail/container_dialog.tsx
@@ -0,0 +1,95 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, makeStyles, useTheme } from '@material-ui/core';
+import 'moment/locale/fr';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Container, Volume } from '../service_detail';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    border: `1px solid ${theme.palette.divider}`,
+    borderRadius: '4px',
+    padding: '8px',
+    margin: '0.25rem 0',
+    overflow: 'auto',
+    wordBreak: 'break-word',
+    '&:hover': {
+      backgroundColor: theme.palette.type === 'dark' ? '#ffffff10' : '#00000005',
+      cursor: 'pointer'
+    }
+  },
+  card_title: {
+    fontSize: 'large',
+    fontFamily: 'monospace'
+  },
+  label: {
+    fontWeight: 500
+  },
+  mono: {
+    fontSize: 'larger',
+    fontFamily: 'monospace'
+  }
+}));
+
+const DEFAULT_CONTAINER: Container = {
+  allow_internet_access: false,
+  command: null,
+  cpu_cores: 1,
+  environment: [],
+  image: '',
+  ports: [],
+  ram_mb: 512,
+  ram_mb_min: 128,
+  registry_password: '',
+  registry_username: ''
+};
+
+type ContainerDialogProps = {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+  container?: Container;
+  name?: string;
+  volumes?: { [name: string]: Volume };
+  onSave: (newContainer: Container, name?: string, newVolumes?: { [name: string]: Volume }) => void;
+};
+
+const WrappedContainerDialog = ({ open, setOpen, container, name, volumes, onSave }: ContainerDialogProps) => {
+  const { t } = useTranslation(['adminServices']);
+  const [modified, setModified] = useState(false);
+  const theme = useTheme();
+
+  const handleSave = () => {
+    onSave(container, name, volumes);
+  };
+
+  return (
+    <div style={{ paddingTop: theme.spacing(1) }}>
+      <Dialog open={open} onClose={() => setOpen(false)} aria-labelledby="form-dialog-title" fullWidth maxWidth="md">
+        <DialogTitle id="form-dialog-title">
+          {t(`container.dialog.title.${name === null ? 'container' : 'dependency'}`)}
+        </DialogTitle>
+        <DialogContent>
+          <div>{name}</div>
+          <div>{JSON.stringify(container)}</div>
+          <div>{JSON.stringify(volumes)}</div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)} color="secondary">
+            {t('container.dialog.cancelText')}
+          </Button>
+          <Button onClick={handleSave} color="primary" disabled={!modified || container.image === ''}>
+            {t('container.dialog.acceptText')}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+WrappedContainerDialog.defaultProps = {
+  container: DEFAULT_CONTAINER,
+  name: null,
+  volumes: null
+};
+
+const ContainerDialog = React.memo(WrappedContainerDialog);
+export default ContainerDialog;
