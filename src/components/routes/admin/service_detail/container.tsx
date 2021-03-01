fbd8e8c37334c28934aae984d04382d4b660b449
Steve Garon <steve.garon@cyber.gc.ca>
2021-03-01 04:55:34 +0000

    Extacted container card and get container edit dialog ready.

--- src/components/routes/admin/service_detail/container.tsx
+++ src/components/routes/admin/service_detail/container.tsx
@@ -1,48 +1,12 @@
-import {
-  Button,
-  Card,
-  Grid,
-  IconButton,
-  makeStyles,
-  MenuItem,
-  Select,
-  Tooltip,
-  Typography,
-  useTheme
-} from '@material-ui/core';
+import { Button, Grid, IconButton, MenuItem, Select, Tooltip, Typography, useTheme } from '@material-ui/core';
 import RemoveCircleOutlineOutlinedIcon from '@material-ui/icons/RemoveCircleOutlineOutlined';
 import { Skeleton } from '@material-ui/lab';
 import 'moment/locale/fr';
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { CgSmartphoneChip, CgSmartphoneRam } from 'react-icons/cg';
-import { Container, ServiceDetail, Volume } from '../service_detail';
-
-const useStyles = makeStyles(theme => ({
-  card: {
-    border: `1px solid ${theme.palette.divider}`,
-    borderRadius: '4px',
-    padding: '8px',
-    margin: '0.25rem 0',
-    overflow: 'auto',
-    wordBreak: 'break-word',
-    '&:hover': {
-      backgroundColor: theme.palette.type === 'dark' ? '#ffffff10' : '#00000005',
-      cursor: 'pointer'
-    }
-  },
-  card_title: {
-    fontSize: 'large',
-    fontFamily: 'monospace'
-  },
-  label: {
-    fontWeight: 500
-  },
-  mono: {
-    fontSize: 'larger',
-    fontFamily: 'monospace'
-  }
-}));
+import { ServiceDetail } from '../service_detail';
+import ContainerCard from './container_card';
+import ContainerDialog from './container_dialog';
 
 type ServiceContainerProps = {
   service: ServiceDetail;
@@ -50,113 +14,9 @@ type ServiceContainerProps = {
   setModified: (value: boolean) => void;
 };
 
-type ContainerCardProps = {
-  container: Container;
-  name?: string;
-  volumes?: { [name: string]: Volume };
-  onChange: (newContainer: Container, name?: string, newVolumes?: { [name: string]: Volume }) => void;
-};
-
-const WrappedContainerCard = ({ container, name, volumes, onChange }: ContainerCardProps) => {
-  const { t } = useTranslation(['adminServices']);
-  const theme = useTheme();
-  const classes = useStyles();
-  const yesColor = theme.palette.type === 'dark' ? theme.palette.success.light : theme.palette.success.dark;
-  const noColor = theme.palette.type === 'dark' ? theme.palette.error.light : theme.palette.error.dark;
-
-  const handleContainerEdit = () => {
-    onChange(container, name, volumes);
-  };
-
-  return (
-    <div style={{ paddingTop: theme.spacing(1) }}>
-      <Card className={classes.card} onClick={handleContainerEdit}>
-        <Grid container>
-          {name && (
-            <Grid item xs={12} className={classes.card_title} style={{ fontWeight: 700 }}>
-              {name}
-            </Grid>
-          )}
-          <Grid item xs={12} className={classes.card_title}>
-            {container.image}
-          </Grid>
-          {container.registry_password && container.registry_username && (
-            <Grid item xs={12}>
-              <i>{t('container.card.creds')}</i>
-            </Grid>
-          )}
-          <Grid item xs={12} style={{ paddingTop: theme.spacing(1), paddingBottom: theme.spacing(1) }}>
-            <Tooltip title={t('container.card.cpu')}>
-              <div style={{ display: 'inline-block', paddingRight: theme.spacing(4) }}>
-                <CgSmartphoneChip size={24} style={{ verticalAlign: 'middle' }} />
-                <span style={{ paddingLeft: theme.spacing(1), verticalAlign: 'middle' }}>{container.cpu_cores}</span>
-              </div>
-            </Tooltip>
-            <Tooltip title={t('container.card.ram')}>
-              <div style={{ display: 'inline-block' }}>
-                <CgSmartphoneRam size={24} style={{ verticalAlign: 'middle' }} />
-                <span style={{ paddingLeft: theme.spacing(1), verticalAlign: 'middle' }}>{container.ram_mb}</span>
-              </div>
-            </Tooltip>
-          </Grid>
-          {container.command && (
-            <>
-              <Grid item xs={5} sm={4} md={2} className={classes.label}>{`${t('container.card.command')}:`}</Grid>
-              <Grid item xs={7} sm={8} md={10} className={classes.mono}>
-                {container.command}
-              </Grid>
-            </>
-          )}
-          <Grid item xs={5} sm={4} md={2} className={classes.label}>{`${t('container.card.internet')}:`}</Grid>
-          <Grid
-            item
-            xs={7}
-            sm={8}
-            md={10}
-            className={classes.mono}
-            style={{ color: container.allow_internet_access ? yesColor : noColor }}
-          >
-            {container.allow_internet_access ? t('container.card.yes') : t('container.card.no')}
-          </Grid>
-          {container.environment && container.environment.length !== 0 && (
-            <Grid item xs={12}>
-              <div className={classes.label}>{`${t('container.card.env')}:`}&nbsp;</div>
-              {container.environment.map((env, id) => {
-                return (
-                  <div key={id} className={classes.mono} style={{ paddingLeft: '2rem' }}>
-                    {`${env.name} = ${env.value}`}
-                  </div>
-                );
-              })}
-            </Grid>
-          )}
-          {volumes && Object.keys(volumes).length !== 0 && (
-            <Grid item xs={12}>
-              <div className={classes.label}>{`${t('container.card.volumes')}:`}&nbsp;</div>
-              {Object.keys(volumes).map((vol, id) => {
-                return (
-                  <div key={id} className={classes.mono} style={{ paddingLeft: '2rem' }}>
-                    {`${vol} = ${volumes[vol].mount_path} (${volumes[vol].capacity})`}
-                  </div>
-                );
-              })}
-            </Grid>
-          )}
-        </Grid>
-      </Card>
-    </div>
-  );
-};
-
-WrappedContainerCard.defaultProps = {
-  name: null,
-  volumes: null
-};
-
-const ContainerCard = React.memo(WrappedContainerCard);
-
 const ServiceContainer = ({ service, setService, setModified }: ServiceContainerProps) => {
   const { t } = useTranslation(['adminServices']);
+  const [dialog, setDialog] = useState(false);
   const theme = useTheme();
 
   const handleChannelChange = event => {
@@ -257,7 +117,8 @@ const ServiceContainer = ({ service, setService, setModified }: ServiceContainer
         )}
       </Grid>
       <Grid item xs={12}>
-        <Button variant="contained" color="primary">
+        <ContainerDialog open={dialog} setOpen={setDialog} name="" onSave={handleDependencyChange} />
+        <Button variant="contained" color="primary" onClick={() => setDialog(true)}>
           {t('container.dependencies.add')}
         </Button>
       </Grid>
---
+++ src/components/routes/admin/service_detail/container_card.tsx
@@ -0,0 +1,148 @@
+import { Card, Grid, makeStyles, Tooltip, useTheme } from '@material-ui/core';
+import 'moment/locale/fr';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { CgSmartphoneChip, CgSmartphoneRam } from 'react-icons/cg';
+import { Container, Volume } from '../service_detail';
+import ContainerDialog from './container_dialog';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    border: `1px solid ${theme.palette.divider}`,
+    borderRadius: '4px',
+    padding: '8px',
+    margin: '0.25rem 0',
+    overflow: 'auto',
+    wordBreak: 'break-word',
+    '&:hover': {
+      backgroundColor: theme.palette.type === 'dark' ? '#ffffff10' : '#00000005',
+      cursor: 'pointer'
+    }
+  },
+  card_title: {
+    fontSize: 'large',
+    fontFamily: 'monospace'
+  },
+  label: {
+    fontWeight: 500
+  },
+  mono: {
+    fontSize: 'larger',
+    fontFamily: 'monospace'
+  }
+}));
+
+type ContainerCardProps = {
+  container: Container;
+  name?: string;
+  volumes?: { [name: string]: Volume };
+  onChange: (newContainer: Container, name?: string, newVolumes?: { [name: string]: Volume }) => void;
+};
+
+const WrappedContainerCard = ({ container, name, volumes, onChange }: ContainerCardProps) => {
+  const { t } = useTranslation(['adminServices']);
+  const [dialog, setDialog] = useState(false);
+  const theme = useTheme();
+  const classes = useStyles();
+  const yesColor = theme.palette.type === 'dark' ? theme.palette.success.light : theme.palette.success.dark;
+  const noColor = theme.palette.type === 'dark' ? theme.palette.error.light : theme.palette.error.dark;
+
+  const handleContainerChange = () => {
+    onChange(container, name, volumes);
+  };
+
+  return (
+    <div style={{ paddingTop: theme.spacing(1) }}>
+      <ContainerDialog
+        open={dialog}
+        setOpen={setDialog}
+        name={name}
+        container={container}
+        volumes={volumes}
+        onSave={handleContainerChange}
+      />
+      <Card className={classes.card} onClick={() => setDialog(true)}>
+        <Grid container>
+          {name && (
+            <Grid item xs={12} className={classes.card_title} style={{ fontWeight: 700 }}>
+              {name}
+            </Grid>
+          )}
+          <Grid item xs={12} className={classes.card_title}>
+            {container.image}
+          </Grid>
+          {container.registry_password && container.registry_username && (
+            <Grid item xs={12}>
+              <i>{t('container.card.creds')}</i>
+            </Grid>
+          )}
+          <Grid item xs={12} style={{ paddingTop: theme.spacing(1), paddingBottom: theme.spacing(1) }}>
+            <Tooltip title={t('container.card.cpu')}>
+              <div style={{ display: 'inline-block', paddingRight: theme.spacing(4) }}>
+                <CgSmartphoneChip size={24} style={{ verticalAlign: 'middle' }} />
+                <span style={{ paddingLeft: theme.spacing(1), verticalAlign: 'middle' }}>{container.cpu_cores}</span>
+              </div>
+            </Tooltip>
+            <Tooltip title={t('container.card.ram')}>
+              <div style={{ display: 'inline-block' }}>
+                <CgSmartphoneRam size={24} style={{ verticalAlign: 'middle' }} />
+                <span style={{ paddingLeft: theme.spacing(1), verticalAlign: 'middle' }}>{container.ram_mb}</span>
+              </div>
+            </Tooltip>
+          </Grid>
+          {container.command && (
+            <>
+              <Grid item xs={5} sm={4} md={2} className={classes.label}>{`${t('container.card.command')}:`}</Grid>
+              <Grid item xs={7} sm={8} md={10} className={classes.mono}>
+                {container.command}
+              </Grid>
+            </>
+          )}
+          <Grid item xs={5} sm={4} md={2} className={classes.label}>{`${t('container.card.internet')}:`}</Grid>
+          <Grid
+            item
+            xs={7}
+            sm={8}
+            md={10}
+            className={classes.mono}
+            style={{ color: container.allow_internet_access ? yesColor : noColor }}
+          >
+            {container.allow_internet_access ? t('container.card.yes') : t('container.card.no')}
+          </Grid>
+          {container.environment && container.environment.length !== 0 && (
+            <Grid item xs={12}>
+              <div className={classes.label}>{`${t('container.card.env')}:`}&nbsp;</div>
+              {container.environment.map((env, id) => {
+                return (
+                  <div key={id} className={classes.mono} style={{ paddingLeft: '2rem' }}>
+                    {`${env.name} = ${env.value}`}
+                  </div>
+                );
+              })}
+            </Grid>
+          )}
+          {volumes && Object.keys(volumes).length !== 0 && (
+            <Grid item xs={12}>
+              <div className={classes.label}>{`${t('container.card.volumes')}:`}&nbsp;</div>
+              {Object.keys(volumes).map((vol, id) => {
+                return (
+                  <div key={id} className={classes.mono} style={{ paddingLeft: '2rem' }}>
+                    {`${vol} = ${volumes[vol].mount_path} (${volumes[vol].capacity})`}
+                  </div>
+                );
+              })}
+            </Grid>
+          )}
+        </Grid>
+      </Card>
+    </div>
+  );
+};
+
+WrappedContainerCard.defaultProps = {
+  name: null,
+  volumes: null
+};
+
+const ContainerCard = React.memo(WrappedContainerCard);
+export default ContainerCard;
