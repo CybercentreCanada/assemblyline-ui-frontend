9a59e134100c35afbbb53c8b13e68cf6ec1504cf
Steve Garon <steve.garon@cyber.gc.ca>
2021-08-27 19:01:49 +0000

    Allow hidden params to be shown

--- src/components/layout/serviceSpec.tsx
+++ src/components/layout/serviceSpec.tsx
@@ -1,13 +1,18 @@
 import {
+  Button,
   Checkbox,
   FormControlLabel,
   makeStyles,
   MenuItem,
   Select,
   TextField,
+  Tooltip,
   Typography,
   useTheme
 } from '@material-ui/core';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const useStyles = makeStyles(theme => ({
   no_pad: {
@@ -22,6 +27,111 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+function Service({ service, idx, setParam, setParamAsync }) {
+  const theme = useTheme();
+  const [showMore, setShowMore] = useState(false);
+  const { t } = useTranslation();
+  return (
+    <div key={idx} style={{ paddingTop: theme.spacing(1), paddingBottom: theme.spacing(1), pageBreakInside: 'avoid' }}>
+      <Typography variant="subtitle1" gutterBottom>
+        <b>{service.name}</b>
+      </Typography>
+      {service.params.map(
+        (param, pidx) =>
+          !param.hide && (
+            <Param key={pidx} param={param} pidx={pidx} idx={idx} setParam={setParam} setParamAsync={setParamAsync} />
+          )
+      )}
+      {showMore
+        ? service.params.map(
+            (param, pidx) =>
+              param.hide && (
+                <Param
+                  key={pidx}
+                  param={param}
+                  pidx={pidx}
+                  idx={idx}
+                  setParam={setParam}
+                  setParamAsync={setParamAsync}
+                />
+              )
+          )
+        : service.params.filter(param => param.hide).length !== 0 && (
+            <Tooltip title={t('show_more')}>
+              <Button size="small" onClick={() => setShowMore(true)} style={{ padding: 0 }}>
+                <MoreHorizIcon />
+              </Button>
+            </Tooltip>
+          )}
+    </div>
+  );
+}
+
+function Param({ param, pidx, idx, setParam, setParamAsync }) {
+  const classes = useStyles();
+  const theme = useTheme();
+  return (
+    <div key={pidx} style={{ paddingBottom: theme.spacing(1) }}>
+      {param.type === 'bool' ? (
+        <div style={{ paddingLeft: theme.spacing(1) }}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                size="small"
+                checked={param.value === 'true' || param.value === true}
+                name="label"
+                onChange={() => setParam(idx, pidx, !param.value)}
+              />
+            }
+            label={
+              <Typography variant="body2" style={{ textTransform: 'capitalize' }}>
+                {param.name.replace(/_/g, ' ')}
+              </Typography>
+            }
+            className={classes.item}
+          />
+        </div>
+      ) : (
+        <>
+          <div>
+            <Typography variant="caption" gutterBottom style={{ textTransform: 'capitalize' }}>
+              {param.name.replace(/_/g, ' ')}
+            </Typography>
+          </div>
+          {param.type === 'list' ? (
+            <Select
+              margin="dense"
+              value={param.value}
+              variant="outlined"
+              onChange={event => setParam(idx, pidx, event.target.value)}
+              fullWidth
+            >
+              {param.list ? (
+                param.list.map((item, i) => (
+                  <MenuItem key={i} value={item}>
+                    {item}
+                  </MenuItem>
+                ))
+              ) : (
+                <MenuItem value="" />
+              )}
+            </Select>
+          ) : (
+            <TextField
+              variant="outlined"
+              type={param.type === 'int' ? 'number' : 'text'}
+              size="small"
+              fullWidth
+              defaultValue={param.value}
+              onChange={event => setParamAsync(idx, pidx, event.target.value)}
+            />
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
 type ServiceSpecProps = {
   service_spec: any[];
   setParam: (service_id: number, param_id: number, param_value: any) => void;
@@ -31,7 +141,6 @@ type ServiceSpecProps = {
 };
 
 function ServiceSpec({ service_spec, setParam, setParamAsync, isSelected, compressed }: ServiceSpecProps) {
-  const classes = useStyles();
   const theme = useTheme();
   return (
     <div
@@ -52,74 +161,7 @@ function ServiceSpec({ service_spec, setParam, setParamAsync, isSelected, compre
       {service_spec.map(
         (service, idx) =>
           isSelected(service.name) && (
-            <div
-              key={idx}
-              style={{ paddingTop: theme.spacing(1), paddingBottom: theme.spacing(1), pageBreakInside: 'avoid' }}
-            >
-              <Typography variant="subtitle1" gutterBottom>
-                <b>{service.name}</b>
-              </Typography>
-              {service.params.map((param, pidx) => (
-                <div key={pidx} style={{ paddingBottom: theme.spacing(1) }}>
-                  {param.type === 'bool' ? (
-                    <div style={{ paddingLeft: theme.spacing(1) }}>
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            size="small"
-                            checked={param.value === 'true' || param.value === true}
-                            name="label"
-                            onChange={() => setParam(idx, pidx, !param.value)}
-                          />
-                        }
-                        label={
-                          <Typography variant="body2" style={{ textTransform: 'capitalize' }}>
-                            {param.name.replace(/_/g, ' ')}
-                          </Typography>
-                        }
-                        className={classes.item}
-                      />
-                    </div>
-                  ) : (
-                    <>
-                      <div>
-                        <Typography variant="caption" gutterBottom style={{ textTransform: 'capitalize' }}>
-                          {param.name.replace(/_/g, ' ')}
-                        </Typography>
-                      </div>
-                      {param.type === 'list' ? (
-                        <Select
-                          margin="dense"
-                          value={param.value}
-                          variant="outlined"
-                          onChange={event => setParam(idx, pidx, event.target.value)}
-                          fullWidth
-                        >
-                          {param.list ? (
-                            param.list.map((item, i) => (
-                              <MenuItem key={i} value={item}>
-                                {item}
-                              </MenuItem>
-                            ))
-                          ) : (
-                            <MenuItem value="" />
-                          )}
-                        </Select>
-                      ) : (
-                        <TextField
-                          variant="outlined"
-                          type={param.type === 'int' ? 'number' : 'text'}
-                          size="small"
-                          fullWidth
-                          defaultValue={param.value}
-                          onChange={event => setParamAsync(idx, pidx, event.target.value)}
-                        />
-                      )}
-                    </>
-                  )}
-                </div>
-              ))}
-            </div>
+            <Service key={idx} service={service} idx={idx} setParam={setParam} setParamAsync={setParamAsync} />
           )
       )}
     </div>
