633b27175ebf209887b80dde88c219d37236eabd
Steve Garon <steve.garon@cyber.gc.ca>
2021-09-23 20:04:32 +0000

    Fix admin landing page

--- src/components/routes/admin.tsx
+++ src/components/routes/admin.tsx
@@ -8,10 +8,17 @@ import { Redirect } from 'react-router-dom';
 export default function Admin() {
   const layout = useMyLayout();
   const { user: currentUser } = useUser<CustomUser>();
+  let items = [];
+  for (const item of layout.leftnav.elements) {
+    if (item.type === 'group' && item.element.id === 'adminmenu') {
+      // eslint-disable-next-line @typescript-eslint/dot-notation
+      items = item.element['items'];
+    }
+  }
 
   return currentUser.is_admin ? (
     <PageCenter margin={4} width="100%">
-      <LinkGrid items={layout.topnav.adminMenu} />
+      <LinkGrid items={layout.topnav.adminMenu.length !== 0 ? layout.topnav.adminMenu : items} />
     </PageCenter>
   ) : (
     <Redirect to="/forbidden" />
