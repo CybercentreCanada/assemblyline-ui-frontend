4274687419058fa447e4de2e4d25be4a6eac06b9
Steve Garon <steve.garon@cyber.gc.ca>
2021-10-07 14:14:23 +0000

    Added missing imports

--- src/components/routes/admin/service_detail/updater.tsx
+++ src/components/routes/admin/service_detail/updater.tsx
@@ -4,10 +4,13 @@ import {
   Grid,
   IconButton,
   InputAdornment,
+  MenuItem,
   OutlinedInput,
   Radio,
   RadioGroup,
+  Select,
   Slider,
+  TextField,
   Tooltip,
   Typography,
   useTheme
@@ -241,7 +244,7 @@ const ServiceUpdater = ({ service, setService, setModified }: ServiceUpdaterProp
           </Grid>
         </Grid>
       )}
-        
+
       <Grid item xs={12}>
         <Typography variant="subtitle2">{t('updater.sources')}</Typography>
         <SourceDialog
