5c306c0289aac15cb1a926a3aee6ef4db49fff04
Steve Garon <steve.garon@cyber.gc.ca>
2021-11-09 17:58:50 +0000

    Catch all errors and display a message to the user instead of just a white screen

--- src/commons/components/app/AppContextProvider.tsx
+++ src/commons/components/app/AppContextProvider.tsx
@@ -1,7 +1,20 @@
-import { makeStyles, ThemeProvider, useMediaQuery } from '@material-ui/core';
+import {
+  Button,
+  CssBaseline,
+  makeStyles,
+  Paper,
+  ThemeProvider,
+  Typography,
+  useMediaQuery,
+  useTheme
+} from '@material-ui/core';
 import useAppTheme, { AppThemeProps } from 'commons/components/hooks/useAppTheme';
 import { SnackbarProvider } from 'notistack';
 import React, { useState } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { useTranslation } from 'react-i18next';
+import { GiMineExplosion } from 'react-icons/gi';
+import PageCenter from '../layout/pages/PageCenter';
 
 //
 interface AppbarStyles {
@@ -56,6 +69,49 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.only('xs')]: { wordBreak: 'break-word' }
   }
 }));
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  const { t } = useTranslation();
+  const theme = useTheme();
+  const downSM = useMediaQuery(theme.breakpoints.down('sm'));
+  const isXS = useMediaQuery(theme.breakpoints.only('xs'));
+
+  return (
+    <div role="alert">
+      <CssBaseline />
+      <PageCenter width={isXS ? '100%' : '70%'} margin={4}>
+        <div style={{ paddingTop: theme.spacing(10), fontSize: 200, color: theme.palette.secondary.main }}>
+          <GiMineExplosion fontSize="inherit" />
+        </div>
+        <Typography variant={downSM ? 'h4' : 'h3'} gutterBottom>
+          {t('error.title')}
+        </Typography>
+        <Typography variant={downSM ? 'body1' : 'h6'} gutterBottom>
+          {t('error.description')}
+        </Typography>
+        <Paper
+          component="pre"
+          variant="outlined"
+          style={{
+            padding: theme.spacing(2),
+            whiteSpace: 'pre-wrap',
+            wordBreak: 'break-word',
+            minHeight: downSM ? theme.spacing(8) : theme.spacing(16),
+            display: 'flex',
+            alignContent: 'center',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          {error.message}
+        </Paper>
+        <Button onClick={resetErrorBoundary} style={{ margin: theme.spacing(4) }} color="primary">
+          {t('error.button')}
+        </Button>
+      </PageCenter>
+    </div>
+  );
+}
 
 // Implementation of the AppContext provider component.
 // This should be the root application component renderered by the index.tsx into the dom element-id 'root'
@@ -126,7 +182,14 @@ const AppContextProvider: React.FC<AppProviderProps> = ({ defaultTheme, colors, 
       }}
     >
       <ThemeProvider theme={appTheme}>
-        <SnackbarProvider classes={{ root: classes.snackroot }}>{children}</SnackbarProvider>
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+          onReset={() => {
+            window.location.reload();
+          }}
+        >
+          <SnackbarProvider classes={{ root: classes.snackroot }}>{children}</SnackbarProvider>
+        </ErrorBoundary>
       </ThemeProvider>
     </AppContext.Provider>
   );
