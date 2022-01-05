d2dcf3cb40198f462cf845f58a5ff64906e0e427
Nicolas Rheaume <nicolas.rheaume@cyber.gc.ca>
2022-01-05 17:20:58 +0000

    useLocation: Added scroll to the URL

--- src/components/visual/HexViewer/hooks/useLocation.tsx
+++ src/components/visual/HexViewer/hooks/useLocation.tsx
@@ -1,6 +1,6 @@
 import SimpleSearchQuery from 'components/visual/SearchBar/simple-search-query';
 import React, { useCallback, useContext, useEffect, useMemo, useRef } from 'react';
-import { HexProps, useCopy, useCursor } from '..';
+import { HexProps, useCopy, useCursor, useScroll } from '..';
 
 export type HexParams = {
   cursor?: string;
@@ -14,6 +14,7 @@ export const LocationContext = React.createContext<LocationContextProps>(null);
 
 export const WrappedLocationProvider = ({ children }: HexProps) => {
   const { nextCursorIndex, onCursorIndexChange } = useCursor();
+  const { nextScrollIndex, onScrollChange } = useScroll();
   const { onCopyText } = useCopy();
 
   const query = useRef<SimpleSearchQuery>(null);
@@ -21,16 +22,18 @@ export const WrappedLocationProvider = ({ children }: HexProps) => {
   useEffect(() => {
     query.current = new SimpleSearchQuery(window.location.search, '');
     const params: HexParams = query.current.getParams();
+    if (params.hasOwnProperty('scroll')) onScrollChange('setScroll', parseInt(params.cursor));
     if (params.hasOwnProperty('cursor')) onCursorIndexChange(parseInt(params.cursor));
-  }, [onCursorIndexChange]);
+  }, [onCursorIndexChange, onScrollChange]);
 
   const onLocationShare = useCallback(() => {
     query.current.deleteAll();
+    if (nextScrollIndex.current !== null) query.current.set('scroll', nextScrollIndex.current);
     if (nextCursorIndex.current !== null) query.current.set('cursor', nextCursorIndex.current);
     onCopyText(
       `${window.location.origin}${window.location.pathname}?${query.current.toString()}${window.location.hash}`
     );
-  }, [nextCursorIndex, onCopyText]);
+  }, [nextCursorIndex, nextScrollIndex, onCopyText]);
 
   return (
     <LocationContext.Provider
