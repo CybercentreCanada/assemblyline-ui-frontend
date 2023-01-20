2f5574788879c7991a730b05115b89059ead6549
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2023-01-20 18:10:48 +0000

    Reverted to original AppContextProvider

--- src/commons_deprecated/components/app/AppContextProvider.tsx
+++ src/commons_deprecated/components/app/AppContextProvider.tsx
@@ -1,4 +1,4 @@
-import { Button, CssBaseline, Paper, Theme, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Button, CssBaseline, Paper, ThemeProvider, Typography, useMediaQuery, useTheme } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import useAppTheme, { AppThemeProps } from 'commons_deprecated/components/hooks/useAppTheme';
 import { SnackbarProvider } from 'notistack';
@@ -7,11 +7,6 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { useTranslation } from 'react-i18next';
 import { GiSpottedBug } from 'react-icons/gi';
 import PageCenter from '../layout/pages/PageCenter';
-
-declare module '@mui/styles/defaultTheme' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
-}
 
 //
 interface AppbarStyles {
@@ -175,95 +170,11 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-// Implementation of the AppContext provider component.
-// This should be the root application component renderered by the index.tsx into the dom element-id 'root'
-const AppThemeInit: React.FC<any> = ({ defaultTheme, colors, defaultContext, children }) => {
-  // Application provided context.
-  const [context, setContext] = useState<any>(defaultContext);
-  const classes = useStyles();
-
-  // Theme state.
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const [theme, setTheme] = useState<'dark' | 'light'>(getInitialTheme(defaultTheme, prefersDarkMode));
-
-  // Theme toggle handler.
-  const toggleTheme = () => {
-    const newTheme = theme === 'dark' ? 'light' : 'dark';
-    localStorage.setItem('darkMode', JSON.stringify(newTheme === 'dark'));
-    setTheme(newTheme);
-  };
-
-  // Build the theme.
-  const [appTheme] = useAppTheme(theme === 'dark', colors);
-
-  // Compute appbar styles for specified layout.
-  const getAppbarStyles = (layout: 'side' | 'top'): AppbarStyles => {
-    const isTopLayout = layout === 'top';
-    const isDarkTheme = theme === 'dark';
-    const isLightTheme = theme === 'light';
-
-    // Compute background color.
-    let backgroundColor = isTopLayout ? appTheme.palette.primary.dark : appTheme.palette.background.default;
-    if (isTopLayout) {
-      if (isDarkTheme && colors.appbar?.sticky?.dark) {
-        backgroundColor = colors.appbar.sticky.dark.backgroundColor;
-      }
-      if (isLightTheme && colors.appbar?.sticky?.light) {
-        backgroundColor = colors.appbar.sticky.light.backgroundColor;
-      }
-    }
-
-    // Compute elevation.
-    const elevation = isTopLayout
-      ? colors.appbar?.sticky?.elevation !== undefined
-        ? colors.appbar?.sticky?.elevation
-        : 2
-      : 0;
-
-    // Wrap it up and send-it!
-    return {
-      color: appTheme.palette.getContrastText(backgroundColor),
-      backgroundColor,
-      elevation
-    };
-  };
-
-  // Initialize app context provider, theme, snackbar and render children component.
-  // Snackbar needs to be rendered as child of theme provider.  CSS glitches ensues if not the case.
-  return (
-    <AppContext.Provider
-      value={{
-        context,
-        theme,
-        colors,
-        isDarkTheme: theme === 'dark',
-        isLightTheme: theme === 'light',
-        toggleTheme,
-        setContext,
-        getAppbarStyles
-      }}
-    >
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={appTheme}>
-          <ErrorBoundary
-            FallbackComponent={ErrorFallback}
-            onReset={() => {
-              window.location.reload();
-            }}
-          >
-            <SnackbarProvider classes={{ root: classes.snackroot }}>{children}</SnackbarProvider>
-          </ErrorBoundary>
-        </ThemeProvider>
-      </StyledEngineProvider>
-    </AppContext.Provider>
-  );
-};
-
 function ErrorFallback({ error, resetErrorBoundary }) {
   const { t } = useTranslation();
   const theme = useTheme();
   const classes = useStyles();
-  const downSM = useMediaQuery(theme.breakpoints.down('xl'));
+  const downSM = useMediaQuery(theme.breakpoints.down('sm'));
   const isXS = useMediaQuery(theme.breakpoints.only('xs'));
 
   return (
@@ -371,18 +282,16 @@ const AppContextProvider: React.FC<AppProviderProps> = ({ defaultTheme, colors, 
         getAppbarStyles
       }}
     >
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={appTheme}>
-          <ErrorBoundary
-            FallbackComponent={ErrorFallback}
-            onReset={() => {
-              window.location.reload();
-            }}
-          >
-            <SnackbarProvider classes={{ root: classes.snackroot }}>{children}</SnackbarProvider>
-          </ErrorBoundary>
-        </ThemeProvider>
-      </StyledEngineProvider>
+      <ThemeProvider theme={appTheme}>
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+          onReset={() => {
+            window.location.reload();
+          }}
+        >
+          <SnackbarProvider classes={{ root: classes.snackroot }}>{children}</SnackbarProvider>
+        </ErrorBoundary>
+      </ThemeProvider>
     </AppContext.Provider>
   );
 };
