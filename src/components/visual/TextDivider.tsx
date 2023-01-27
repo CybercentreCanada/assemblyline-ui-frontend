22285755381ad203b5289103ab710d6c1b7d4f64
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2023-01-27 21:06:53 +0000

    fixed paper textDivider

--- src/components/visual/TextDivider.tsx
+++ src/components/visual/TextDivider.tsx
@@ -1,29 +1,34 @@
 import makeStyles from '@mui/styles/makeStyles';
+import clsx from 'clsx';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-const useStyles = (forcePaper: boolean) =>
-  makeStyles(theme => ({
-    divider: {
-      display: 'inline-block',
-      textAlign: 'center',
-      width: '100%',
-      margin: '30px 0',
-      position: 'relative',
-      borderTop: `1px solid ${theme.palette.divider}`
-    },
-    inner: {
-      backgroundColor: theme.palette.background.paper,
-      left: '50%',
-      marginLeft: '-30px',
-      position: 'absolute',
-      top: '-10px',
-      width: '60px',
-      [theme.breakpoints.down('lg')]: {
-        backgroundColor: forcePaper ? theme.palette.background.paper : theme.palette.background.default
-      }
+const useStyles = makeStyles(theme => ({
+  divider: {
+    display: 'inline-block',
+    textAlign: 'center',
+    width: '100%',
+    margin: '30px 0',
+    position: 'relative',
+    borderTop: `1px solid ${theme.palette.divider}`
+  },
+  inner: {
+    backgroundColor: theme.palette.background.paper,
+    left: '50%',
+    marginLeft: '-30px',
+    position: 'absolute',
+    top: '-10px',
+    width: '60px',
+    [theme.breakpoints.down('lg')]: {
+      backgroundColor: theme.palette.background.default
     }
-  }))();
+  },
+  forcePaper: {
+    [theme.breakpoints.down('lg')]: {
+      backgroundColor: theme.palette.background.paper
+    }
+  }
+}));
 
 interface TextDividerProps {
   forcePaper?: boolean;
@@ -31,11 +36,11 @@ interface TextDividerProps {
 
 const TextDivider: React.FC<TextDividerProps> = ({ forcePaper = false }) => {
   const { t } = useTranslation();
-  const classes = useStyles(forcePaper);
+  const classes = useStyles();
 
   return (
     <div>
-      <div className={classes.divider}>
+      <div className={clsx(classes.divider, forcePaper && classes.forcePaper)}>
         <div className={classes.inner}>{t('divider')}</div>
       </div>
     </div>
