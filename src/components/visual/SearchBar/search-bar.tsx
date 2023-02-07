d1b4904581e2b4ff3e579ff9614a62724a3b9382
Steve Garon <steve.garon@cyber.gc.ca>
2023-02-07 16:00:51 +0000

    Fix tooltip order in searchbar

--- src/components/visual/SearchBar/search-bar.tsx
+++ src/components/visual/SearchBar/search-bar.tsx
@@ -125,17 +125,17 @@ const SearchBar: React.FC<SearchBarProps> = ({
             onClear={_onValueClear}
           />
         </Box>
-        <IconButton
-          onClick={_onValueClear}
-          edge="end"
-          size={!upMD ? 'small' : 'large'}
-          style={{ marginRight: theme.spacing(upMD ? 0 : 0.5) }}
-          disabled={searching}
-        >
-          <Tooltip title={t('clear_filter')}>
+        <Tooltip title={t('clear_filter')}>
+          <IconButton
+            onClick={_onValueClear}
+            edge="end"
+            size={!upMD ? 'small' : 'large'}
+            style={{ marginRight: theme.spacing(upMD ? 0 : 0.5) }}
+            disabled={searching}
+          >
             <BackspaceIcon fontSize={!upMD ? 'small' : 'medium'} />
-          </Tooltip>
-        </IconButton>
+          </IconButton>
+        </Tooltip>
         {buttons.length !== 0 && (
           <Divider
             orientation="vertical"
@@ -143,18 +143,21 @@ const SearchBar: React.FC<SearchBarProps> = ({
             style={{ marginLeft: theme.spacing(upMD ? 1 : 0.5), marginRight: theme.spacing(upMD ? 1 : 0.5) }}
           />
         )}
-        {buttons.map((b, i) => (
-          <IconButton
-            key={`searchbar-button-${i}`}
-            {...b.props}
-            edge="end"
-            size={!upMD ? 'small' : 'large'}
-            style={{ marginRight: theme.spacing(upMD ? 0 : 0.5) }}
-            disabled={searching}
-          >
-            {b.tooltip ? <Tooltip title={b.tooltip}>{b.icon}</Tooltip> : b.icon}
-          </IconButton>
-        ))}
+        {buttons.map((b, i) => {
+          const button = (
+            <IconButton
+              key={`searchbar-button-${i}`}
+              {...b.props}
+              edge="end"
+              size={!upMD ? 'small' : 'large'}
+              style={{ marginRight: theme.spacing(upMD ? 0 : 0.5) }}
+              disabled={searching}
+            >
+              {b.icon}
+            </IconButton>
+          );
+          return b.tooltip ? <Tooltip title={b.tooltip}>{button}</Tooltip> : button;
+        })}
         {extras}
       </Box>
       {searching && (
