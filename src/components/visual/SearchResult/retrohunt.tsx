58cac7c86729ac16e714ac012ea0ddc0c7cee0d4
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2023-03-22 13:43:53 +0000

    Fixed and sorted the search tables

--- src/components/visual/SearchResult/retrohunt.tsx
+++ src/components/visual/SearchResult/retrohunt.tsx
@@ -1,7 +1,6 @@
 import { AlertTitle, Skeleton, Tooltip } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import TableContainer from '@mui/material/TableContainer';
-import useALContext from 'components/hooks/useALContext';
 import 'moment/locale/fr';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -38,7 +37,6 @@ const WrappedRetrohuntTable: React.FC<RetrohuntTableProps> = ({
   onRowClick = () => null
 }) => {
   const { t, i18n } = useTranslation(['search']);
-  const { c12nDef } = useALContext();
 
   return retrohuntResults ? (
     retrohuntResults.total !== 0 ? (
@@ -47,7 +45,7 @@ const WrappedRetrohuntTable: React.FC<RetrohuntTableProps> = ({
           <DivTableHead>
             <DivTableRow>
               <SortableHeaderCell sortField="created" allowSort={allowSort}>
-                {t('header.time')}
+                {t('header.created')}
               </SortableHeaderCell>
               <SortableHeaderCell sortField="code" allowSort={allowSort}>
                 {t('header.code')}
@@ -58,24 +56,6 @@ const WrappedRetrohuntTable: React.FC<RetrohuntTableProps> = ({
               <SortableHeaderCell sortField="classification" allowSort={allowSort}>
                 {t('header.classification')}
               </SortableHeaderCell>
-              {/* <SortableHeaderCell sortField="max_score" allowSort={allowSort}>
-                {t('header.verdict')}
-              </SortableHeaderCell>
-              <DivTableCell>{t('header.description')}</DivTableCell>
-              <SortableHeaderCell sortField="params.submitter" allowSort={allowSort}>
-                {t('header.user')}
-              </SortableHeaderCell>
-              <SortableHeaderCell sortField="file_count" allowSort={allowSort}>
-                {t('header.numfiles')}
-              </SortableHeaderCell>
-              {c12nDef && c12nDef.enforce && (
-                <SortableHeaderCell sortField="classification" allowSort={allowSort}>
-                  {t('header.classification')}
-                </SortableHeaderCell>
-              )}
-              <SortableHeaderCell sortField="error_count" allowSort={allowSort}>
-                {t('header.status')}
-              </SortableHeaderCell> */}
             </DivTableRow>
           </DivTableHead>
           <DivTableBody>
@@ -84,9 +64,6 @@ const WrappedRetrohuntTable: React.FC<RetrohuntTableProps> = ({
                 key={id}
                 component={Link}
                 to={() => undefined}
-                // to={
-                //   retrohunt.state === 'completed' ? `/retrohunt/${retrohunt.id}` : `/retrohunt/detail/${retrohunt.id}`
-                // }
                 onClick={event => {
                   event.preventDefault();
                   onRowClick(retrohunt?.code);
@@ -106,34 +83,6 @@ const WrappedRetrohuntTable: React.FC<RetrohuntTableProps> = ({
                 <DivTableCell>{retrohunt?.code}</DivTableCell>
                 <DivTableCell>{retrohunt?.creator}</DivTableCell>
                 <DivTableCell>{retrohunt?.classification}</DivTableCell>
-                {/* <DivTableCell>
-                  <Tooltip title={retrohunt.times.submitted}>
-                    <>
-                      <Moment fromNow locale={i18n.language} date={retrohunt.times.submitted} />
-                    </>
-                  </Tooltip>
-                </DivTableCell>
-                <DivTableCell>
-                  <Verdict score={retrohunt.max_score} fullWidth />
-                </DivTableCell>
-                <DivTableCell breakable>{maxLenStr(retrohunt.params.description, 150)}</DivTableCell>
-                <DivTableCell style={{ whiteSpace: 'nowrap' }}>{retrohunt.params.submitter}</DivTableCell>
-                <DivTableCell>{retrohunt.file_count}</DivTableCell>
-                {c12nDef && c12nDef.enforce && (
-                  <DivTableCell>
-                    <Classification type="text" size="tiny" c12n={retrohunt.classification} format="short" />
-                  </DivTableCell>
-                )}
-                <DivTableCell style={{ textAlign: 'center' }}>
-                  <RetrohuntState state={retrohunt.state} error_count={retrohunt.error_count} />
-                </DivTableCell>
-                <DivTableCell style={{ textAlign: 'center' }}>
-                  {retrohunt.from_archive && (
-                    <Tooltip title={t('archive')}>
-                      <ArchiveOutlinedIcon />
-                    </Tooltip>
-                  )}
-                </DivTableCell> */}
               </LinkRow>
             ))}
           </DivTableBody>
