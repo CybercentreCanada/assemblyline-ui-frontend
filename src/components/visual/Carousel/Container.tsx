83071c6d8cb4043ba1ecf9b6ea382b08b97e5098
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2023-09-28 07:13:45 +0000

    Implementing the carousel navigation mode

---
+++ src/components/visual/Carousel/Container.tsx
@@ -0,0 +1,466 @@
+import BrokenImageOutlinedIcon from '@mui/icons-material/BrokenImageOutlined';
+import ChevronLeftOutlinedIcon from '@mui/icons-material/ChevronLeftOutlined';
+import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+import ZoomInIcon from '@mui/icons-material/ZoomIn';
+import { alpha, Button, CircularProgress, IconButton, Modal, Paper, Skeleton, Tooltip, useTheme } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
+import clsx from 'clsx';
+import Carousel from 'commons/addons/carousel/Carousel';
+import useMyAPI from 'components/hooks/useMyAPI';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import CarouselItem from './Item';
+
+const useStyles = makeStyles(theme => {
+  const navbarHeight = 'min(128px, 30vw, 30vh)';
+
+  // const isSmall = `@media (max-width: ${theme.breakpoints.values.md}px) or (max-height: ${theme.breakpoints.values.sm}px)`;
+
+  const augmentedPaper = theme.palette.augmentColor({ color: { main: theme.palette.background.paper } })[
+    theme.palette.mode === 'dark' ? 'light' : 'dark'
+  ];
+
+  const options = {
+    easing: theme.transitions.easing.easeInOut,
+    duration: theme.transitions.duration.shortest
+  };
+
+  return {
+    backdrop: {
+      // backgroundColor: 'transparent',
+      backdropFilter: 'blur(2px)',
+      transition: 'backdrop-filter 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;',
+      zIndex: 1350
+    },
+    root: {
+      height: '100%',
+      width: '100%'
+    },
+    menu: {
+      position: 'absolute',
+      width: '100%',
+      display: 'grid',
+      gridTemplateColumns: 'auto 1fr auto',
+      alignItems: 'start',
+      justifyItems: 'center'
+    },
+    info: {
+      borderRadius: '0px 0px 4px 4px',
+      display: 'grid',
+      alignContent: 'end',
+      alignItems: 'stretch',
+      gridTemplateColumns: 'auto 1fr',
+      padding: theme.spacing(1),
+      columnGap: theme.spacing(1),
+      minWidth: '10vw',
+      '&:hover>div': {
+        whiteSpace: 'wrap !important'
+      },
+      '&>div:nth-child(2n+1)': {
+        fontWeight: 500
+      },
+      '&>div:nth-child(2n)': {
+        fontWeight: 400,
+        overflowX: 'hidden',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis'
+      }
+    },
+
+    buttonWrapper: {
+      cursor: 'pointer',
+      padding: theme.spacing(1),
+      backgroundColor: 'rgba(0,0,0,0)',
+      transition: theme.transitions.create('background-color', options),
+      '&:hover>button': {
+        backgroundColor: augmentedPaper
+      }
+    },
+    button: {
+      color: theme.palette.text.secondary,
+      backgroundColor: theme.palette.background.paper
+    },
+    navButton: {
+      position: 'absolute',
+      top: '64px',
+      bottom: navbarHeight,
+      display: 'grid',
+      placeItems: 'center',
+      cursor: 'pointer',
+      padding: theme.spacing(1),
+      backgroundColor: 'rgba(0,0,0,0)',
+      transition: theme.transitions.create('background-color', options),
+      '&:hover': {
+        backgroundColor: alpha(augmentedPaper, 0.1)
+      },
+      '&:hover>div': {
+        backgroundColor: augmentedPaper
+      }
+    },
+    navPrev: {
+      left: 0,
+      borderRadius: `0 ${theme.spacing(0.5)} ${theme.spacing(0.5)} 0`
+    },
+    navNext: {
+      right: 0,
+      borderRadius: `${theme.spacing(0.5)} 0 0 ${theme.spacing(0.5)}`
+    },
+    navIcon: {
+      color: theme.palette.text.secondary,
+      backgroundColor: theme.palette.background.paper
+    },
+    navActive: {
+      backgroundColor: alpha(augmentedPaper, 0.1),
+      '&>div': {
+        backgroundColor: augmentedPaper
+      }
+    },
+    navbarContainer: {
+      position: 'absolute',
+      bottom: 0,
+      width: '100%',
+      height: navbarHeight,
+      overflow: 'scroll',
+      scrollbarWidth: 'none', // Firefox
+      '-ms-overflow-style': 'none', // Internet Explorer 10+
+      '&::-webkit-scrollbar': {
+        display: 'none' // Safari and Chrome
+      }
+    },
+    navbar: {
+      height: '100%',
+      width: 'fit-content',
+      minWidth: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      gap: theme.spacing(1),
+      padding: theme.spacing(1)
+    },
+
+    imageContainer: {
+      position: 'absolute',
+      width: '100%',
+      top: '64px',
+      bottom: navbarHeight,
+      display: 'grid',
+      placeItems: 'center'
+    },
+    imageWrapper: {
+      position: 'absolute',
+      display: 'grid'
+    },
+    imageOverlay: {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      justifyContent: 'space-between'
+    },
+    imageOverlayItem: {
+      width: '20%',
+      cursor: 'pointer'
+    },
+    image: {
+      minHeight: navbarHeight,
+      minWidth: navbarHeight,
+      maxWidth: '100vw',
+      maxHeight: `calc(100vh - 64px - ${navbarHeight})`,
+      imageRendering: 'pixelated'
+    },
+    loadingContainer: {
+      height: `calc(2 * ${navbarHeight})`,
+      aspectRatio: '4 / 3',
+      display: 'grid',
+      placeItems: 'center',
+      borderRadius: theme.spacing(0.5),
+      backgroundColor: theme.palette.background.paper,
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundBlendMode: 'soft-light',
+      imageRendering: 'unset',
+      overflow: 'hidden'
+    }
+  };
+});
+
+export type Image = {
+  name: string;
+  description: string;
+  img: string;
+  thumb: string;
+};
+
+type CarouselContainerProps = {
+  images: Image[];
+  open: boolean;
+  index: number;
+  setIndex: React.Dispatch<React.SetStateAction<number>>;
+  onClose: () => void;
+};
+
+const WrappedCarouselContainer = ({
+  images = [],
+  open = false,
+  index = 0,
+  setIndex = () => null,
+  onClose = () => null
+}: CarouselContainerProps) => {
+  const { t } = useTranslation('carousel');
+  const theme = useTheme();
+  const classes = useStyles();
+  const { apiCall } = useMyAPI();
+
+  const [thumbData, setThumbData] = useState<string>(null);
+  const [imgData, setImgData] = useState<string>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [isZooming, setIsZooming] = useState<boolean>(false);
+  const [zoom, setZoom] = useState<number>(100);
+
+  const prevRef = useRef<HTMLButtonElement>(null);
+  const nextRef = useRef<HTMLButtonElement>(null);
+  const navbarRef = useRef<HTMLDivElement>(null);
+  const navbarScroll = useRef<{ isDown: boolean; isDragging: boolean; scrollLeft: Number; startX: number }>({
+    isDown: false,
+    isDragging: false,
+    scrollLeft: 0,
+    startX: 0
+  });
+
+  const currentImage = useMemo<Image>(() => images && images[index], [images, index]);
+
+  // const isSmall = useMediaQuery(
+  //   `@media (max-width: ${theme.breakpoints.values.md}px) or (max-height: ${theme.breakpoints.values.sm}px)`
+  // );
+
+  const handleImageChange = useCallback(
+    (value: number) =>
+      (event: any = null) => {
+        event?.stopPropagation();
+        if (!images && images.length <= 1) return;
+        setIndex(i => (i + value) % images.length);
+      },
+    [images, setIndex]
+  );
+
+  const handleNavbarDown = useCallback((event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    navbarScroll.current = {
+      isDown: true,
+      isDragging: false,
+      startX: event.pageX - navbarRef.current.offsetLeft,
+      scrollLeft: navbarRef.current.scrollLeft
+    };
+  }, []);
+
+  const handleNavbarStop = useCallback((event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    navbarScroll.current = { isDown: false, isDragging: false, scrollLeft: 0, startX: 0 };
+  }, []);
+
+  const handleNavbarMove = useCallback((event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!navbarScroll.current.isDown) return;
+    event.preventDefault();
+    const x = event.pageX - navbarRef.current.offsetLeft;
+    const walkX = x - navbarScroll.current.startX;
+    navbarRef.current.scrollLeft = navbarScroll.current.scrollLeft.valueOf() - walkX;
+    navbarScroll.current.isDragging = Math.abs(walkX) > 20;
+  }, []);
+
+  const handleImagePointerEnter = useCallback(
+    (type: 'prev' | 'next') => (event: React.PointerEvent<HTMLDivElement>) => {
+      event.stopPropagation();
+      type === 'prev'
+        ? prevRef.current?.classList.add(classes.navActive)
+        : nextRef.current?.classList.add(classes.navActive);
+    },
+    [classes.navActive]
+  );
+
+  const handleImagePointerLeave = useCallback(
+    (type: 'prev' | 'next') => (event: React.PointerEvent<HTMLDivElement>) => {
+      event.stopPropagation();
+      type === 'prev'
+        ? prevRef.current?.classList.remove(classes.navActive)
+        : nextRef.current?.classList.remove(classes.navActive);
+    },
+    [classes.navActive]
+  );
+
+  useEffect(() => {
+    if (!currentImage) return;
+    apiCall({
+      url: `/api/v4/file/image/${currentImage.thumb}/`,
+      allowCache: true,
+      onSuccess: api_data => setThumbData(api_data.api_response),
+      onFailure: () => setThumbData(null)
+    });
+    apiCall({
+      url: `/api/v4/file/image/${currentImage.img}/`,
+      allowCache: true,
+      onEnter: () => setLoading(true),
+      onExit: () => setLoading(false),
+      onSuccess: api_data => setImgData(api_data.api_response),
+      onFailure: () => setImgData(null)
+    });
+
+    return () => {
+      setThumbData(null);
+      setImgData(null);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentImage]);
+
+  useEffect(() => {
+    const thumbs = document.getElementById('carousel')?.querySelectorAll('.carousel-thumb');
+    if (!thumbs || index < 0 || index >= thumbs?.length) return;
+    thumbs[index]?.scrollIntoView({
+      inline: 'center',
+      behavior: 'smooth'
+    });
+  }, [imgData, index]);
+
+  return (
+    images &&
+    images.length > 0 && (
+      <Carousel onPrevious={handleImageChange(-1)} onNext={handleImageChange(1)}>
+        <Modal className={classes.backdrop} open={open} onClose={onClose}>
+          <div id="carousel" className={classes.root}>
+            <div className={classes.imageContainer} onClick={onClose}>
+              <div className={classes.imageWrapper}>
+                <div className={classes.imageOverlay} onClick={e => e.stopPropagation()}>
+                  <div
+                    className={classes.imageOverlayItem}
+                    onPointerEnter={handleImagePointerEnter('prev')}
+                    onPointerLeave={handleImagePointerLeave('prev')}
+                    onClick={handleImageChange(-1)}
+                  />
+                  <div
+                    className={classes.imageOverlayItem}
+                    onPointerEnter={handleImagePointerEnter('next')}
+                    onPointerLeave={handleImagePointerLeave('next')}
+                    onClick={handleImageChange(1)}
+                  />
+                </div>
+                {imgData ? (
+                  <img
+                    className={clsx(true && classes.image)}
+                    src={imgData}
+                    alt={currentImage?.name}
+                    draggable={false}
+                    // onMouseMove={handleImageMouseMove}
+                    // onMouseLeave={handleImageMouseLeave}
+                    // onClick={handleImageClick}
+                    // style={
+                    //   {
+                    //     // minWidth: '256px',
+                    //     // width: `${zoom}%`
+                    //     // minWidth: `max(${zoom}%, 256px)`,
+                    //     // minWidth: `256px`,
+                    //     // maxWidth: `max(${zoom}vw, 256px)`
+                    //     // transform: `scale(${Math.max(zoom / 100, 1)})`
+                    //     // transform: `scale(${zoom / 100})`
+                    //     // transform: `translate(-${zoom}%, -${zoom}%) scale(${zoom / 100})`
+                    //   }
+                    // }
+                  />
+                ) : (
+                  <div
+                    className={classes.loadingContainer}
+                    style={thumbData && { backgroundImage: `url(${thumbData})` }}
+                  >
+                    {loading ? (
+                      <CircularProgress color="primary" />
+                    ) : (
+                      <BrokenImageOutlinedIcon color="primary" fontSize="large" />
+                    )}
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div id="carousel-menu" className={clsx(classes.menu)}>
+              <div className={classes.buttonWrapper} onClick={onClose}>
+                <Tooltip title={t('close')}>
+                  <IconButton className={classes.button} size="large" children={<CloseIcon />} />
+                </Tooltip>
+              </div>
+              <Paper className={classes.info}>
+                <div>{t('name')}</div>
+                <div>{currentImage ? currentImage?.name : loading && <Skeleton variant="rounded" />}</div>
+                <div>{t('description')}</div>
+                <div>{currentImage ? currentImage?.description : loading && <Skeleton variant="rounded" />}</div>
+              </Paper>
+              <div className={classes.buttonWrapper}>
+                <Tooltip title={t('zoom')}>
+                  <IconButton className={classes.button} size="large" children={<ZoomInIcon />} />
+                </Tooltip>
+              </div>
+            </div>
+
+            <Button
+              className={clsx(classes.navButton, classes.navPrev)}
+              ref={prevRef}
+              size="large"
+              onClick={handleImageChange(-1)}
+            >
+              <Tooltip title={t('prev')}>
+                <IconButton
+                  className={classes.navIcon}
+                  component="div"
+                  size="large"
+                  disableFocusRipple
+                  disableRipple
+                  disableTouchRipple
+                  children={<ChevronLeftOutlinedIcon />}
+                />
+              </Tooltip>
+            </Button>
+
+            <Button
+              className={clsx(classes.navButton, classes.navNext)}
+              ref={nextRef}
+              size="large"
+              onClick={handleImageChange(1)}
+            >
+              <Tooltip title={t('next')}>
+                <IconButton
+                  className={classes.navIcon}
+                  component="div"
+                  size="large"
+                  disableFocusRipple
+                  disableRipple
+                  disableTouchRipple
+                  children={<ChevronRightOutlinedIcon />}
+                />
+              </Tooltip>
+            </Button>
+
+            <div
+              id="carousel-navbar"
+              ref={navbarRef}
+              className={clsx(classes.navbarContainer)}
+              onPointerDown={handleNavbarDown}
+              onPointerLeave={handleNavbarStop}
+              onPointerUp={handleNavbarStop}
+              onPointerMove={handleNavbarMove}
+            >
+              <div className={clsx(classes.navbar)}>
+                {images.map((image, i) => (
+                  <CarouselItem
+                    key={'thumb-' + i}
+                    alt={image.name}
+                    src={image.thumb}
+                    selected={index === i}
+                    onClick={() => !navbarScroll.current.isDragging && setIndex(i)}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </Modal>
+      </Carousel>
+    )
+  );
+};
+
+export const CarouselContainer = React.memo(WrappedCarouselContainer);
---
+++ src/components/visual/Carousel/Item.tsx
@@ -0,0 +1,90 @@
+import BrokenImageOutlinedIcon from '@mui/icons-material/BrokenImageOutlined';
+import { Button, CircularProgress, Tooltip } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
+import clsx from 'clsx';
+import useMyAPI from 'components/hooks/useMyAPI';
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    height: '100%',
+    aspectRatio: '1 / 1',
+    backgroundColor: theme.palette.background.paper,
+    padding: 0,
+    overflow: 'hidden',
+    '&:hover': {
+      backgroundColor: theme.palette.background.paper
+    }
+  },
+  img: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    imageRendering: 'pixelated',
+    borderRadius: theme.spacing(0.5),
+    filter: `brightness(50%)`,
+    transition: theme.transitions.create('filter', {
+      easing: theme.transitions.easing.easeInOut,
+      duration: theme.transitions.duration.short
+    }),
+    '&:hover': {
+      filter: `brightness(90%)`
+    }
+  },
+  selected: {
+    border: `2px solid ${theme.palette.primary.main}`,
+    '&>img': {
+      filter: `brightness(100%)`
+    }
+  }
+}));
+
+type Props = {
+  alt: string;
+  src: string;
+  selected?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+const WrappedCarouselItem = ({ alt, src, selected, onClick }: Props) => {
+  const { t } = useTranslation(['carousel']);
+  const classes = useStyles();
+  const { apiCall } = useMyAPI();
+
+  const [data, setData] = useState<string>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    apiCall({
+      url: `/api/v4/file/image/${src}/`,
+      allowCache: true,
+      onEnter: () => setLoading(true),
+      onExit: () => setLoading(false),
+      onSuccess: api_data => setData(api_data.api_response),
+      onFailure: () => setData(null)
+    });
+
+    return () => {
+      setData(null);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [alt, src]);
+
+  return (
+    <Tooltip title={alt} placement="top">
+      <Button className={clsx('carousel-thumb', classes.button, selected && classes.selected)} onPointerUp={onClick}>
+        {data ? (
+          <img className={classes.img} src={data} alt={alt} draggable={false} />
+        ) : loading ? (
+          <CircularProgress color="primary" />
+        ) : (
+          <BrokenImageOutlinedIcon color="primary" fontSize="large" />
+        )}
+      </Button>
+    </Tooltip>
+  );
+};
+
+export const CarouselItem = React.memo(WrappedCarouselItem);
+export default CarouselItem;
