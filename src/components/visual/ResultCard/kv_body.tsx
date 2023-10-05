af89971ef8127ebf706cb7801969c282d9862d96
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2023-10-05 13:11:33 +0000

    Changed the display for the String Hits

--- src/components/visual/ResultCard/kv_body.tsx
+++ src/components/visual/ResultCard/kv_body.tsx
@@ -1,5 +1,51 @@
+import makeStyles from '@mui/styles/makeStyles';
 import TitleKey from 'components/visual/TitleKey';
-import { default as React } from 'react';
+import { default as React, useCallback } from 'react';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, auto)',
+    columnGap: theme.spacing(1)
+  },
+  offsets: {
+    display: 'flex',
+    columnGap: theme.spacing(1)
+  }
+}));
+
+const StringHits = ({ values }: { values: string[] }) => {
+  const classes = useStyles();
+
+  const Hit = useCallback(
+    ({ value }: { value: string }) => {
+      const title = `${value.match(/.* : /g)}`.replace(' : ', '');
+      const hit = `${value.match(/''.*''/g)}`.replace(/''/g, '');
+      const offsets = `${value.match(/\[@.*\]/g)}`.replace('[@ ', '').replace(']', '').split(', ');
+
+      return (
+        <>
+          <div>{title}</div>
+          <div style={{ justifySelf: 'end' }}>{hit}</div>
+          <div className={classes.offsets}>
+            {offsets.map((offset, i) => (
+              <div key={i}>{offset}</div>
+            ))}
+          </div>
+        </>
+      );
+    },
+    [classes.offsets]
+  );
+
+  return (
+    <div className={classes.container}>
+      {values.map((value, i) => (
+        <Hit key={i} value={value} />
+      ))}
+    </div>
+  );
+};
 
 const WrappedKVBody = ({ body }) => (
   <table cellSpacing={0}>
@@ -20,7 +66,9 @@ const WrappedKVBody = ({ body }) => (
             <td style={{ paddingRight: '16px', wordBreak: 'normal' }}>
               <TitleKey title={key} />
             </td>
-            <td style={{ wordBreak: 'break-word' }}>{value}</td>
+            <td style={{ wordBreak: 'break-word' }}>
+              {key === 'string_hits' ? <StringHits values={body[key]} /> : value}
+            </td>
           </tr>
         );
       })}
