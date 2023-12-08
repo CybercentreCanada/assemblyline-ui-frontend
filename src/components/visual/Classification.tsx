64751697818f1dc51921e947670c5780bc288667
Steve Garon <steve.garon@cyber.gc.ca>
2023-12-08 14:40:19 +0000

    Fix classification loading state

--- src/components/visual/Classification.tsx
+++ src/components/visual/Classification.tsx
@@ -214,8 +214,7 @@ function WrappedClassification({
   return (
     c12nDef &&
     c12nDef.enforce &&
-    !!validated?.parts?.lvl &&
-    (c12n ? (
+    (!!validated?.parts?.lvl && c12n ? (
       <>
         {type === 'text' ? (
           <span className={classes[computeColor()]}>
