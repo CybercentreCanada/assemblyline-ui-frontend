d2651eb4da70029f342322eec9dc5ea70219eb8a
Steve Garon <steve.garon@cyber.gc.ca>
2023-12-11 16:24:41 +0000

    Added an input base Date picker

--- src/components/visual/DatePicker.tsx
+++ src/components/visual/DatePicker.tsx
@@ -2,7 +2,7 @@ import { Button, Dialog, DialogActions, IconButton, TextField, Tooltip } from '@
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 
 import EventIcon from '@mui/icons-material/Event';
-import { LocalizationProvider, StaticDatePicker } from '@mui/x-date-pickers';
+import { DatePicker as MuiDatePicker, LocalizationProvider, StaticDatePicker } from '@mui/x-date-pickers';
 import moment from 'moment';
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -11,64 +11,94 @@ interface DatePickerProps {
   date: string;
   setDate: (date: string) => void;
   tooltip?: string;
+  type?: 'button' | 'input';
 }
 
-function WrappedDatePicker({ date, setDate, tooltip = null }: DatePickerProps) {
+function WrappedDatePicker({ date, setDate, tooltip = null, type = 'button' }: DatePickerProps) {
   const [tempDate, setTempDate] = React.useState(null);
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const { t } = useTranslation();
 
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  tomorrow.setHours(0, 0, 0, 0);
+
   useEffect(() => {
-    setTempDate(date);
+    setTempDate(moment(date));
   }, [date]);
 
   // Build chip based on computed values
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
-      <Tooltip title={tooltip ? tooltip : t('date.open')}>
-        <IconButton onClick={handleOpen}>
-          <EventIcon />
-        </IconButton>
-      </Tooltip>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <StaticDatePicker
-          displayStaticWrapperAs="desktop"
-          value={moment(tempDate)}
+      {type === 'button' ? (
+        <>
+          <Tooltip title={tooltip ? tooltip : t('date.open')}>
+            <IconButton onClick={handleOpen}>
+              <EventIcon />
+            </IconButton>
+          </Tooltip>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <StaticDatePicker
+              displayStaticWrapperAs="desktop"
+              value={moment(tempDate)}
+              onChange={newValue => {
+                setTempDate(newValue.format('YYYY-MM-DDThh:mm:ss.SSSSSSZ'));
+              }}
+              renderInput={params => <TextField {...params} />}
+              minDate={moment(tomorrow)}
+            />
+
+            <DialogActions>
+              <Button
+                onClick={() => {
+                  setDate(null);
+                  handleClose();
+                }}
+                color="secondary"
+              >
+                {t('date.clear')}
+              </Button>
+              <Button
+                onClick={() => {
+                  setDate(tempDate);
+                  handleClose();
+                }}
+                color="primary"
+                autoFocus
+                disabled={tempDate === null}
+              >
+                {t('date.select')}
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </>
+      ) : (
+        <MuiDatePicker
+          value={tempDate}
           onChange={newValue => {
-            setTempDate(newValue.format('YYYY-MM-DDThh:mm:ss.SSSSSSZ'));
+            setTempDate(newValue);
+            setDate(newValue && newValue.isValid() ? `${newValue.format('YYYY-MM-DDThh:mm:ss.SSSSSS')}Z` : null);
           }}
-          renderInput={params => <TextField {...params} />}
+          // renderInput={params => <TextField {...params} />}
+          renderInput={({ inputRef, inputProps, InputProps }) => (
+            <TextField
+              size="small"
+              label={tooltip ? tooltip : null}
+              ref={inputRef}
+              inputProps={{ ...inputProps }}
+              InputProps={{ ...InputProps }}
+            />
+          )}
+          minDate={moment(tomorrow)}
         />
-        <DialogActions>
-          <Button
-            onClick={() => {
-              setDate(null);
-              handleClose();
-            }}
-            color="secondary"
-          >
-            {t('date.clear')}
-          </Button>
-          <Button
-            onClick={() => {
-              setDate(tempDate);
-              handleClose();
-            }}
-            color="primary"
-            autoFocus
-            disabled={tempDate === null}
-          >
-            {t('date.select')}
-          </Button>
-        </DialogActions>
-      </Dialog>
+      )}
     </LocalizationProvider>
   );
 }
