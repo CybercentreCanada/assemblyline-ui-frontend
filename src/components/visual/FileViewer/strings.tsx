a05958c63971ad140da71a32b9632b66255b5636
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2024-01-31 15:20:41 +0000

    Removed the diff functionality

--- src/components/visual/FileViewer/strings.tsx
+++ src/components/visual/FileViewer/strings.tsx
@@ -4,11 +4,7 @@ import useMyAPI from 'components/hooks/useMyAPI';
 import { CustomUser } from 'components/hooks/useMyUser';
 import ForbiddenPage from 'components/routes/403';
 import React, { useEffect, useMemo, useState } from 'react';
-import { useLocation } from 'react-router';
 import MonacoEditor, { LANGUAGE_SELECTOR } from '../MonacoEditor';
-import SimpleSearchQuery from '../SearchBar/simple-search-query';
-
-const DIFF_QUERY = 'diff';
 
 type Props = {
   sha256: string;
@@ -16,19 +12,13 @@ type Props = {
 };
 
 const WrappedStringsSection: React.FC<Props> = ({ sha256, type: propType = null }) => {
-  const location = useLocation();
   const { apiCall } = useMyAPI();
   const { user: currentUser } = useAppUser<CustomUser>();
 
   const [data, setData] = useState<string>(null);
   const [error, setError] = useState<string>(null);
-  const [diffData, setDiffData] = useState<string>(null);
 
   const type = useMemo<string>(() => (propType && propType in LANGUAGE_SELECTOR ? propType : 'unknown'), [propType]);
-  const diffSha256 = useMemo<string>(() => {
-    const query = new SimpleSearchQuery(location.search);
-    return query.has(DIFF_QUERY) ? query.get(DIFF_QUERY) : null;
-  }, [location.search]);
 
   useEffect(() => {
     if (!sha256 || data) return;
@@ -46,21 +36,8 @@ const WrappedStringsSection: React.FC<Props> = ({ sha256, type: propType = null 
   }, [data, sha256]);
 
   useEffect(() => {
-    if (!diffSha256) return;
-    apiCall({
-      url: `/api/v4/file/strings/${diffSha256}/`,
-      allowCache: true,
-      onEnter: () => setDiffData(null),
-      onSuccess: api_data => setDiffData(api_data.api_response),
-      onFailure: api_data => setError(api_data.api_error_message)
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data, sha256]);
-
-  useEffect(() => {
     return () => {
       setData(null);
-      setDiffData(null);
     };
   }, [sha256]);
 
@@ -68,16 +45,7 @@ const WrappedStringsSection: React.FC<Props> = ({ sha256, type: propType = null 
   else if (error) return <Alert severity="error">{error}</Alert>;
   else if (!data) return <LinearProgress />;
   else
-    return (
-      <MonacoEditor
-        diff={!!diffSha256}
-        value={!diffSha256 ? data : null}
-        modified={diffSha256 ? data : null}
-        original={diffSha256 ? diffData : null}
-        language={LANGUAGE_SELECTOR[type]}
-        options={{ links: false, readOnly: true }}
-      />
-    );
+    return <MonacoEditor value={data} language={LANGUAGE_SELECTOR[type]} options={{ links: false, readOnly: true }} />;
 };
 
 export const StringsSection = React.memo(WrappedStringsSection);
