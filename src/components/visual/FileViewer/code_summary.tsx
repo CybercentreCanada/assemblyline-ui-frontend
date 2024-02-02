e1cb33b9fffab7eee1ff1c8832986fb8b7e55ebd
Steve Garon <steve.garon@cyber.gc.ca>
2024-02-02 16:26:14 +0000

    Fix code summary in mobile mode

--- src/components/visual/FileViewer/code_summary.tsx
+++ src/components/visual/FileViewer/code_summary.tsx
@@ -1,14 +1,21 @@
-import { Alert, LinearProgress, Tooltip, useTheme } from '@mui/material';
+import { Alert, CircularProgress, Tooltip, useTheme } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import useAppUser from 'commons/components/app/hooks/useAppUser';
 import useMyAPI from 'components/hooks/useMyAPI';
 import { CustomUser } from 'components/hooks/useMyUser';
 import ForbiddenPage from 'components/routes/403';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import AIMarkdown from '../AiMarkdown';
 
 const useStyles = makeStyles(theme => ({
+  spinner: {
+    textAlign: 'center',
+    position: 'relative',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  },
   watermark: {
     float: 'right',
     color: theme.palette.text.disabled,
@@ -19,24 +26,33 @@ const useStyles = makeStyles(theme => ({
 
 type Props = {
   sha256: string;
+  archiveOnly?: boolean;
 };
 
-const WrappedCodeSection: React.FC<Props> = ({ sha256 }) => {
+const WrappedCodeSection: React.FC<Props> = ({ sha256, archiveOnly = false }) => {
   const { apiCall } = useMyAPI();
   const { user: currentUser } = useAppUser<CustomUser>();
   const { t } = useTranslation(['fileViewer']);
   const classes = useStyles();
   const theme = useTheme();
 
+  const [analysing, setAnalysing] = useState(false);
   const [codeError, setCodeError] = useState(null);
   const [codeSummary, setCodeSummary] = useState(null);
   const [codeTruncated, setCodeTruncated] = useState(false);
 
-  useEffect(() => {
-    if (!codeSummary) {
+  const getCodeSummary = useCallback(
+    noCache => {
+      const params = [];
+      if (noCache) {
+        params.push('no_cache');
+      }
+      if (archiveOnly) {
+        params.push('archive_only');
+      }
       apiCall({
-        allowCache: true,
-        url: `/api/v4/file/code_summary/${sha256}/`,
+        allowCache: !noCache,
+        url: `/api/v4/file/code_summary/${sha256}/${noCache ? '?no_cache' : ''}`,
         onSuccess: api_data => {
           if (codeError !== null) setCodeError(null);
           setCodeSummary(api_data.api_response.content);
@@ -44,12 +60,25 @@ const WrappedCodeSection: React.FC<Props> = ({ sha256 }) => {
         },
         onFailure: api_data => {
           setCodeError(api_data.api_error_message);
-          setCodeSummary(null);
-          setCodeTruncated(false);
-        }
+          if (codeSummary !== null) {
+            setCodeSummary(null);
+            setCodeTruncated(false);
+          }
+        },
+        onEnter: () => setAnalysing(true),
+        onExit: () => setAnalysing(false)
       });
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [codeSummary, codeError, sha256]
+  );
+
+  useEffect(() => {
+    if (!codeSummary) {
+      getCodeSummary(false);
     }
     return () => {
+      setCodeError(null);
       setCodeSummary(null);
       setCodeTruncated(false);
     };
@@ -57,26 +86,31 @@ const WrappedCodeSection: React.FC<Props> = ({ sha256 }) => {
   }, [sha256]);
 
   if (!currentUser.roles.includes('file_detail')) return <ForbiddenPage />;
-  else if (codeError) return <Alert severity="error">{codeError}</Alert>;
-  else if (!codeSummary) return <LinearProgress />;
-  else
-    return (
-      <div
-        style={{
-          padding: theme.spacing(1),
-          marginTop: !codeError ? theme.spacing(-4) : null
-        }}
-      >
-        <AIMarkdown markdown={codeSummary} truncated={codeTruncated} />
+  return (
+    <>
+      <div style={{ flexGrow: 1, marginTop: !analysing && !codeError ? theme.spacing(-4) : null }}>
+        {analysing ? (
+          <div className={classes.spinner}>
+            <div style={{ paddingBottom: theme.spacing(2) }}>{t('analysing_code')}</div>
+            <CircularProgress variant="indeterminate" />
+          </div>
+        ) : codeError ? (
+          <Alert severity="error">{codeError}</Alert>
+        ) : (
+          <AIMarkdown markdown={codeSummary} truncated={codeTruncated} />
+        )}
+      </div>
+      {!analysing && (codeSummary || codeError) && (
         <div>
           <Tooltip title={t('powered_by_ai.tooltip')} placement="top-end">
-            <div className={classes.watermark} style={{ paddingBottom: theme.spacing(1) }}>
+            <div className={classes.watermark} onClick={() => getCodeSummary(true)}>
               {t('powered_by_ai')}
             </div>
           </Tooltip>
         </div>
-      </div>
-    );
+      )}
+    </>
+  );
 };
 
 export const CodeSection = React.memo(WrappedCodeSection);
