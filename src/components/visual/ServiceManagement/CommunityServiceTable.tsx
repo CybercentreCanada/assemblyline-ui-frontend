63c36eff4d8b98aca23515c0c6e57904a96122c3
Steve Garon <steve.garon@cyber.gc.ca>
2024-02-15 00:49:54 +0000

    Intermediate dialog to install community services

--- src/components/visual/ServiceManagement/CommunityServiceTable.tsx
+++ src/components/visual/ServiceManagement/CommunityServiceTable.tsx
@@ -1,8 +1,21 @@
 import CloudDownloadOutlinedIcon from '@mui/icons-material/CloudDownloadOutlined';
-import { AlertTitle, IconButton, Link, Paper, Skeleton, TableContainer, Tooltip } from '@mui/material';
+import {
+  AlertTitle,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Link,
+  Paper,
+  Skeleton,
+  TableContainer,
+  Tooltip
+} from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import 'moment/locale/fr';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { JSONFeedItem } from '.';
 import { DivTable, DivTableBody, DivTableCell, DivTableHead, DivTableRow, ExternalLinkRow } from '../DivTable';
@@ -34,6 +47,7 @@ const useStyles = makeStyles(() => ({
 
 const WrappedCommunityServiceTable: React.FC<Props> = ({ services, installingServices, onInstall }: Props) => {
   const { t } = useTranslation(['search']);
+  const [serviceToInstall, setServiceToInstall] = useState<JSONFeedItem>(null);
   const classes = useStyles();
 
   const navigate = useCallback(url => {
@@ -42,70 +56,91 @@ const WrappedCommunityServiceTable: React.FC<Props> = ({ services, installingSer
 
   return services ? (
     services.length !== 0 ? (
-      <TableContainer component={Paper}>
-        <DivTable>
-          <DivTableHead>
-            <DivTableRow>
-              <DivTableCell>{t('header.name')}</DivTableCell>
-              <DivTableCell>{t('header.author')}</DivTableCell>
-              <DivTableCell>{t('header.description')}</DivTableCell>
-              <DivTableCell />
-            </DivTableRow>
-          </DivTableHead>
-          <DivTableBody>
-            {services?.map((service, i) => (
-              <ExternalLinkRow key={i + ' - ' + service.title} hover href={service.url}>
-                <DivTableCell>{service.summary}</DivTableCell>
-                <DivTableCell>
-                  <Link
-                    href={service.authors[0].url}
-                    onClick={event => {
-                      event.preventDefault();
-                      event.stopPropagation();
-                      navigate(service.authors[0].url);
-                    }}
+      <>
+        <Dialog
+          open={serviceToInstall !== null}
+          onClose={() => setServiceToInstall(null)}
+          aria-labelledby="serviceToInstall=title"
+          fullWidth
+          maxWidth="md"
+        >
+          <DialogTitle id="serviceToInstall=title">{t('serviceToInstall.title')}</DialogTitle>
+          <DialogContent></DialogContent>
+          <DialogActions>
+            <Button onClick={() => setServiceToInstall(null)} color="secondary">
+              {t('serviceToInstall.cancelText')}
+            </Button>
+            <Button onClick={() => onInstall([serviceToInstall])} color="primary">
+              {t('serviceToInstall.acceptText')}
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <TableContainer component={Paper}>
+          <DivTable>
+            <DivTableHead>
+              <DivTableRow>
+                <DivTableCell>{t('header.name')}</DivTableCell>
+                <DivTableCell>{t('header.author')}</DivTableCell>
+                <DivTableCell>{t('header.description')}</DivTableCell>
+                <DivTableCell />
+              </DivTableRow>
+            </DivTableHead>
+            <DivTableBody>
+              {services?.map((service, i) => (
+                <ExternalLinkRow key={i + ' - ' + service.title} hover href={service.url}>
+                  <DivTableCell>{service.summary}</DivTableCell>
+                  <DivTableCell>
+                    <Link
+                      href={service.authors[0].url}
+                      onClick={event => {
+                        event.preventDefault();
+                        event.stopPropagation();
+                        navigate(service.authors[0].url);
+                      }}
+                      style={{ textDecoration: 'none' }}
+                    >
+                      {service.authors[0].name}
+                    </Link>
+                  </DivTableCell>
+                  <DivTableCell>{service.content_text}</DivTableCell>
+                  <DivTableCell
+                    className={classes.center}
+                    style={{ whiteSpace: 'nowrap', paddingTop: 0, paddingBottom: 0 }}
                   >
-                    {service.authors[0].name}
-                  </Link>
-                </DivTableCell>
-                <DivTableCell>{service.content_text}</DivTableCell>
-                <DivTableCell
-                  className={classes.center}
-                  style={{ whiteSpace: 'nowrap', paddingTop: 0, paddingBottom: 0 }}
-                >
-                  <Tooltip
-                    title={
-                      installingServices?.includes(service?.summary)
-                        ? t('installing')
-                        : `${service.title} ${t('available')}!`
-                    }
-                  >
-                    <span>
-                      <IconButton
-                        color="primary"
-                        onClick={event => {
-                          event.preventDefault();
-                          event.stopPropagation();
-                          onInstall([service]);
-                        }}
-                        disabled={installingServices?.includes(service?.summary)}
-                        size="large"
-                      >
-                        <CloudDownloadOutlinedIcon />
-                      </IconButton>
-                    </span>
-                  </Tooltip>
-                </DivTableCell>
-              </ExternalLinkRow>
-            ))}
-          </DivTableBody>
-        </DivTable>
-      </TableContainer>
+                    <Tooltip
+                      title={
+                        installingServices?.includes(service?.summary)
+                          ? t('installing')
+                          : `${service.title} ${t('available')}!`
+                      }
+                    >
+                      <span>
+                        <IconButton
+                          color="primary"
+                          onClick={event => {
+                            event.preventDefault();
+                            event.stopPropagation();
+                            setServiceToInstall(service);
+                          }}
+                          disabled={installingServices?.includes(service?.summary)}
+                          size="large"
+                        >
+                          <CloudDownloadOutlinedIcon />
+                        </IconButton>
+                      </span>
+                    </Tooltip>
+                  </DivTableCell>
+                </ExternalLinkRow>
+              ))}
+            </DivTableBody>
+          </DivTable>
+        </TableContainer>
+      </>
     ) : (
       <div style={{ width: '100%' }}>
         <InformativeAlert>
-          <AlertTitle>{t('no_services_available_title')}</AlertTitle>
-          {t('no_services_available_desc')}
+          <AlertTitle>{t('no_community_services_available_title')}</AlertTitle>
+          {t('no_community_services_available_desc')}
         </InformativeAlert>
       </div>
     )
