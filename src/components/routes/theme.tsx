c162cac9f52615a0499218f6a7c489c42404ad0d
Steve Garon <steve.garon@cyber.gc.ca>
2024-03-20 21:47:05 +0000

    Added more stuff to the theme page

--- src/components/routes/theme.tsx
+++ src/components/routes/theme.tsx
@@ -1,5 +1,36 @@
+import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined';
 import FileOpenIcon from '@mui/icons-material/FileOpen';
-import { Button, IconButton, Paper, Stack, TableContainer, TextField } from '@mui/material';
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Button,
+  Card,
+  Checkbox,
+  CircularProgress,
+  Divider,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  IconButton,
+  InputLabel,
+  LinearProgress,
+  MenuItem,
+  Paper,
+  Radio,
+  RadioGroup,
+  Rating,
+  Select,
+  Skeleton,
+  Slider,
+  Stack,
+  Switch,
+  Tab,
+  TableContainer,
+  Tabs,
+  TextField,
+  useTheme
+} from '@mui/material';
 import Typography from '@mui/material/Typography';
 import PageCenter from 'commons/components/pages/PageCenter';
 import Classification from 'components/visual/Classification';
@@ -8,20 +39,24 @@ import { DivTable, DivTableBody, DivTableCell, DivTableHead, DivTableRow, LinkRo
 import AssemblylineIcon from 'components/visual/Icons';
 
 const Theme = () => {
+  const theme = useTheme();
   return (
     <PageCenter width="65%" margin={4}>
       <Stack alignItems="center" spacing={2}>
-        <Typography variant="h6">Icons</Typography>
+        <Typography variant="h4">Icons</Typography>
         <AssemblylineIcon sx={{ width: '192px', height: '192px' }} />
 
-        <Typography variant="h6">Text</Typography>
+        <Typography variant="h4">Text</Typography>
         <Stack direction="row" spacing={1}>
           <Typography color="textPrimary">This is what the default text will look like</Typography>
           <Typography color="textSecondary">Alternate text will look like that</Typography>
+          <Typography color={theme.palette.action.disabled}>Disabled text will look like this</Typography>
           <Typography color="primary">Text in primary color will look like this</Typography>
           <Typography color="secondary">Text in secondary color like that</Typography>
+          <Typography color="error">Text in error will look like this</Typography>
         </Stack>
-        <Typography variant="h6">Buttons</Typography>
+
+        <Typography variant="h4">Buttons</Typography>
         <Stack direction="row" spacing={1}>
           {[
             'primary' as 'primary',
@@ -64,7 +99,8 @@ const Theme = () => {
             </Button>
           ))}
         </Stack>
-        <Typography variant="h6">Icon Buttons</Typography>
+
+        <Typography variant="h4">Icon Buttons</Typography>
         <Stack direction="row" spacing={1}>
           {[
             'primary' as 'primary',
@@ -80,7 +116,23 @@ const Theme = () => {
             </IconButton>
           ))}
         </Stack>
-        <Typography variant="h6">Custom Chips</Typography>
+        <Stack direction="row" spacing={1}>
+          {[
+            'primary' as 'primary',
+            'secondary' as 'secondary',
+            'default' as 'default',
+            'error' as 'error',
+            'info' as 'info',
+            'success' as 'success',
+            'warning' as 'warning'
+          ].map(val => (
+            <IconButton color={val}>
+              <AddPhotoAlternateOutlinedIcon />
+            </IconButton>
+          ))}
+        </Stack>
+
+        <Typography variant="h4">Custom Chips</Typography>
         <Stack direction="row" spacing={1}>
           {[
             'primary' as 'primary',
@@ -146,7 +198,8 @@ const Theme = () => {
             <CustomChip variant="outlined" type="square" color={val} label={val} />
           ))}
         </Stack>
-        <Typography variant="h6">Classifications</Typography>
+
+        <Typography variant="h4">Classifications</Typography>
         <Stack direction="row" spacing={1}>
           {['TLP:C', 'TLP:G', 'TLP:A'].map(val => (
             <Classification type="outlined" format="long" c12n={val} />
@@ -167,13 +220,84 @@ const Theme = () => {
             <Classification type="text" format="long" c12n={val} />
           ))}
         </Stack>
-        <Typography variant="h6">Forms</Typography>
+
+        <Typography variant="h4">Forms</Typography>
         <Stack direction="row" spacing={1}>
           {['outlined' as 'outlined', 'standard' as 'standard', 'filled' as 'filled'].map(val => (
             <TextField variant={val} size="small" placeholder={val} />
           ))}
         </Stack>
-        <Typography variant="h6">Tables</Typography>
+        <Stack direction="row" spacing={1} sx={{ width: '100%' }}>
+          {['outlined' as 'outlined', 'standard' as 'standard', 'filled' as 'filled'].map(val => (
+            <FormControl fullWidth>
+              <InputLabel>{val}</InputLabel>
+              <Select label={val} variant={val} fullWidth>
+                <MenuItem value={1}>Value 1</MenuItem>
+                <MenuItem value={2}>Value 1</MenuItem>
+                <MenuItem value={3}>Value 1</MenuItem>
+              </Select>
+            </FormControl>
+          ))}
+        </Stack>
+        <Box sx={{ width: '100%' }}>
+          <Divider />
+        </Box>
+        <CircularProgress variant="indeterminate" />
+        <Box sx={{ width: '100%' }}>
+          <LinearProgress variant="indeterminate" />
+        </Box>
+        <Stack direction="row" spacing={1} sx={{ width: '100%' }}>
+          <Slider defaultValue={30} />
+          <Slider disabled defaultValue={20} />
+        </Stack>
+        <Skeleton sx={{ width: '100%' }}></Skeleton>
+        <Stack direction="row" spacing={1}>
+          {[
+            'primary' as 'primary',
+            'secondary' as 'secondary',
+            'default' as 'default',
+            'error' as 'error',
+            'info' as 'info',
+            'success' as 'success',
+            'warning' as 'warning'
+          ].map(val => (
+            <Switch color={val} defaultChecked />
+          ))}
+          <Switch />
+        </Stack>
+        <Stack direction="row" spacing={1}>
+          {[
+            'primary' as 'primary',
+            'secondary' as 'secondary',
+            'default' as 'default',
+            'error' as 'error',
+            'info' as 'info',
+            'success' as 'success',
+            'warning' as 'warning'
+          ].map(val => (
+            <Switch color={val} defaultChecked disabled />
+          ))}
+          <Switch disabled />
+        </Stack>
+        <Stack direction="row" spacing={1}>
+          <Checkbox defaultChecked />
+          <Checkbox />
+          <Checkbox disabled />
+          <Checkbox checked />
+        </Stack>
+        <FormControl>
+          <FormLabel>Radio Title</FormLabel>
+          <RadioGroup defaultValue="radio 2" name="radio-buttons-group">
+            {['radio 1', 'radio 2', 'radio 3'].map(val => (
+              <FormControlLabel value={val} control={<Radio />} label={val} />
+            ))}
+            <FormControlLabel disabled value="disabled" control={<Radio />} label="disabled" />
+          </RadioGroup>
+        </FormControl>
+
+        <Rating name="simple-controlled" value={2} />
+
+        <Typography variant="h4">Tables</Typography>
         <TableContainer component={Paper}>
           <DivTable>
             <DivTableHead>
@@ -210,6 +334,33 @@ const Theme = () => {
             </DivTableBody>
           </DivTable>
         </TableContainer>
+
+        <Typography variant="h4">Alerts</Typography>
+        <Stack direction="row" spacing={1}>
+          {['error' as 'error', 'info' as 'info', 'success' as 'success', 'warning' as 'warning'].map(val => (
+            <Alert severity={val}>
+              <AlertTitle>{val}</AlertTitle>
+              This is a {val} Alert with a title.
+            </Alert>
+          ))}
+        </Stack>
+
+        <Typography variant="h4">Containers</Typography>
+        <Stack direction="row" spacing={1} sx={{ width: '100%' }}>
+          <Card sx={{ width: '100%', height: '100px' }}>Card</Card>
+          <Paper sx={{ width: '100%', height: '100px' }}>Paper</Paper>
+        </Stack>
+
+        <Typography variant="h4">Navigation</Typography>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={2} aria-label="basic tabs example">
+            <Tab label="Item One" value={1} />
+            <Tab label="Item Two" value={2} />
+            <Tab label="Item Three" value={3} />
+          </Tabs>
+        </Box>
+
+        <Typography variant="h4">Assemblyline results</Typography>
       </Stack>
     </PageCenter>
   );
