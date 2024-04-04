5216c968a679332941218d059310beb2a74d603d
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2024-04-04 20:52:05 +0000

    Cleaned the List

--- src/components/routes/alerts2/components/List.tsx
+++ src/components/routes/alerts2/components/List.tsx
@@ -1,49 +1,15 @@
 import { AlertTitle, useMediaQuery, useTheme } from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
 import SimpleList from 'commons/addons/lists/simplelist/SimpleList';
-import useAppUser from 'commons/components/app/hooks/useAppUser';
-import useALContext from 'components/hooks/useALContext';
 import useMyAPI from 'components/hooks/useMyAPI';
-import { ALField, CustomUser } from 'components/hooks/useMyUser';
 import { DEFAULT_QUERY } from 'components/routes/alerts';
 import InformativeAlert from 'components/visual/InformativeAlert';
-import SearchQuery, { SearchFilter } from 'components/visual/SearchBar/search-query';
 import SimpleSearchQuery from 'components/visual/SearchBar/simple-search-query';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Alert, AlertItem } from '../models/Alert';
 import { AlertActions } from './Actions';
 import AlertListItem from './ListItem';
-
-const useStyles = makeStyles(theme => ({
-  desc: {
-    transform: 'rotate(0deg)',
-    transition: theme.transitions.create('transform', {
-      easing: theme.transitions.easing.easeInOut,
-      duration: theme.transitions.duration.shortest
-    })
-  },
-  asc: {
-    transform: 'rotate(180deg)'
-  },
-  actions: {
-    display: 'flex',
-    gap: theme.spacing(1),
-    justifyContent: 'flex-end',
-    flexWrap: 'wrap',
-    marginTop: theme.spacing(1)
-  },
-  drawerInner: {
-    display: 'flex',
-    flexDirection: 'column',
-    padding: theme.spacing(3),
-    width: '600px',
-    [theme.breakpoints.only('xs')]: {
-      width: '100vw'
-    }
-  }
-}));
 
 type ListResponse = {
   items: AlertItem[];
@@ -62,38 +28,26 @@ type GroupedResponse = {
 };
 
 export const ALERT_SIMPLELIST_ID = 'al.alerts.simplelist';
+
 const PAGE_SIZE = 50;
 
-type Props = {};
-
-export const WrappedALertList = ({}: Props) => {
-  const { t } = useTranslation('favorites');
-  const classes = useStyles();
+export const WrappedALertList = () => {
+  const { t } = useTranslation('alerts');
   const theme = useTheme();
   const navigate = useNavigate();
   const location = useLocation();
   const { apiCall } = useMyAPI();
-  const { c12nDef } = useALContext();
-  const { user: currentUser } = useAppUser<CustomUser>();
 
-  const isMDUp = useMediaQuery(theme.breakpoints.up('md'));
   const isLGDown = useMediaQuery(theme.breakpoints.down('lg'));
 
-  const { indexes: fieldIndexes } = useALContext();
-  const [searchQuery, setSearchQuery] = useState<SearchQuery>(null);
-  const [fields, setFields] = useState<ALField[]>([]);
-  const [statusFilters, setStatusFilters] = useState<SearchFilter[]>([]);
-  const [priorityFilters, setPriorityFilters] = useState<SearchFilter[]>([]);
-  const [labelFilters, setLabelFilters] = useState<SearchFilter[]>([]);
-  const [valueFilters, setValueFilters] = useState<SearchFilter[]>([]);
-  // const [state, setState] = useReducer(alertStateReducer, DEFAULT_STATE);
-  const [search, setSearch] = useState<string>(null);
-
   const [alerts, setAlerts] = useState<Alert[]>([]);
-  const [total, setTotal] = useState<number>(0);
   const [countedTotal, setCountedTotal] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
   const [scrollReset, setScrollReset] = useState<boolean>(false);
+
+  const nextOffset = useRef<number>(0);
+  const executionTime = useRef<string>('');
+  const loadingRef = useRef<boolean>(false);
 
   const buildSearchQuery = useCallback(
     (singles: string[] = [], multiples: string[] = []): SimpleSearchQuery => {
@@ -117,25 +71,42 @@ export const WrappedALertList = ({}: Props) => {
     [location.search]
   );
 
-  const handleListFetch = useCallback(
-    (props: { offset?: number; rows?: number; tcStart?: string }) => {
-      const { offset = 0, rows = PAGE_SIZE, tcStart = null } = props;
+  const handleFetch = useCallback(
+    (search: string) => {
+      if (loadingRef.current) return;
+      loadingRef.current = true;
 
+      const current = new SimpleSearchQuery(search, DEFAULT_QUERY);
+      const params = current.getParams();
+      const groupBy = (
+        current.has('group_by') && current.get('group_by', '') === '' ? '' : 'group_by' in params ? params.group_by : ''
+      ) as string;
+
+      const pathname = groupBy !== '' ? `/api/v4/alert/grouped/${groupBy}/` : `/api/v4/alert/list/`;
       const query = buildSearchQuery(['q', 'no_delay', 'sort', 'tc', 'track_total_hits'], ['fq']);
 
-      query.set('offset', offset);
-      query.set('rows', rows);
-      tcStart && query.set('tc_start', tcStart);
+      query.set('offset', nextOffset.current);
+      query.set('rows', PAGE_SIZE);
+      executionTime.current && query.set('tc_start', executionTime.current);
 
       apiCall({
-        url: `/api/v4/alert/list/?${query.toString()}`,
+        url: `${pathname}?${query.toString()}`,
         method: 'GET',
-        onSuccess: ({ api_response }: { api_response: ListResponse }) => {
+        onSuccess: ({ api_response }: { api_response: ListResponse | GroupedResponse }) => {
+          setCountedTotal('counted_total' in api_response ? api_response.counted_total : api_response.items.length);
+
+          if ('tc_start' in api_response) {
+            executionTime.current = api_response.tc_start;
+          } else if (!executionTime.current && api_response.items.length > 0) {
+            executionTime.current = api_response.items[0].reporting_ts;
+          }
+
+          nextOffset.current = api_response.offset + api_response.rows;
+
           setAlerts(values => [
-            ...values.filter(value => value.index < offset),
-            ...api_response.items.map((item, i) => ({ ...item, id: item.alert_id, index: offset + i }))
+            ...values.filter(value => value.index < nextOffset.current),
+            ...api_response.items.map((item, i) => ({ ...item, id: item.alert_id, index: nextOffset.current + i }))
           ]);
-          setTotal(api_response.total);
         },
         onEnter: () => {
           setLoading(true);
@@ -143,50 +114,13 @@ export const WrappedALertList = ({}: Props) => {
         onExit: () => {
           setLoading(false);
           setScrollReset(false);
+          loadingRef.current = false;
         }
       });
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [buildSearchQuery]
+    [buildSearchQuery, location.search]
   );
-
-  const handleGroupFetch = useCallback(
-    (props: { offset?: number; rows?: number; groupBy?: string; tcStart?: string }) => {
-      const { offset = 0, rows = PAGE_SIZE, groupBy = null, tcStart = null } = props;
-
-      const query = buildSearchQuery(['q', 'no_delay', 'sort', 'tc', 'track_total_hits'], ['fq']);
-
-      query.set('offset', offset);
-      query.set('rows', rows);
-      tcStart && query.set('tc_start', tcStart);
-
-      apiCall({
-        url: `/api/v4/alert/grouped/${groupBy}/?${query.toString()}`,
-        method: 'GET',
-        onSuccess: ({ api_response }: { api_response: GroupedResponse }) => {
-          setAlerts(values => [
-            ...values.filter(value => value.index < offset),
-            ...api_response.items.map((item, i) => ({ ...item, id: item.alert_id, index: offset + i }))
-          ]);
-          setTotal(api_response.total);
-          setCountedTotal(api_response.counted_total);
-        },
-        onEnter: () => {
-          setLoading(true);
-        },
-        onExit: () => {
-          setLoading(false);
-          setScrollReset(false);
-        }
-      });
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [buildSearchQuery]
-  );
-
-  const handleFetchMore = useCallback(() => {
-    console.log('fetch mroe');
-  }, []);
 
   const handleSelectedItemChange = useCallback(
     (item: Alert, index?: number) => {
@@ -199,24 +133,19 @@ export const WrappedALertList = ({}: Props) => {
   );
 
   useEffect(() => {
-    const query = new SimpleSearchQuery(location.search, DEFAULT_QUERY);
-    const params = query.getParams();
-    const groupBy = (
-      query.has('group_by') && query.get('group_by', '') === '' ? '' : 'group_by' in params ? params.group_by : ''
-    ) as string;
-
-    groupBy !== ''
-      ? handleGroupFetch({ offset: 0, rows: PAGE_SIZE, groupBy: groupBy })
-      : handleListFetch({ offset: 0, rows: PAGE_SIZE });
-
+    nextOffset.current = 0;
+    executionTime.current = null;
     setScrollReset(true);
-  }, [handleGroupFetch, handleListFetch, location.search]);
+    setAlerts([]);
+
+    handleFetch(location.search);
+  }, [handleFetch, location.search]);
 
   return (
     <SimpleList
       id={ALERT_SIMPLELIST_ID}
       disableProgress
-      scrollInfinite={countedTotal < total}
+      scrollInfinite={countedTotal > 0}
       scrollReset={scrollReset}
       scrollLoadNextThreshold={75}
       scrollTargetId="app-scrollct"
@@ -230,7 +159,7 @@ export const WrappedALertList = ({}: Props) => {
           </InformativeAlert>
         </div>
       }
-      onLoadNext={() => handleFetchMore()}
+      onLoadNext={() => handleFetch(location.search)}
       onCursorChange={handleSelectedItemChange}
       onItemSelected={handleSelectedItemChange}
       onRenderActions={(item: Alert, index?: number) => <AlertActions />}
