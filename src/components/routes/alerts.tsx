7622a5415878bcdc0a7c214040d12d0630be518c
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2024-04-05 08:29:06 +0000

    Changes to the Alerts page

--- src/components/routes/alerts.tsx
+++ src/components/routes/alerts.tsx
@@ -1,28 +1,35 @@
-import { Box, Divider, Grid, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { AlertTitle, Divider, Grid, Typography, useMediaQuery, useTheme } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
+import SimpleList from 'commons/addons/lists/simplelist/SimpleList';
 import useAppUser from 'commons/components/app/hooks/useAppUser';
 import PageFullWidth from 'commons/components/pages/PageFullWidth';
 import PageHeader from 'commons/components/pages/PageHeader';
 import useALContext from 'components/hooks/useALContext';
 import useDrawer from 'components/hooks/useDrawer';
+import useMyAPI from 'components/hooks/useMyAPI';
 import useMySnackbar from 'components/hooks/useMySnackbar';
 import { CustomUser } from 'components/hooks/useMyUser';
+import InformativeAlert from 'components/visual/InformativeAlert';
 import SearchBar from 'components/visual/SearchBar/search-bar';
 import SearchQuery from 'components/visual/SearchBar/search-query';
 import { DEFAULT_SUGGESTION } from 'components/visual/SearchBar/search-textfield';
-import 'moment/locale/fr';
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import SimpleSearchQuery from 'components/visual/SearchBar/simple-search-query';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
 import { useLocation } from 'react-router-dom';
 import ForbiddenPage from './403';
+import AlertActions from './alerts2/components/Actions';
 import AlertDefaultSearchParameters from './alerts2/components/DefaultSearchParameters';
 import { AlertFavorites } from './alerts2/components/Favorites';
 import AlertFilters from './alerts2/components/Filters';
-import ALertList from './alerts2/components/List';
+import AlertListItem from './alerts2/components/ListItem';
+import { AlertSearchResults } from './alerts2/components/Results';
 import { AlertSorts } from './alerts2/components/Sorts';
 import AlertWorkflows from './alerts2/components/Workflows';
 import AlertDetail2 from './alerts2/detail';
+import { Alert, AlertItem } from './alerts2/models/Alert';
+import { getGroupBy } from './alerts2/utils/buildSearchQuery';
 
 const useStyles = makeStyles(theme => ({
   pageTitle: {
@@ -36,11 +43,6 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.only('xs')]: {
       width: '100vw'
     }
-  },
-  searchresult: {
-    marginTop: theme.spacing(1),
-    fontStyle: 'italic',
-    minHeight: theme.spacing(3)
   },
   modeToggler: {
     border: 'none',
@@ -76,6 +78,24 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+type ListResponse = {
+  items: AlertItem[];
+  offset: number;
+  rows: number;
+  total: number;
+};
+
+type GroupedResponse = {
+  counted_total: number;
+  items: AlertItem[];
+  offset: number;
+  rows: number;
+  tc_start: string;
+  total: number;
+};
+
+export const ALERT_SIMPLELIST_ID = 'al.alerts.simplelist';
+
 export const DEFAULT_PARAMS = {
   offset: 0,
   rows: 50,
@@ -94,15 +114,31 @@ const WrappedAlertsPage = () => {
   const theme = useTheme();
   const location = useLocation();
   const navigate = useNavigate();
+  const { apiCall } = useMyAPI();
   const { indexes } = useALContext();
   const { user: currentUser } = useAppUser<CustomUser>();
   const { showSuccessMessage } = useMySnackbar();
   const { globalDrawerOpened, setGlobalDrawer } = useDrawer();
 
   const [searchQuery, setSearchQuery] = useState<SearchQuery>(null);
+  const [alerts, setAlerts] = useState<Alert[]>([]);
+  const [countedTotal, setCountedTotal] = useState<number>(0);
+  const [total, setTotal] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
+  const [scrollReset, setScrollReset] = useState<boolean>(false);
 
+  const queryRef = useRef<string>('');
+  const nextOffset = useRef<number>(0);
+  const executionTime = useRef<string>('');
+  const loadingRef = useRef<boolean>(false);
+
+  const isLGDown = useMediaQuery(theme.breakpoints.down('lg'));
   const upMD = useMediaQuery(theme.breakpoints.up('md'));
+
+  const query = useMemo<SimpleSearchQuery>(
+    () => new SimpleSearchQuery(location.search, DEFAULT_QUERY),
+    [location.search]
+  );
 
   const suggestions = useMemo<string[]>(
     () =>
@@ -112,11 +148,107 @@ const WrappedAlertsPage = () => {
     [indexes]
   );
 
-  const handleClear = useCallback(() => {}, []);
+  const buildSearchQuery = useCallback(
+    (singles: string[] = [], multiples: string[] = []): SimpleSearchQuery => {
+      const defaults = new SimpleSearchQuery(DEFAULT_QUERY);
+      const current = new SimpleSearchQuery(location.search);
+      const newQuery = new SimpleSearchQuery('');
 
-  const handleValueChange = useCallback(() => {}, []);
+      singles.forEach(key => {
+        const value = current.get(key);
+        const other = defaults.get(key);
+        if (value && value !== '') newQuery.set(key, value);
+        else if (!current.has(key) && other && other !== '') newQuery.set(key, other);
+      });
 
-  const handleSearch = useCallback(() => {}, []);
+      multiples.forEach(key => {
+        [...defaults.getAll(key, []), ...current.getAll(key, [])].forEach(value => newQuery.add(key, value));
+      });
+
+      return newQuery;
+    },
+    [location.search]
+  );
+
+  const handleClear = useCallback(() => {
+    queryRef.current = '';
+    query.delete('q');
+    navigate(`${location.pathname}?${query.getDeltaString()}${location.hash}`);
+  }, [location.hash, location.pathname, navigate, query]);
+
+  const handleValueChange = (inputValue: string) => {
+    queryRef.current = inputValue;
+  };
+
+  const handleSearch = useCallback(
+    (filterValue: string = '', inputEl: HTMLInputElement = null) => {
+      if (queryRef.current !== '') {
+        query.set('q', queryRef.current);
+        navigate(`${location.pathname}?${query.getDeltaString()}${location.hash}`);
+      } else {
+        handleClear();
+      }
+      if (inputEl) inputEl.focus();
+    },
+    [handleClear, location.hash, location.pathname, navigate, query]
+  );
+
+  const handleFetch = useCallback(
+    (search: string) => {
+      if (loadingRef.current) return;
+      loadingRef.current = true;
+
+      const groupBy = getGroupBy(search, DEFAULT_QUERY);
+      const pathname = groupBy !== '' ? `/api/v4/alert/grouped/${groupBy}/` : `/api/v4/alert/list/`;
+      const newQuery = buildSearchQuery(['q', 'no_delay', 'sort', 'tc', 'track_total_hits'], ['fq']);
+
+      newQuery.set('offset', nextOffset.current);
+      newQuery.set('rows', DEFAULT_PARAMS.rows);
+      executionTime.current && newQuery.set('tc_start', executionTime.current);
+
+      apiCall({
+        url: `${pathname}?${newQuery.toString()}`,
+        method: 'GET',
+        onSuccess: ({ api_response }: { api_response: ListResponse | GroupedResponse }) => {
+          setCountedTotal('counted_total' in api_response ? api_response.counted_total : api_response.items.length);
+          setTotal(api_response.total);
+
+          if ('tc_start' in api_response) {
+            executionTime.current = api_response.tc_start;
+          } else if (!executionTime.current && api_response.items.length > 0) {
+            executionTime.current = api_response.items[0].reporting_ts;
+          }
+
+          nextOffset.current = api_response.offset + api_response.rows;
+
+          setAlerts(values => [
+            ...values.filter(value => value.index < nextOffset.current),
+            ...api_response.items.map((item, i) => ({ ...item, id: item.alert_id, index: nextOffset.current + i }))
+          ]);
+        },
+        onEnter: () => {
+          setLoading(true);
+        },
+        onExit: () => {
+          setLoading(false);
+          setScrollReset(false);
+          loadingRef.current = false;
+        }
+      });
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [buildSearchQuery, location.search]
+  );
+
+  const handleSelectedItemChange = useCallback(
+    (item: Alert, index?: number) => {
+      if (!item) return;
+      // Unfocus the simple list so the drawer does not try to refocus it when closing...
+      if (isLGDown) document.getElementById(ALERT_SIMPLELIST_ID).blur();
+      navigate(`${location.pathname}${location.search}#${item.alert_id}`);
+    },
+    [isLGDown, location.pathname, location.search, navigate]
+  );
 
   useEffect(() => {
     if (!globalDrawerOpened && location.hash) {
@@ -130,6 +262,31 @@ const WrappedAlertsPage = () => {
       setGlobalDrawer(<AlertDetail2 id={location.hash.substr(1)} inDrawer />, { hasMaximize: true });
     }
   }, [location.hash, setGlobalDrawer]);
+
+  useEffect(() => {
+    nextOffset.current = 0;
+    executionTime.current = null;
+    setScrollReset(true);
+    setAlerts([]);
+
+    handleFetch(location.search);
+  }, [handleFetch, location.search]);
+
+  useEffect(() => {
+    const update = ({ detail }: CustomEvent<Alert>) => {
+      setAlerts(values => {
+        const index = values.findIndex(value => value.alert_id === detail.alert_id);
+        return index >= 0
+          ? [...values.slice(0, index), { ...values[index], ...detail }, ...values.slice(index + 1, values.length)]
+          : values;
+      });
+    };
+
+    window.addEventListener('alertUpdate', update);
+    return () => {
+      window.removeEventListener('alertUpdate', update);
+    };
+  }, []);
 
   if (!currentUser.roles.includes('alert_view')) return <ForbiddenPage />;
   else
@@ -147,7 +304,7 @@ const WrappedAlertsPage = () => {
         <PageHeader isSticky>
           <div style={{ paddingTop: theme.spacing(1) }}>
             <SearchBar
-              initValue={searchQuery ? searchQuery.getQuery() : ''}
+              initValue={query.get('q', '')}
               searching={loading}
               suggestions={suggestions}
               placeholder={t('search.placeholder')}
@@ -169,23 +326,35 @@ const WrappedAlertsPage = () => {
                 </>
               }
             >
-              <Box className={classes.searchresult}>
-                {/* {isMDUp ? (
-                  <SearchResultLarge
-                    searching={loading}
-                    total={total}
-                    query={searchQuery}
-                    onApplyFilters={onApplyFilters}
-                  />
-                ) : (
-                  <SearchResultSmall searching={loading} total={total} query={searchQuery} />
-                )} */}
-              </Box>
+              <AlertSearchResults searching={loading} total={total} />
             </SearchBar>
           </div>
         </PageHeader>
 
-        <ALertList />
+        <SimpleList
+          id={ALERT_SIMPLELIST_ID}
+          disableProgress
+          scrollInfinite={countedTotal > 0 && countedTotal < total}
+          scrollReset={scrollReset}
+          scrollLoadNextThreshold={75}
+          scrollTargetId="app-scrollct"
+          loading={loading}
+          items={alerts}
+          emptyValue={
+            <div style={{ width: '100%' }}>
+              <InformativeAlert>
+                <AlertTitle>{t('no_alerts_title')}</AlertTitle>
+                {t('no_alerts_desc')}
+              </InformativeAlert>
+            </div>
+          }
+          onLoadNext={() => handleFetch(location.search)}
+          onCursorChange={handleSelectedItemChange}
+          onItemSelected={handleSelectedItemChange}
+          onRenderActions={(item: Alert, index?: number) => <AlertActions alert={item} />}
+        >
+          {(item: Alert) => <AlertListItem item={item} />}
+        </SimpleList>
       </PageFullWidth>
     );
 };
