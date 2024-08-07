42f91e5abf6915bc8ee9a42d22477284a3caf938
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2024-08-07 15:52:53 +0000

    In the event of an internal server error on file upload, cancel upload and show error message

--- src/components/routes/submit.tsx
+++ src/components/routes/submit.tsx
@@ -149,6 +149,10 @@ const Submit: React.FC<any> = () => {
               api_data.api_error_message.includes('API'))
           ) {
             window.location.reload();
+          } else if (data.api_status_code == 500) {
+            // Internal server error occurred, cancel upload and show error message
+            cancelUpload();
+            showErrorMessage(t('submit.file.upload_fail'));
           }
         }
       } catch (ex) {
