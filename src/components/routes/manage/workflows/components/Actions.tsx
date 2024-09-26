ab6f97273a45d6b5344d0b63762ab35bfa652437
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2024-09-26 14:00:16 +0000

    Added the new implementation of the workflows components

---
+++ src/components/routes/manage/workflows/components/Actions.tsx
@@ -0,0 +1,291 @@
+import ControlPointDuplicateOutlinedIcon from '@mui/icons-material/ControlPointDuplicateOutlined';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import RemoveCircleOutlineOutlinedIcon from '@mui/icons-material/RemoveCircleOutlineOutlined';
+import ToggleOffOutlinedIcon from '@mui/icons-material/ToggleOffOutlined';
+import ToggleOnIcon from '@mui/icons-material/ToggleOn';
+import YoutubeSearchedForIcon from '@mui/icons-material/YoutubeSearchedFor';
+import { IconButton, Skeleton, Tooltip, useTheme } from '@mui/material';
+import useALContext from 'components/hooks/useALContext';
+import useMyAPI from 'components/hooks/useMyAPI';
+import useMySnackbar from 'components/hooks/useMySnackbar';
+import type { Workflow } from 'components/models/base/workflow';
+import ConfirmationDialog from 'components/visual/ConfirmationDialog';
+import React, { useCallback, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+
+type ShowRelatedAlertsActionProps = {
+  id: string;
+  workflow: Workflow;
+};
+
+export const ShowRelatedAlertsAction: React.FC<ShowRelatedAlertsActionProps> = React.memo(
+  ({ id = null, workflow = null }) => {
+    const { t } = useTranslation(['manageWorkflowDetail']);
+    const theme = useTheme();
+    const { user: currentUser } = useALContext();
+
+    if (!id || !currentUser.roles.includes('alert_view')) return null;
+    else if (!workflow)
+      return <Skeleton variant="circular" height="2.5rem" width="2.5rem" style={{ margin: theme.spacing(0.5) }} />;
+    else
+      return (
+        <Tooltip title={t('usage')}>
+          <div>
+            <IconButton
+              component={Link}
+              to={`/alerts/?q=events.entity_id:${id}`}
+              size="large"
+              style={{ color: theme.palette.action.active }}
+            >
+              <YoutubeSearchedForIcon />
+            </IconButton>
+          </div>
+        </Tooltip>
+      );
+  }
+);
+
+type DuplicateWorkflowActionProps = {
+  id: string;
+  workflow: Workflow;
+};
+
+export const DuplicateWorkflowAction: React.FC<DuplicateWorkflowActionProps> = React.memo(
+  ({ id = null, workflow = null }) => {
+    const { t } = useTranslation(['manageWorkflowDetail']);
+    const theme = useTheme();
+    const { user: currentUser } = useALContext();
+
+    const to = useMemo<string>(() => {
+      const query = new URLSearchParams(
+        !workflow
+          ? ''
+          : [
+              ['classification', `${workflow.classification}`],
+              ['enabled', `${workflow.enabled}`],
+              ...workflow.labels.map(label => ['labels', `${label}`]),
+              ['name', `${workflow.name}`],
+              ['priority', `${workflow.priority}`],
+              ['query', `${workflow.query}`],
+              ['status', `${workflow.status}`]
+            ]
+      );
+      return `${location.pathname}${location.search}#/create/?${query.toString()}`;
+    }, [workflow]);
+
+    if (!id || !currentUser.roles.includes('workflow_manage')) return null;
+    else if (!workflow)
+      return <Skeleton variant="circular" height="2.5rem" width="2.5rem" style={{ margin: theme.spacing(0.5) }} />;
+    else
+      return (
+        <Tooltip title={t('duplicate')}>
+          <div>
+            <IconButton
+              style={{
+                color: theme.palette.mode === 'dark' ? theme.palette.success.light : theme.palette.success.dark
+              }}
+              component={Link}
+              to={to}
+              size="large"
+            >
+              <ControlPointDuplicateOutlinedIcon />
+            </IconButton>
+          </div>
+        </Tooltip>
+      );
+  }
+);
+
+type EditWorkflowActionProps = {
+  id: string;
+  workflow: Workflow;
+};
+
+export const EditWorkflowAction: React.FC<EditWorkflowActionProps> = React.memo(({ id = null, workflow = null }) => {
+  const { t } = useTranslation(['manageWorkflowDetail']);
+  const theme = useTheme();
+  const { user: currentUser } = useALContext();
+
+  if (!id || !currentUser.roles.includes('workflow_manage')) return null;
+  else if (!workflow)
+    return <Skeleton variant="circular" height="2.5rem" width="2.5rem" style={{ margin: theme.spacing(0.5) }} />;
+  else
+    return (
+      <Tooltip title={t('edit')}>
+        <div>
+          <IconButton
+            style={{ color: theme.palette.mode === 'dark' ? theme.palette.primary.light : theme.palette.primary.dark }}
+            component={Link}
+            to={`${location.pathname}${location.search}#/create/${id}`}
+            size="large"
+          >
+            <EditOutlinedIcon />
+          </IconButton>
+        </div>
+      </Tooltip>
+    );
+});
+
+type EnableWorkflowActionProps = {
+  id: string;
+  workflow: Workflow;
+  onChange: (enabled: boolean) => void;
+};
+
+export const EnableWorkflowAction: React.FC<EnableWorkflowActionProps> = React.memo(
+  ({ id = null, workflow = null, onChange = () => null }) => {
+    const { t } = useTranslation(['manageWorkflowDetail']);
+    const theme = useTheme();
+    const { apiCall } = useMyAPI();
+    const { user: currentUser } = useALContext();
+    const { showSuccessMessage } = useMySnackbar();
+
+    const [enableDialog, setEnableDialog] = useState<boolean>(false);
+    const [disableDialog, setDisableDialog] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const handleWorkflowEnable = useCallback(() => {
+      if (!currentUser.roles.includes('workflow_manage')) return;
+      apiCall({
+        url: `/api/v4/workflow/enable/${id}/`,
+        method: 'PUT',
+        body: { enabled: true },
+        onSuccess: () => {
+          setEnableDialog(false);
+          showSuccessMessage(t('enable.success'));
+          setTimeout(() => window.dispatchEvent(new CustomEvent('reloadWorkflows')), 1000);
+          onChange(true);
+        },
+        onEnter: () => setLoading(true),
+        onExit: () => setLoading(false)
+      });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentUser.roles, id, onChange]);
+
+    const handleWorkflowDisable = useCallback(() => {
+      if (!currentUser.roles.includes('workflow_manage')) return;
+      apiCall({
+        url: `/api/v4/workflow/enable/${id}/`,
+        method: 'PUT',
+        body: { enabled: false },
+        onSuccess: () => {
+          setDisableDialog(false);
+          showSuccessMessage(t('disable.success'));
+          setTimeout(() => window.dispatchEvent(new CustomEvent('reloadWorkflows')), 1000);
+          onChange(false);
+        },
+        onEnter: () => setLoading(true),
+        onExit: () => setLoading(false)
+      });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentUser.roles, id, onChange]);
+
+    if (!id || !currentUser.roles.includes('workflow_manage')) return null;
+    else if (!workflow)
+      return <Skeleton variant="circular" height="2.5rem" width="2.5rem" style={{ margin: theme.spacing(0.5) }} />;
+    else
+      return (
+        <>
+          <Tooltip title={workflow.enabled ? t('enabled') : t('disabled')}>
+            <div>
+              <IconButton
+                style={{ color: theme.palette.text.primary }}
+                onClick={workflow.enabled ? () => setDisableDialog(true) : () => setEnableDialog(true)}
+                size="large"
+              >
+                {workflow.enabled ? <ToggleOnIcon /> : <ToggleOffOutlinedIcon />}
+              </IconButton>
+            </div>
+          </Tooltip>
+          <ConfirmationDialog
+            open={disableDialog}
+            handleClose={() => setDisableDialog(false)}
+            handleAccept={handleWorkflowDisable}
+            title={t('disable.title')}
+            cancelText={t('disable.cancelText')}
+            acceptText={t('disable.acceptText')}
+            text={t('disable.text')}
+            waiting={loading}
+          />
+          <ConfirmationDialog
+            open={enableDialog}
+            handleClose={() => setEnableDialog(false)}
+            handleAccept={handleWorkflowEnable}
+            title={t('enable.title')}
+            cancelText={t('enable.cancelText')}
+            acceptText={t('enable.acceptText')}
+            text={t('enable.text')}
+            waiting={loading}
+          />
+        </>
+      );
+  }
+);
+
+type DeleteWorkflowActionProps = {
+  id: string;
+  workflow: Workflow;
+};
+
+export const DeleteWorkflowAction: React.FC<DeleteWorkflowActionProps> = React.memo(
+  ({ id = null, workflow = null }) => {
+    const { t } = useTranslation(['manageWorkflowDetail']);
+    const theme = useTheme();
+    const location = useLocation();
+    const navigate = useNavigate();
+    const { apiCall } = useMyAPI();
+    const { user: currentUser } = useALContext();
+    const { showSuccessMessage } = useMySnackbar();
+
+    const [deleteDialog, setDeleteDialog] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const handleRemoveWorkflow = useCallback(() => {
+      if (!currentUser.roles.includes('workflow_manage')) return;
+      apiCall({
+        url: `/api/v4/workflow/${id}/`,
+        method: 'DELETE',
+        onSuccess: () => {
+          setDeleteDialog(false);
+          showSuccessMessage(t('delete.success'));
+          navigate(`/manage/workflows${location.search}`);
+          setTimeout(() => window.dispatchEvent(new CustomEvent('reloadWorkflows')), 1000);
+        },
+        onEnter: () => setLoading(true),
+        onExit: () => setLoading(false)
+      });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentUser.roles, id, location.search]);
+
+    if (!id || !currentUser.roles.includes('workflow_manage')) return null;
+    else if (!workflow)
+      return <Skeleton variant="circular" height="2.5rem" width="2.5rem" style={{ margin: theme.spacing(0.5) }} />;
+    else
+      return (
+        <>
+          <Tooltip title={t('remove')}>
+            <div>
+              <IconButton
+                style={{ color: theme.palette.mode === 'dark' ? theme.palette.error.light : theme.palette.error.dark }}
+                onClick={() => setDeleteDialog(true)}
+                size="large"
+              >
+                <RemoveCircleOutlineOutlinedIcon />
+              </IconButton>
+            </div>
+          </Tooltip>
+          <ConfirmationDialog
+            open={deleteDialog}
+            handleClose={() => setDeleteDialog(false)}
+            handleAccept={handleRemoveWorkflow}
+            title={t('delete.title')}
+            cancelText={t('delete.cancelText')}
+            acceptText={t('delete.acceptText')}
+            text={t('delete.text')}
+            waiting={loading}
+          />
+        </>
+      );
+  }
+);
---
+++ src/components/routes/manage/workflows/index.tsx
@@ -0,0 +1,208 @@
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import EventBusyOutlinedIcon from '@mui/icons-material/EventBusyOutlined';
+import EventOutlinedIcon from '@mui/icons-material/EventOutlined';
+import { Grid, IconButton, Tooltip, useTheme } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import useAppUser from 'commons/components/app/hooks/useAppUser';
+import PageFullWidth from 'commons/components/pages/PageFullWidth';
+import PageHeader from 'commons/components/pages/PageHeader';
+import useALContext from 'components/hooks/useALContext';
+import useDrawer from 'components/hooks/useDrawer';
+import useMyAPI from 'components/hooks/useMyAPI';
+import type { WorkflowIndexed } from 'components/models/base/workflow';
+import type { CustomUser } from 'components/models/ui/user';
+import ForbiddenPage from 'components/routes/403';
+import SearchHeader from 'components/visual/SearchBar/SearchHeader';
+import type { SearchParams } from 'components/visual/SearchBar/SearchParams';
+import { createSearchParams } from 'components/visual/SearchBar/SearchParams';
+import { SearchParamsProvider, useSearchParams } from 'components/visual/SearchBar/SearchParamsContext';
+import type { SearchParamsResult } from 'components/visual/SearchBar/SearchParser';
+import { DEFAULT_SUGGESTION } from 'components/visual/SearchBar/search-textfield';
+import WorkflowTable from 'components/visual/SearchResult/workflow';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router';
+import { useLocation } from 'react-router-dom';
+import WorkflowCreate from './create';
+import WorkflowDetail from './detail';
+
+type SearchResults = {
+  items: WorkflowIndexed[];
+  offset: number;
+  rows: number;
+  total: number;
+};
+
+const WORKFLOWS_PARAMS = createSearchParams(p => ({
+  query: p.string(''),
+  offset: p.number(0).min(0).hidden().ignored(),
+  rows: p.number(25).enforced().hidden().ignored(),
+  sort: p.string('last_seen desc').ignored(),
+  filters: p.filters([]),
+  track_total_hits: p.number(10000).nullable().ignored(),
+  refresh: p.boolean(false).hidden().ignored()
+}));
+
+type WorkflowsParams = SearchParams<typeof WORKFLOWS_PARAMS>;
+
+const WorkflowsSearch = () => {
+  const { t } = useTranslation(['manageWorkflows']);
+  const theme = useTheme();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { apiCall } = useMyAPI();
+  const { indexes } = useALContext();
+  const { user: currentUser } = useAppUser<CustomUser>();
+  const { globalDrawerOpened, setGlobalDrawer, closeGlobalDrawer } = useDrawer();
+  const { search, setSearchParams, setSearchObject } = useSearchParams<WorkflowsParams>();
+
+  const [workflowResults, setWorkflowResults] = useState<SearchResults>(null);
+  const [searching, setSearching] = useState<boolean>(false);
+
+  const suggestions = useMemo<string[]>(
+    () => [...Object.keys(indexes.workflow).filter(name => indexes.workflow[name].indexed), ...DEFAULT_SUGGESTION],
+    [indexes.workflow]
+  );
+
+  const handleReload = useCallback(
+    (body: SearchParamsResult<WorkflowsParams>) => {
+      if (!currentUser.roles.includes('workflow_view')) return;
+
+      apiCall({
+        url: '/api/v4/search/workflow/',
+        method: 'POST',
+        body: body
+          .set(o => ({ ...o, query: o.query || '*' }))
+          .omit(['refresh'])
+          .toObject(),
+        onSuccess: ({ api_response }) => setWorkflowResults(api_response as SearchResults),
+        onEnter: () => setSearching(true),
+        onExit: () => setSearching(false)
+      });
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [currentUser.roles]
+  );
+
+  const setWorkflowID = useCallback(
+    (wf_id: string) => navigate(`${location.pathname}${location.search}#/detail/${wf_id}`),
+    [location.pathname, location.search, navigate]
+  );
+
+  useEffect(() => {
+    if (!location.hash || globalDrawerOpened || !workflowResults) return;
+    navigate(`${location.pathname}${location.search || ''}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [globalDrawerOpened]);
+
+  useEffect(() => {
+    const url = new URL(`${window.location.origin}${location.hash.slice(1)}`);
+
+    if (url.pathname.startsWith('/detail/')) {
+      setGlobalDrawer(<WorkflowDetail id={url.pathname.slice('/detail/'.length)} onClose={closeGlobalDrawer} />);
+    } else if (url.pathname.startsWith('/create/')) {
+      setGlobalDrawer(
+        <WorkflowCreate
+          id={url.pathname.slice('/create/'.length)}
+          search={url.search.slice(1)}
+          onClose={closeGlobalDrawer}
+        />
+      );
+    } else {
+      closeGlobalDrawer();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.hash]);
+
+  useEffect(() => {
+    handleReload(search);
+  }, [handleReload, search]);
+
+  useEffect(() => {
+    function reload() {
+      setSearchObject(o => ({ ...o, offset: 0, refresh: !o.refresh }));
+    }
+
+    window.addEventListener('reloadWorkflows', reload);
+    return () => {
+      window.removeEventListener('reloadWorkflows', reload);
+    };
+  }, [setSearchObject]);
+
+  return currentUser.roles.includes('workflow_view') ? (
+    <PageFullWidth margin={4}>
+      <div style={{ paddingBottom: theme.spacing(2) }}>
+        <Grid container alignItems="center">
+          <Grid item xs>
+            <Typography variant="h4">{t('title')}</Typography>
+          </Grid>
+          {currentUser.roles.includes('workflow_manage') && (
+            <Grid item xs style={{ textAlign: 'right', flexGrow: 0 }}>
+              <Tooltip title={t('add_workflow')}>
+                <IconButton
+                  style={{
+                    color: theme.palette.mode === 'dark' ? theme.palette.success.light : theme.palette.success.dark
+                  }}
+                  onClick={() => navigate(`${location.pathname}${location.search}#/create/`)}
+                  size="large"
+                >
+                  <AddCircleOutlineOutlinedIcon />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+          )}
+        </Grid>
+      </div>
+
+      <PageHeader isSticky>
+        <div style={{ paddingTop: theme.spacing(1) }}>
+          <SearchHeader
+            params={search.toParams()}
+            loading={searching}
+            results={workflowResults}
+            resultLabel={
+              search.get('query')
+                ? t(`filtered${workflowResults?.total === 1 ? '' : 's'}`)
+                : t(`total${workflowResults?.total === 1 ? '' : 's'}`)
+            }
+            onChange={v => setSearchParams(v)}
+            paramDefaults={search.defaults().toObject()}
+            searchInputProps={{ placeholder: t('filter'), options: suggestions }}
+            actionProps={[
+              {
+                tooltip: { title: t('never_used') },
+                icon: { children: <EventBusyOutlinedIcon /> },
+                button: {
+                  onClick: () => setSearchObject(o => ({ ...o, offset: 0, filters: [...o.filters, 'hit_count:0'] }))
+                }
+              },
+              {
+                tooltip: { title: t('old') },
+                icon: { children: <EventOutlinedIcon /> },
+                button: {
+                  onClick: () =>
+                    setSearchObject(o => ({ ...o, offset: 0, filters: [...o.filters, 'last_seen:[* TO now-3m]'] }))
+                }
+              }
+            ]}
+          />
+        </div>
+      </PageHeader>
+
+      <div style={{ paddingTop: theme.spacing(2), paddingLeft: theme.spacing(0.5), paddingRight: theme.spacing(0.5) }}>
+        <WorkflowTable workflowResults={workflowResults} setWorkflowID={setWorkflowID} />
+      </div>
+    </PageFullWidth>
+  ) : (
+    <ForbiddenPage />
+  );
+};
+
+const WrappedWorkflowsPage = () => (
+  <SearchParamsProvider params={WORKFLOWS_PARAMS}>
+    <WorkflowsSearch />
+  </SearchParamsProvider>
+);
+
+export const WorkflowsPage = React.memo(WrappedWorkflowsPage);
+export default WorkflowsPage;
