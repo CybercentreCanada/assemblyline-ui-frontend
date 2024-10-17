7216185eb46599fb0374b27b1e749719d72ad55f
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2024-10-17 02:45:00 +0000

    more changes

--- src/components/routes/submit2/contexts/createStoreContext.tsx
+++ src/components/routes/submit2/contexts/createStoreContext.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useCallback, useContext, useRef, useSyncExternalS
 
 type UseStoreDataReturn<Store extends object> = {
   get: () => Store;
-  set: (input: Store | ((prev: Store) => Store)) => void;
+  set: <Value extends unknown>(next: Value, mutator: (value: Value, store: Store) => Store) => void;
   subscribe: (callback: () => void) => () => void;
 };
 
@@ -18,8 +18,8 @@ export const createStoreContext = <Store extends object>(initialState: Store) =>
 
     const subscribers = useRef(new Set<() => void>());
 
-    const set = useCallback((input: Store | ((prev: Store) => Store)) => {
-      store.current = typeof input === 'function' ? input(store.current) : input;
+    const set = useCallback(<Value extends unknown>(next: Value, mutator: (value: Value, store: Store) => Store) => {
+      store.current = JSON.parse(JSON.stringify(mutator(next, store.current)));
       subscribers.current.forEach(callback => callback());
     }, []);
 
@@ -41,9 +41,11 @@ export const createStoreContext = <Store extends object>(initialState: Store) =>
     <StoreContext.Provider value={useStoreData()}>{children}</StoreContext.Provider>
   );
 
-  const useStore = <SelectorOutput extends unknown>(
-    selector: (store: Store) => SelectorOutput
-  ): [SelectorOutput, (input: Store | ((prev: Store) => Store)) => void] => {
+  const useStore = <Value extends unknown>(
+    selector: (store: Store) => Value,
+    mutator: (value: Value, store: Store) => Store
+    // ): [Value, (input: Value | ((prev: Value) => Value)) => void] => {
+  ): [Value, React.Dispatch<React.SetStateAction<Value>>] => {
     const store = useContext(StoreContext);
     if (!store) {
       throw new Error('Store not found');
@@ -55,8 +57,32 @@ export const createStoreContext = <Store extends object>(initialState: Store) =>
       () => selector(initialState)
     );
 
-    return [state, store.set];
+    const is = useCallback(
+      (input: Value | ((prev: Value) => Value)): input is (prev: Value) => Value => typeof input === 'function',
+      []
+    );
+
+    const mutate = useCallback(
+      (s: Value, set: (next: Value, mutator: (value: Value, store: Store) => Store) => void) =>
+        (input: Value | ((prev: Value) => Value)): void => {
+          const value = is(input) ? input(s) : input;
+          set(value, mutator);
+        },
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      []
+    );
+
+    return [state, mutate(state, store.set)];
   };
+
+  // const useMutate = <SelectorOutput extends unknown>(mutator: (store: Store, value: SelectorOutput) => Store) => {
+  //   const store = useContext(StoreContext);
+  //   if (!store) {
+  //     throw new Error('Store not found');
+  //   }
+
+  //   return store.set;
+  // };
 
   return { Provider, useStore };
 };
