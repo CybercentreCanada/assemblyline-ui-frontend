2b2b403ec7b937c24d1f10a3946ede1e75f58828
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2024-12-18 20:34:06 +0000

    Added the TextInput

---
+++ src/components/visual/Inputs/TextInput.tsx
@@ -0,0 +1,144 @@
+import RefreshOutlinedIcon from '@mui/icons-material/RefreshOutlined';
+import type {
+  AutocompleteProps,
+  FormHelperTextProps,
+  IconButtonProps,
+  TextFieldProps,
+  TooltipProps,
+  TypographyProps
+} from '@mui/material';
+import {
+  Autocomplete,
+  FormControl,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Skeleton,
+  Typography,
+  useTheme
+} from '@mui/material';
+import type { ElementType } from 'react';
+import React, { useState } from 'react';
+
+type Props<
+  Value,
+  Multiple extends boolean,
+  DisableClearable extends boolean,
+  FreeSolo extends boolean,
+  ChipComponent extends ElementType
+> = Omit<
+  AutocompleteProps<Value, Multiple, DisableClearable, FreeSolo, ChipComponent>,
+  'renderInput' | 'options' | 'onChange' | 'value'
+> & {
+  label: string;
+  labelProps?: TypographyProps;
+  loading?: boolean;
+  options?: AutocompleteProps<Value, Multiple, DisableClearable, FreeSolo, ChipComponent>['options'];
+  value: string;
+  errors?: (string | false)[];
+  tooltip?: TooltipProps['title'];
+  tooltipProps?: Omit<TooltipProps, 'children' | 'title'>;
+  error?: TextFieldProps['helperText'];
+  errorProps?: FormHelperTextProps;
+  reset?: boolean;
+  onChange?: AutocompleteProps<Value, Multiple, DisableClearable, FreeSolo, ChipComponent>['onInputChange'];
+  onReset?: IconButtonProps['onClick'];
+};
+
+const WrappedTextInput = <
+  Value,
+  Multiple extends boolean,
+  DisableClearable extends boolean,
+  FreeSolo extends boolean,
+  ChipComponent extends ElementType
+>({
+  label,
+  labelProps,
+  value,
+  disabled,
+  loading = false,
+  options = [],
+
+  tooltip,
+  tooltipProps,
+  error = null,
+  errorProps = null,
+  reset = false,
+  onChange,
+  onReset,
+  ...other
+}: Props<Value, Multiple, DisableClearable, FreeSolo, ChipComponent>) => {
+  const theme = useTheme();
+
+  const [_value, setValue] = useState(null);
+
+  return (
+    <>
+      <Typography
+        component={InputLabel}
+        htmlFor={label}
+        variant="body2"
+        whiteSpace="nowrap"
+        textTransform="capitalize"
+        gutterBottom
+        {...labelProps}
+        children={label}
+      />
+      <FormControl fullWidth>
+        {loading ? (
+          <Skeleton sx={{ height: '40px', transform: 'unset' }} />
+        ) : (
+          <Autocomplete
+            id={label}
+            autoComplete
+            freeSolo
+            disableClearable
+            fullWidth
+            size="small"
+            value={_value}
+            disabled={disabled}
+            inputValue={value || ''}
+            options={options}
+            onChange={(e, v) => setValue(v)}
+            onInputChange={(e, v, o) => {
+              setValue(v);
+              onChange(e, v, o);
+            }}
+            renderInput={params => (
+              <>
+                <OutlinedInput
+                  id={label}
+                  error={!!error}
+                  endAdornment={
+                    !reset ? null : (
+                      <InputAdornment position="start">
+                        <IconButton edge="end" onClick={onReset}>
+                          <RefreshOutlinedIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }
+                  {...params}
+                />
+                {!error || disabled ? null : (
+                  <FormHelperText
+                    sx={{ color: theme.palette.error.main, ...errorProps?.sx }}
+                    variant="outlined"
+                    {...errorProps}
+                  >
+                    {error}
+                  </FormHelperText>
+                )}
+              </>
+            )}
+            {...other}
+          />
+        )}
+      </FormControl>
+    </>
+  );
+};
+
+export const TextInput = React.memo(WrappedTextInput);
