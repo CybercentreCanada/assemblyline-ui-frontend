29c3bf9453d6fb1d208a501742dc6ee1b45f2760
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2024-12-19 07:23:34 +0000

    Modified the submission parameters

--- src/components/routes/submit/components/SubmissionParameters.tsx
+++ src/components/routes/submit/components/SubmissionParameters.tsx
@@ -1,47 +1,66 @@
 import { Typography, useTheme } from '@mui/material';
 import useALContext from 'components/hooks/useALContext';
 import { useForm } from 'components/routes/submit/contexts/form';
-import { BooleanInput } from 'components/routes/submit/inputs/BooleanInput';
-import { NumberInput } from 'components/routes/submit/inputs/NumberInput';
-import { SliderInput } from 'components/routes/submit/inputs/SliderInput';
-import { TextInput } from 'components/routes/submit/inputs/TextInput';
+import { CheckboxInput } from 'components/visual/Inputs/CheckboxInput';
+import { NumberInput } from 'components/visual/Inputs/NumberInput';
+import { SliderInput } from 'components/visual/Inputs/SliderInput';
+import { TextInput } from 'components/visual/Inputs/TextInput';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-const WrappedSubmissionParameters = () => {
+type Props = {
+  profile?: string;
+  loading?: boolean;
+  disabled?: boolean;
+};
+
+const WrappedSubmissionParameters = ({ profile = null, loading = false, disabled = false }: Props) => {
   const { t } = useTranslation(['submit', 'settings']);
   const theme = useTheme();
   const form = useForm();
   const { configuration } = useALContext();
 
   return (
-    <div style={{ textAlign: 'left', marginTop: theme.spacing(2) }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: 'left',
+        marginTop: theme.spacing(2),
+        rowGap: theme.spacing(1)
+      }}
+    >
       <Typography variant="h6" gutterBottom>
         {t('options.submission')}
       </Typography>
 
-      <form.Field
-        name="settings.description"
-        children={({ state, handleBlur, handleChange }) => (
+      <form.Subscribe
+        selector={state => [state.values?.settings?.description]}
+        children={([description]) => (
           <TextInput
             label={t('options.submission.desc')}
-            loading={!form.state.values.settings}
-            value={state.value}
-            onBlur={handleBlur}
-            onChange={v => handleChange(v)}
+            value={description}
+            loading={loading}
+            disabled={disabled}
+            onChange={(event, value) => {
+              form.setStore(s => {
+                s.settings.description = value;
+                return s;
+              });
+            }}
           />
         )}
       />
 
-      <form.Field
-        name="settings.priority"
-        children={({ state, handleBlur, handleChange }) => (
+      <form.Subscribe
+        selector={state => [state.values?.settings?.priority]}
+        children={([priority]) => (
           <SliderInput
             label={t('options.submission.priority')}
-            loading={!form.state.values.settings}
-            value={state.value}
-            valueLabelDisplay={'auto'}
-            size="small"
+            value={priority}
+            loading={loading}
+            disabled={disabled}
+            step={null}
             min={500}
             max={1500}
             marks={[
@@ -49,100 +68,144 @@ const WrappedSubmissionParameters = () => {
               { label: t('options.submission.priority.medium'), value: 1000 },
               { label: t('options.submission.priority.high'), value: 1500 }
             ]}
-            step={null}
-            onBlur={handleBlur}
-            onChange={(_, value: number) => handleChange(value)}
-            // disabled={
-            //   !currentUser.roles.includes('submission_customize') &&
-            //   form.state.values.submissionProfile?.priority !== undefined
-            // }
+            onChange={(event, value: number) => {
+              form.setStore(s => {
+                s.settings.priority = value;
+                return s;
+              });
+            }}
           />
         )}
       />
 
-      <div style={{ paddingTop: theme.spacing(1), paddingBottom: theme.spacing(1) }}>
-        <form.Field
-          name="settings.generate_alert"
-          children={({ state, handleBlur, handleChange }) => (
-            <BooleanInput
+      <div>
+        <form.Subscribe
+          selector={state => [state.values?.settings?.profiles?.[profile]?.generate_alert]}
+          children={([generate_alert]) => (
+            <CheckboxInput
               label={t('options.submission.generate_alert')}
-              loading={!form.state.values.settings}
-              value={state.value}
-              onBlur={handleBlur}
-              onClick={() => handleChange(!state.value)}
+              value={generate_alert?.value}
+              loading={loading}
+              disabled={disabled}
+              onChange={() => {
+                form.setStore(s => {
+                  s.settings.profiles[profile].generate_alert = {
+                    ...s.settings.profiles[profile].generate_alert,
+                    value: !s.settings.profiles[profile].generate_alert.value
+                  };
+                  return s;
+                });
+              }}
             />
           )}
         />
 
-        <form.Field
-          name="settings.ignore_filtering"
-          children={({ state, handleBlur, handleChange }) => (
-            <BooleanInput
+        <form.Subscribe
+          selector={state => [state.values?.settings?.profiles?.[profile]?.ignore_filtering]}
+          children={([ignore_filtering]) => (
+            <CheckboxInput
               label={t('options.submission.ignore_filtering')}
-              loading={!form.state.values.settings}
-              value={state.value}
-              onBlur={handleBlur}
-              onClick={() => handleChange(!state.value)}
+              value={ignore_filtering?.value}
+              loading={loading}
+              disabled={disabled}
+              onChange={() => {
+                form.setStore(s => {
+                  s.settings.profiles[profile].ignore_filtering = {
+                    ...s.settings.profiles[profile].ignore_filtering,
+                    value: !s.settings.profiles[profile].ignore_filtering.value
+                  };
+                  return s;
+                });
+              }}
             />
           )}
         />
 
-        <form.Field
-          name="settings.ignore_cache"
-          children={({ state, handleBlur, handleChange }) => (
-            <BooleanInput
+        <form.Subscribe
+          selector={state => [state.values?.settings?.profiles?.[profile]?.ignore_cache]}
+          children={([ignore_cache]) => (
+            <CheckboxInput
               label={t('options.submission.ignore_cache')}
-              loading={!form.state.values.settings}
-              value={state.value}
-              onBlur={handleBlur}
-              onClick={() => handleChange(!state.value)}
+              value={ignore_cache?.value}
+              loading={loading}
+              disabled={disabled}
+              onChange={() => {
+                form.setStore(s => {
+                  s.settings.profiles[profile].ignore_cache = {
+                    ...s.settings.profiles[profile].ignore_cache,
+                    value: !s.settings.profiles[profile].ignore_cache.value
+                  };
+                  return s;
+                });
+              }}
             />
           )}
         />
 
-        <form.Field
-          name="settings.ignore_dynamic_recursion_prevention"
-          children={({ state, handleBlur, handleChange }) => (
-            <BooleanInput
+        <form.Subscribe
+          selector={state => [state.values?.settings?.profiles?.[profile]?.ignore_dynamic_recursion_prevention]}
+          children={([ignore_dynamic_recursion_prevention]) => (
+            <CheckboxInput
               label={t('options.submission.ignore_dynamic_recursion_prevention')}
-              loading={!form.state.values.settings}
-              value={state.value}
-              onBlur={handleBlur}
-              onClick={() => handleChange(!state.value)}
+              value={ignore_dynamic_recursion_prevention?.value}
+              loading={loading}
+              disabled={disabled}
+              onChange={() => {
+                form.setStore(s => {
+                  s.settings.profiles[profile].ignore_dynamic_recursion_prevention = {
+                    ...s.settings.profiles[profile].ignore_dynamic_recursion_prevention,
+                    value: !s.settings.profiles[profile].ignore_dynamic_recursion_prevention.value
+                  };
+                  return s;
+                });
+              }}
             />
           )}
         />
 
-        <form.Field
-          name="settings.profile"
-          children={({ state, handleBlur, handleChange }) => (
-            <BooleanInput
+        <form.Subscribe
+          selector={state => [state.values?.settings?.profile]}
+          children={([profiling]) => (
+            <CheckboxInput
               label={t('options.submission.profile')}
-              loading={!form.state.values.settings}
-              value={state.value}
-              onBlur={handleBlur}
-              onClick={() => handleChange(!state.value)}
+              value={profiling}
+              loading={loading}
+              disabled={disabled}
+              onChange={() => {
+                form.setStore(s => {
+                  s.settings.profile = !s.settings.profile;
+                  return s;
+                });
+              }}
             />
           )}
         />
 
-        <form.Field
-          name="settings.deep_scan"
-          children={({ state, handleBlur, handleChange }) => (
-            <BooleanInput
+        <form.Subscribe
+          selector={state => [state.values?.settings?.profiles?.[profile]?.deep_scan]}
+          children={([deep_scan]) => (
+            <CheckboxInput
               label={t('options.submission.deep_scan')}
-              loading={!form.state.values.settings}
-              value={state.value}
-              onBlur={handleBlur}
-              onClick={() => handleChange(!state.value)}
+              value={deep_scan?.value}
+              loading={loading}
+              disabled={disabled}
+              onChange={() => {
+                form.setStore(s => {
+                  s.settings.profiles[profile].deep_scan = {
+                    ...s.settings.profiles[profile].deep_scan,
+                    value: !s.settings.profiles[profile].deep_scan.value
+                  };
+                  return s;
+                });
+              }}
             />
           )}
         />
       </div>
 
-      <form.Field
-        name="settings.ttl"
-        children={({ state, handleBlur, handleChange }) => (
+      <form.Subscribe
+        selector={state => [state.values?.settings?.profiles?.[profile]?.ttl]}
+        children={([ttl]) => (
           <NumberInput
             label={`${t('options.submission.ttl')} (${
               configuration.submission.max_dtl !== 0
@@ -150,12 +213,20 @@ const WrappedSubmissionParameters = () => {
                 : t('options.submission.ttl.forever')
             })`}
             endAdornment={t('settings:submissions.ttl_days')}
-            loading={!form.state.values.settings}
+            value={ttl?.value}
+            loading={loading}
+            disabled={disabled}
             min={configuration.submission.max_dtl !== 0 ? 1 : 0}
             max={configuration.submission.max_dtl !== 0 ? configuration.submission.max_dtl : 365}
-            value={state.value}
-            onBlur={handleBlur}
-            onChange={event => handleChange(Number(event.target.value))}
+            onChange={(event, value) => {
+              form.setStore(s => {
+                s.settings.profiles[profile].ttl = {
+                  ...s.settings.profiles[profile].ttl,
+                  value: value
+                };
+                return s;
+              });
+            }}
           />
         )}
       />
