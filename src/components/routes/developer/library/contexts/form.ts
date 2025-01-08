b20f5c7766b27de18c720b8ce5f5d3f3bc34d4ce
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2025-01-08 01:35:33 +0000

    changed the form

--- src/components/routes/developer/library/contexts/form.ts
+++ src/components/routes/developer/library/contexts/form.ts
@@ -3,26 +3,46 @@ import { createFormContext } from 'components/core/form/createFormContext';
 type LibraryFormStore = {
   inputs: {
     open: boolean;
-    text: string;
-    number: number;
-    date: string;
-    select: string;
-    checkbox: boolean;
-    switch: boolean;
-    slider: number;
+    state: {
+      disabled: boolean;
+      loading: boolean;
+      reset: boolean;
+      tooltip: boolean;
+      error: boolean;
+      readOnly: boolean;
+    };
+    values: {
+      text: string;
+      number: number;
+      date: string;
+      select: string;
+      checkbox: boolean;
+      switch: boolean;
+      slider: number;
+    };
   };
 };
 
 const LIBRARY_FORM_STORE: LibraryFormStore = Object.freeze({
   inputs: {
     open: false,
-    text: '',
-    number: 0,
-    date: '',
-    select: '',
-    checkbox: false,
-    switch: false,
-    slider: 0
+    state: {
+      disabled: false,
+      loading: false,
+      reset: false,
+      tooltip: false,
+      error: false,
+      readOnly: false
+    },
+    values: {
+      text: '',
+      number: 0,
+      date: '',
+      select: '',
+      checkbox: false,
+      switch: false,
+      slider: 0
+    }
   }
 });
 
