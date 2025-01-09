b9389f2eebc0a9f3d3df3749857d113c4102a445
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2025-01-09 21:59:42 +0000

    Added the changes to the Library index

--- src/components/routes/developer/library/index.tsx
+++ src/components/routes/developer/library/index.tsx
@@ -3,12 +3,18 @@ import { PageHeader } from 'components/visual/Layouts/PageHeader';
 import React from 'react';
 import { FormProvider } from './contexts/form';
 import { InputsSection } from './sections/Inputs';
+import { LayoutSection } from './sections/Layout';
+import { ListInputsSection } from './sections/ListInputs';
 
 const LibraryContent = () => {
   return (
     <PageCenter width="100%" height="100%" textAlign="left" margin={4}>
       <PageHeader primary="Library" secondary="Visual Components" />
       <InputsSection />
+      <LayoutSection />
+      <ListInputsSection />
+
+      <div style={{ height: '500px' }} />
     </PageCenter>
   );
 };
