4cddadae0c28db0373ea7dfb6f02aa03077c86cf
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2025-01-10 20:50:04 +0000

    Changed the library form

--- src/components/routes/developer/library/contexts/form.ts
+++ src/components/routes/developer/library/contexts/form.ts
@@ -1,17 +1,31 @@
 import { createFormContext } from 'components/core/form/createFormContext';
 import { INPUTS_LIBRARY_STATE, type InputsLibraryState } from 'components/routes/developer/library/sections/Inputs';
 import { LAYOUT_LIBRARY_STATE, type LayoutLibraryState } from 'components/routes/developer/library/sections/Layout';
+import { LIST_LIBRARY_STATE, type ListLibraryState } from 'components/routes/developer/library/sections/List';
 import {
   LIST_INPUTS_LIBRARY_STATE,
   type ListInputsLibraryState
 } from 'components/routes/developer/library/sections/ListInputs';
 
-type LibraryFormStore = ListInputsLibraryState & InputsLibraryState & LayoutLibraryState;
+type LibraryComponents = ListInputsLibraryState & InputsLibraryState & LayoutLibraryState & ListLibraryState;
+
+type LibraryFormStore = {
+  state: {
+    active: keyof LibraryComponents;
+  };
+  components: LibraryComponents;
+};
 
 const LIBRARY_FORM_STORE: LibraryFormStore = Object.freeze({
-  ...INPUTS_LIBRARY_STATE,
-  ...LAYOUT_LIBRARY_STATE,
-  ...LIST_INPUTS_LIBRARY_STATE
+  state: {
+    active: null
+  },
+  components: {
+    ...INPUTS_LIBRARY_STATE,
+    ...LAYOUT_LIBRARY_STATE,
+    ...LIST_INPUTS_LIBRARY_STATE,
+    ...LIST_LIBRARY_STATE
+  }
 });
 
 export const { FormProvider, useForm } = createFormContext<LibraryFormStore>({
