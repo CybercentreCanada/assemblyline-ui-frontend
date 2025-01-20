c9655dfb56debe95139736df5dbee8c4ed4fca36
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2025-01-20 20:30:09 +0000

    Fixed an error message

--- src/components/hooks/useMyTheme.tsx
+++ src/components/hooks/useMyTheme.tsx
@@ -1,22 +1,18 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-unused-vars */
 import type { AppThemeConfigs } from 'commons/components/app/AppConfigs';
 import { useEffect, useMemo, useState } from 'react';
-import THEME from '../../../public/theme.json';
 
 export const useMyTheme = () => {
-  const [theme, setTheme] = useState<AppThemeConfigs>(THEME);
+  const [theme, setTheme] = useState<AppThemeConfigs>({});
 
   useEffect(() => {
     fetch('/theme.json')
       .then(response => response.json())
       .then(data => setTheme(data as AppThemeConfigs))
+      // eslint-disable-next-line no-console
       .catch(error => console.error('Error fetching the JSON theme file:', error));
   }, []);
 
   return useMemo((): AppThemeConfigs => theme, [theme]);
 };
 
-// const useMyTheme = () =>
-//   useMemo((): AppThemeConfigs => require('json!/theme.json') || (ANALYTICAL_PLATFORM_THEME as AppThemeConfigs), []);
 export default useMyTheme;
