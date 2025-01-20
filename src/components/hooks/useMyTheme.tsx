2d1d79302d568f6b473385bd4b3ddd1a1477c869
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2025-01-20 15:17:49 -0500

    Remove unused constant

--- src/components/hooks/useMyTheme.tsx
+++ src/components/hooks/useMyTheme.tsx
@@ -4,61 +4,6 @@ import type { AppThemeConfigs } from 'commons/components/app/AppConfigs';
 import { useEffect, useMemo, useState } from 'react';
 import THEME from '../../../public/theme.json';
 
-const ANALYTICAL_PLATFORM_THEME: AppThemeConfigs = {
-  components: {
-    MuiPaper: {
-      styleOverrides: {
-        elevation: {
-          backgroundImage: 'none'
-        }
-      }
-    },
-    MuiTab: {
-      styleOverrides: {
-        root: {
-          minWidth: '145px'
-        }
-      }
-    }
-  },
-  palette: {
-    dark: {
-      background: {
-        default: '#202020',
-        paper: '#303030'
-      },
-      primary: {
-        main: '#7DA1DB'
-      },
-      secondary: {
-        main: '#C0DEEC'
-      },
-      divider: '#414141'
-    },
-    light: {
-      background: {
-        default: '#FAFAFA',
-        paper: '#FFFFFF'
-      },
-      primary: {
-        main: '#0062BF'
-      },
-      secondary: {
-        main: '#5189A3'
-      }
-    }
-  },
-  appbar: {
-    dark: {
-      backgroundColor: '#303030'
-    },
-    light: {
-      color: '#000000',
-      backgroundColor: '#FFFFFF'
-    }
-  }
-};
-
 export const useMyTheme = () => {
   const [theme, setTheme] = useState<AppThemeConfigs>(THEME);
 
