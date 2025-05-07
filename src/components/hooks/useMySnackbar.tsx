b2844d5d2b2a8326cde61f2e31549e98f3bb1ba5
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2025-05-07 17:44:49 +0000

    Fixed the snackbar not closing when clicking on it

--- src/components/hooks/useMySnackbar.tsx
+++ src/components/hooks/useMySnackbar.tsx
@@ -1,40 +1,66 @@
-import type { OptionsObject } from 'notistack';
+// Shamelessly stolen from https://raw.githubusercontent.com/CybercentreCanada/howler-ui-internal/refs/heads/develop/src/components/hooks/useMySnackbar.tsx
+import type { OptionsObject, SnackbarMessage } from 'notistack';
 import { useSnackbar } from 'notistack';
+import { useCallback, useMemo } from 'react';
 
 export default function useMySnackbar() {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-  const snackBarOptions: OptionsObject<'default' | 'error' | 'info' | 'success' | 'warning'> = {
-    preventDuplicate: true,
-    anchorOrigin: {
-      vertical: 'bottom',
-      horizontal: 'center'
-    },
-    onClose: snack => {
-      closeSnackbar();
-    }
-  };
+  const snackBarOptions: OptionsObject = useMemo(
+    () => ({
+      preventDuplicate: true,
+      anchorOrigin: {
+        vertical: 'bottom',
+        horizontal: 'center'
+      },
+      SnackbarProps: {
+        onClick: () => {
+          closeSnackbar();
+        }
+      }
+    }),
+    [closeSnackbar]
+  );
 
-  function showErrorMessage(message, timeout = 5000) {
-    enqueueSnackbar(message, { variant: 'error', autoHideDuration: timeout, ...snackBarOptions });
-  }
+  const enqueue = useCallback(
+    (variant: 'error' | 'success' | 'warning' | 'info') =>
+      (message: SnackbarMessage, timeout = 5000, options: OptionsObject = {}) =>
+        enqueueSnackbar(message, {
+          variant,
+          autoHideDuration: timeout,
+          ...snackBarOptions,
+          ...options,
+          SnackbarProps: {
+            ...(options.SnackbarProps ?? {}),
+            onClick: options.SnackbarProps?.onClick
+              ? e => {
+                  options.SnackbarProps?.onClick(e);
+                  snackBarOptions.SnackbarProps.onClick(e);
+                }
+              : snackBarOptions.SnackbarProps.onClick
+          }
+        }),
+    [enqueueSnackbar, snackBarOptions]
+  );
 
-  function showWarningMessage(message, timeout = 5000) {
-    enqueueSnackbar(message, { variant: 'warning', autoHideDuration: timeout, ...snackBarOptions });
-  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const showErrorMessage = useCallback(enqueue('error'), [enqueue]);
 
-  function showSuccessMessage(message, timeout = 5000) {
-    enqueueSnackbar(message, { variant: 'success', autoHideDuration: timeout, ...snackBarOptions });
-  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const showWarningMessage = useCallback(enqueue('warning'), [enqueue]);
 
-  function showInfoMessage(message, timeout = 5000) {
-    enqueueSnackbar(message, { variant: 'info', autoHideDuration: timeout, ...snackBarOptions });
-  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const showSuccessMessage = useCallback(enqueue('success'), [enqueue]);
 
-  return {
-    showErrorMessage,
-    showWarningMessage,
-    showSuccessMessage,
-    showInfoMessage,
-    closeSnackbar
-  };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const showInfoMessage = useCallback(enqueue('info'), [enqueue]);
+
+  return useMemo(
+    () => ({
+      showErrorMessage,
+      showWarningMessage,
+      showSuccessMessage,
+      showInfoMessage
+    }),
+    [showErrorMessage, showInfoMessage, showSuccessMessage, showWarningMessage]
+  );
 }
