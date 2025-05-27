c6e5edeb94e68f951e1c2660db775a3834c6db6c
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2025-05-27 17:16:26 +0000

    Reverted the regexes of the md5, sha1 and sha256.

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,8 +1,8 @@
 import type { HashPatternMap } from 'components/models/base/config';
 
-export const MD5_REGEX = /^[a-fA-F0-9]{32}$/i;
-export const SHA1_REGEX = /^[a-fA-F0-9]{40}$/i;
-export const SHA256_REGEX = /^[a-fA-F0-9]{64}$/i;
+export const MD5_REGEX = /^[a-f0-9]{32}$/i;
+export const SHA1_REGEX = /^[a-f0-9]{40}$/i;
+export const SHA256_REGEX = /^[a-f0-9]{64}$/i;
 export const SSDEEP_REGEX = /^[0-9]{1,18}:[a-zA-Z0-9/+]{0,64}:[a-zA-Z0-9/+]{0,64}$/;
 export const TLSH_REGEX = /^((?:T1)?[0-9a-fA-F]{70})$/;
 export const HASH_MAP = {
