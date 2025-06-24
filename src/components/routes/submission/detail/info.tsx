dd4bed639e707ff86a7c001a7da5cf423e4e2800
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2025-06-24 13:33:55 +0000

    Made the service category disabled colored if all of its services aren't selected.

--- src/components/routes/submission/detail/info.tsx
+++ src/components/routes/submission/detail/info.tsx
@@ -140,7 +140,9 @@ const WrappedInfoSection: React.FC<Props> = ({ submission }) => {
                     >
                       {groupedServices.map(([cat, included, excluded]) => (
                         <>
-                          <i>{cat}: </i>
+                          <i {...(included.length === 0 && { style: { color: theme.palette.action.disabled } })}>
+                            {`${cat}:`}
+                          </i>
                           <div style={{ marginLeft: '1.5rem' }}>
                             <span>{included.join(' | ')}</span>
                             {included.length > 0 && excluded.length > 0 && <span>{' | '}</span>}
