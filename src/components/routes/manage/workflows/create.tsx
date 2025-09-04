37dc7b2f2be7d4c7d8a97dd5d0595faa8f18c5a4
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2025-09-04 19:11:57 +0000

    Changed from use apiCall to use useAPIQuery in the Workflow pages

--- src/components/routes/manage/workflows/create.tsx
+++ src/components/routes/manage/workflows/create.tsx
@@ -1,16 +1,17 @@
 import CheckIcon from '@mui/icons-material/Check';
 import DoDisturbAltOutlinedIcon from '@mui/icons-material/DoDisturbAltOutlined';
-import { Grid, IconButton, MenuItem, styled, Tooltip, useTheme } from '@mui/material';
-import Throttler from 'commons/addons/utils/throttler';
+import { Grid, useTheme } from '@mui/material';
 import PageCenter from 'commons/components/pages/PageCenter';
+import { useAPIMutation } from 'components/core/Query/API/useAPIMutation';
+import { useAPIQuery } from 'components/core/Query/API/useAPIQuery';
 import useALContext from 'components/hooks/useALContext';
-import useMyAPI from 'components/hooks/useMyAPI';
 import useMySnackbar from 'components/hooks/useMySnackbar';
 import type { Alert } from 'components/models/base/alert';
 import type { Priority, Status, Workflow } from 'components/models/base/workflow';
 import { LABELS, PRIORITIES, STATUSES } from 'components/models/base/workflow';
 import type { SearchResult } from 'components/models/ui/search';
 import ForbiddenPage from 'components/routes/403';
+import { IconButton } from 'components/visual/Buttons/IconButton';
 import Classification from 'components/visual/Classification';
 import { CheckboxInput } from 'components/visual/Inputs/CheckboxInput';
 import { ChipsInput } from 'components/visual/Inputs/ChipsInput';
@@ -18,21 +19,9 @@ import { SelectInput } from 'components/visual/Inputs/SelectInput';
 import { TextInput } from 'components/visual/Inputs/TextInput';
 import { PageHeader } from 'components/visual/Layouts/PageHeader';
 import _ from 'lodash';
-import React, { memo, useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation, useParams } from 'react-router';
-
-const THROTTLER = new Throttler(1000);
-
-const MyMenuItem = memo(
-  styled(MenuItem)(({ theme }) => ({
-    ['& .MuiMenuItem-root']: {
-      root: {
-        minHeight: theme.spacing(4)
-      }
-    }
-  }))
-);
 
 type Params = {
   id: string;
@@ -48,16 +37,12 @@ const WrappedWorkflowCreate = ({ id: propID = null, onClose = () => null }: Prop
   const { id: paramID } = useParams<Params>();
   const theme = useTheme();
   const location = useLocation();
-  const { apiCall } = useMyAPI();
   const { c12nDef, configuration, user: currentUser } = useALContext();
   const { showSuccessMessage, showErrorMessage } = useMySnackbar();
 
   const [workflow, setWorkflow] = useState<Workflow>(null);
   const [originalWorkflow, setOriginalWorkflow] = useState<Workflow>(null);
-  const [badQuery, setBadQuery] = useState<boolean>(false);
-  const [results, setResults] = useState<SearchResult<Alert>>(null);
   const [runWorkflow, setRunWorkflow] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(false);
 
   const defaultWorkflow = useMemo<Workflow>(
     () => ({
@@ -84,84 +69,61 @@ const WrappedWorkflowCreate = ({ id: propID = null, onClose = () => null }: Prop
 
   const modified = useMemo<boolean>(() => !_.isEqual(workflow, originalWorkflow), [originalWorkflow, workflow]);
 
-  const disabled = useMemo<boolean>(
-    () => loading || !modified || badQuery || workflow?.query === '' || workflow?.name === '',
-    [badQuery, loading, modified, workflow?.name, workflow?.query]
-  );
-
-  const handleAdd = useCallback(
-    (wf: Workflow, run: boolean) => {
-      if (!currentUser.roles.includes('workflow_manage')) return;
-
-      apiCall<{ success: boolean; workflow_id: string }>({
-        url: `/api/v4/workflow/?run_workflow=${run}`,
-        method: 'PUT',
-        body: {
-          ...wf,
-          priority: !wf.priority ? null : wf.priority,
-          status: !wf.status ? null : wf.status
-        },
-        onSuccess: ({ api_response }) => {
-          showSuccessMessage(t('add.success'));
-          setTimeout(() => window.dispatchEvent(new CustomEvent('reloadWorkflows')), 1000);
-          setTimeout(() => window.dispatchEvent(new CustomEvent('alertRefresh', null)), 1500);
-          onClose(api_response.workflow_id);
-        },
-        onEnter: () => setLoading(true),
-        onExit: () => setLoading(false)
-      });
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [currentUser.roles, t]
-  );
-
-  const handleUpdate = useCallback(
-    (wf: Workflow, run: boolean) => {
-      if (!currentUser.roles.includes('workflow_manage')) return;
-
-      apiCall<{ success: boolean; workflow_id: string }>({
-        url: `/api/v4/workflow/${id}/?run_workflow=${run}`,
-        method: 'POST',
-        body: {
-          ...wf,
-          priority: !wf.priority ? null : wf.priority,
-          status: !wf.status ? null : wf.status
-        },
-        onSuccess: () => {
-          showSuccessMessage(t('update.success'));
-          setTimeout(() => window.dispatchEvent(new CustomEvent('reloadWorkflows')), 1000);
-          onClose(id);
-        },
-        onEnter: () => setLoading(true),
-        onExit: () => setLoading(false)
-      });
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [currentUser.roles, id, t]
-  );
-
-  useEffect(() => {
-    if (!id || !currentUser.roles.includes('workflow_manage') || originalWorkflow) return;
-
-    apiCall<Workflow>({
-      url: `/api/v4/workflow/${id}/`,
-      onSuccess: ({ api_response }) => {
-        const wf = {
-          ...api_response,
-          status: api_response.status || '',
-          priority: api_response.priority || '',
-          enabled: api_response.enabled === undefined ? true : api_response.enabled
-        } as Workflow;
-        setWorkflow(wf);
-        setOriginalWorkflow(wf);
+  const handleAdd = useAPIMutation<[Workflow, boolean], { success: boolean; workflow_id: string }>(
+    (wf: Workflow, run: boolean) => ({
+      url: `/api/v4/workflow/?run_workflow=${run}`,
+      method: 'PUT',
+      body: {
+        ...wf,
+        priority: !wf.priority ? null : wf.priority,
+        status: !wf.status ? null : wf.status
       },
-      onFailure: api_data => {
-        showErrorMessage(api_data.api_error_message);
-        onClose();
+      disabled: !currentUser.roles.includes('workflow_manage'),
+      onSuccess: ({ api_response }) => {
+        showSuccessMessage(t('add.success'));
+        setTimeout(() => window.dispatchEvent(new CustomEvent('reloadWorkflows')), 1000);
+        setTimeout(() => window.dispatchEvent(new CustomEvent('alertRefresh', null)), 1500);
+        onClose(api_response.workflow_id);
       }
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentUser.roles, id, onClose, originalWorkflow]);
+    })
+  );
+
+  const handleUpdate = useAPIMutation<[Workflow, boolean], { success: boolean; workflow_id: string }>(
+    (wf: Workflow, run: boolean) => ({
+      url: `/api/v4/workflow/${id}/?run_workflow=${run}`,
+      method: 'POST',
+      body: {
+        ...wf,
+        priority: !wf.priority ? null : wf.priority,
+        status: !wf.status ? null : wf.status
+      },
+      disabled: !currentUser.roles.includes('workflow_manage'),
+      onSuccess: () => {
+        showSuccessMessage(t('update.success'));
+        setTimeout(() => window.dispatchEvent(new CustomEvent('reloadWorkflows')), 1000);
+        onClose(id);
+      }
+    })
+  );
+
+  const handleFetch = useAPIQuery<Workflow>({
+    url: `/api/v4/workflow/${id}/`,
+    disabled: !id || !currentUser.roles.includes('workflow_manage') || !originalWorkflow,
+    onSuccess: ({ api_response }) => {
+      const wf = {
+        ...api_response,
+        status: api_response.status || '',
+        priority: api_response.priority || '',
+        enabled: api_response.enabled === undefined ? true : api_response.enabled
+      } as Workflow;
+      setWorkflow(wf);
+      setOriginalWorkflow(wf);
+    },
+    onFailure: api_data => {
+      showErrorMessage(api_data.api_error_message);
+      onClose();
+    }
+  });
 
   useEffect(() => {
     setOriginalWorkflow(defaultWorkflow);
@@ -183,28 +145,22 @@ const WrappedWorkflowCreate = ({ id: propID = null, onClose = () => null }: Prop
     }));
   }, [location?.state]);
 
-  useEffect(() => {
-    THROTTLER.delay(() => {
-      if (!!workflow?.query && currentUser.roles.includes('alert_view')) {
-        apiCall<SearchResult<Alert>>({
-          method: 'GET',
-          url: `/api/v4/search/alert/?query=${encodeURIComponent(workflow?.query)}&rows=10&track_total_hits=true`,
-          onSuccess: ({ api_response }) => {
-            setResults(api_response);
-            setBadQuery(false);
-          },
-          onFailure: () => {
-            setResults({ items: [], offset: 0, rows: 10, total: 0 });
-            setBadQuery(true);
-          }
-        });
-      } else {
-        setResults({ items: [], offset: 0, rows: 10, total: 0 });
-        setBadQuery(false);
-      }
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [workflow?.query]);
+  const handleResults = useAPIQuery<SearchResult<Alert>>({
+    url: `/api/v4/search/alert/?query=${encodeURIComponent(workflow?.query)}&rows=10&track_total_hits=true`,
+    disabled: !workflow?.query || !currentUser.roles.includes('alert_view'),
+    delay: 1000
+  });
+
+  const disabled = useMemo<boolean>(
+    () =>
+      !modified ||
+      handleAdd.isPending ||
+      handleFetch.isFetching ||
+      handleUpdate.isPending ||
+      workflow?.name === '' ||
+      workflow?.query === '',
+    [modified, handleAdd.isPending, handleFetch.isFetching, handleUpdate.isPending, workflow?.name, workflow?.query]
+  );
 
   if (!currentUser.roles.includes('workflow_manage')) return <ForbiddenPage />;
   else
@@ -229,48 +185,28 @@ const WrappedWorkflowCreate = ({ id: propID = null, onClose = () => null }: Prop
             <>
               {id ? (
                 <>
-                  <Tooltip title={t('cancel.button')}>
-                    <IconButton
-                      style={{
-                        color: theme.palette.mode === 'dark' ? theme.palette.error.light : theme.palette.error.dark
-                      }}
-                      onClick={() => onClose(id)}
-                    >
-                      <DoDisturbAltOutlinedIcon />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title={t('update.button')}>
-                    <IconButton
-                      style={{
-                        color: !disabled
-                          ? theme.palette.mode === 'dark'
-                            ? theme.palette.success.light
-                            : theme.palette.success.dark
-                          : theme.palette.grey[750]
-                      }}
-                      disabled={disabled}
-                      onClick={() => handleUpdate(workflow, runWorkflow)}
-                    >
-                      <CheckIcon />
-                    </IconButton>
-                  </Tooltip>
-                </>
-              ) : (
-                <Tooltip title={t('add.button')}>
+                  <IconButton tooltip={t('cancel.button')} color="error" onClick={() => onClose(id)}>
+                    <DoDisturbAltOutlinedIcon />
+                  </IconButton>
+
                   <IconButton
-                    style={{
-                      color: !disabled
-                        ? theme.palette.mode === 'dark'
-                          ? theme.palette.success.light
-                          : theme.palette.success.dark
-                        : theme.palette.grey[750]
-                    }}
+                    tooltip={t('update.button')}
+                    color="success"
                     disabled={disabled}
-                    onClick={() => handleAdd(workflow, runWorkflow)}
+                    onClick={() => handleUpdate.mutate(workflow, runWorkflow)}
                   >
                     <CheckIcon />
                   </IconButton>
-                </Tooltip>
+                </>
+              ) : (
+                <IconButton
+                  tooltip={t('add.button')}
+                  color="success"
+                  disabled={disabled}
+                  onClick={() => handleAdd.mutate(workflow, runWorkflow)}
+                >
+                  <CheckIcon />
+                </IconButton>
               )}
             </>
           }
@@ -280,7 +216,7 @@ const WrappedWorkflowCreate = ({ id: propID = null, onClose = () => null }: Prop
           <Grid size={{ xs: 12 }}>
             <TextInput
               label={t('name')}
-              loading={!workflow}
+              loading={!workflow || handleFetch.isFetching}
               value={!workflow ? null : workflow.name}
               required
               onChange={(event, value) => setWorkflow(wf => ({ ...wf, name: value }))}
@@ -290,7 +226,7 @@ const WrappedWorkflowCreate = ({ id: propID = null, onClose = () => null }: Prop
           <Grid size={{ xs: 12 }}>
             <TextInput
               label={t('query')}
-              loading={!workflow}
+              loading={!workflow || handleFetch.isFetching}
               value={!workflow ? null : workflow.query}
               required
               onChange={(event, value) => setWorkflow(wf => ({ ...wf, query: value }))}
@@ -300,7 +236,7 @@ const WrappedWorkflowCreate = ({ id: propID = null, onClose = () => null }: Prop
           <Grid size={{ xs: 12 }}>
             <ChipsInput
               label={t('labels')}
-              loading={!workflow}
+              loading={!workflow || handleFetch.isFetching}
               value={!workflow ? null : workflow.labels}
               options={LABELS}
               onChange={(event, value) => setWorkflow(wf => ({ ...wf, labels: value.map(v => v.toUpperCase()) }))}
@@ -311,7 +247,7 @@ const WrappedWorkflowCreate = ({ id: propID = null, onClose = () => null }: Prop
           <Grid size={{ xs: 12, sm: 6 }}>
             <SelectInput
               label={t('priority')}
-              loading={!workflow}
+              loading={!workflow || handleFetch.isFetching}
               value={!workflow ? null : workflow.priority}
               options={PRIORITIES.map(v => ({ primary: t(`priority.${v || 'null'}`), value: v }))}
               onChange={(event, value: Priority) => setWorkflow(wf => ({ ...wf, priority: value }))}
@@ -321,7 +257,7 @@ const WrappedWorkflowCreate = ({ id: propID = null, onClose = () => null }: Prop
           <Grid size={{ xs: 12, sm: 6 }}>
             <SelectInput
               label={t('status')}
-              loading={!workflow}
+              loading={!workflow || handleFetch.isFetching}
               value={!workflow ? null : workflow.status}
               options={STATUSES.map(v => ({ primary: t(`status.${v || 'null'}`), value: v }))}
               onChange={(event, value: Status) => setWorkflow(wf => ({ ...wf, status: value }))}
@@ -330,8 +266,8 @@ const WrappedWorkflowCreate = ({ id: propID = null, onClose = () => null }: Prop
 
           <Grid size={{ xs: 12 }}>
             <CheckboxInput
-              label={`${t('backport_workflow_prompt')} ${results?.total || 0} ${t('backport_workflow_matching')}`}
-              loading={!workflow}
+              label={`${t('backport_workflow_prompt')} ${handleResults.data?.total || 0} ${t('backport_workflow_matching')}`}
+              loading={!workflow || handleFetch.isFetching}
               preventRender={!!id}
               value={runWorkflow}
               onChange={(event, value) => setRunWorkflow(() => value)}
