9e588c26c4d84deff4a9a3b03987b255cf8dd15c
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2025-10-08 21:19:01 +0000

    Added the path to the Sandbox Ontology viewer

---
+++ src/components/routes/sandbox.tsx
@@ -0,0 +1,30 @@
+import PageCenter from 'commons/components/pages/PageCenter';
+import { useAPIQuery } from 'components/core/Query/API/useAPIQuery';
+import type { ResultOntology } from 'components/models/ontology/ontology';
+import { SandboxBody } from 'components/visual/ResultCard/sandbox_body';
+import React, { useMemo } from 'react';
+import { useParams } from 'react-router';
+
+type ParamProps = {
+  id: string;
+};
+
+export const Sandbox = React.memo(() => {
+  const { id } = useParams<ParamProps>();
+
+  const file = useAPIQuery<{ content: string; truncated: boolean }>({
+    url: `/api/v4/file/ascii/${id}/`
+  });
+
+  const ontology = useMemo(() => (!file?.data ? null : JSON.parse(file.data.content)) as ResultOntology, [file.data]);
+
+  return (
+    <PageCenter margin={4} width="100%">
+      <div style={{ textAlign: 'left' }}>
+        <SandboxBody body={ontology.results} />
+      </div>
+    </PageCenter>
+  );
+});
+
+export default Sandbox;
