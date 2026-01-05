9c32d29ac00a99bec7d279cd51e898d92d9311e2
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2026-01-05 16:02:43 +0000

    Removing some mutation that may be causing the useMutation to trigger multiple times.

--- src/components/core/Query/API/useAPIMutation.tsx
+++ src/components/core/Query/API/useAPIMutation.tsx
@@ -3,7 +3,6 @@ import type { APIRequest, APIResponse } from 'components/core/Query/components/a
 import type { UseAPICallFnProps } from 'components/core/Query/components/useAPICallFn';
 import { useAPICallFn } from 'components/core/Query/components/useAPICallFn';
 import { getAPIResponse } from 'components/core/Query/components/utils';
-import { useMemo } from 'react';
 
 export const useAPIMutation = <
   Props extends unknown[],
@@ -16,57 +15,46 @@ export const useAPIMutation = <
   const queryClient = useQueryClient();
   const apiCallFn = useAPICallFn<APIResponse<Response>>();
 
-  const mutation = useMutation<APIResponse<Response>, APIResponse<Error>, Props, unknown>(
+  const mutation = useMutation<APIResponse<Response>, APIResponse<Error>, Props>(
     {
-      mutationFn: async (props: Props) => apiCallFn(mutationFn(...props)) as Promise<never>
+      mutationFn: (props: Props) => apiCallFn(mutationFn(...props)),
+      retry: false
     },
     queryClient
   );
 
-  const { data, error, serverVersion, statusCode } = useMemo(
-    () => getAPIResponse(mutation.data, mutation.error, mutation.failureReason),
-    [mutation.data, mutation.error, mutation.failureReason]
+  const { data, error, serverVersion, statusCode } = getAPIResponse(
+    mutation.data,
+    mutation.error,
+    mutation.failureReason
   );
 
-  return useMemo(
-    () => ({
-      data: data,
-      error: error,
-      serverVersion: serverVersion,
-      statusCode: statusCode,
-      isError: mutation?.isError,
-      isIdle: mutation?.isIdle,
-      isPending: mutation?.isPending,
-      isPaused: mutation?.isPaused,
-      isSuccess: mutation?.isSuccess,
-      failureCount: mutation?.failureCount,
-      failureReason: mutation?.failureReason,
-      mutate: (...props: Props) => mutation?.mutate(props),
-      mutateAsync: async (...props: Props) => mutation?.mutateAsync(props),
-      reset: mutation?.reset,
-      status: mutation?.status,
-      submittedAt: mutation?.submittedAt,
-      variables: mutation?.variables
-    }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [
-      data,
-      error,
-      mutation?.failureCount,
-      mutation?.failureReason,
-      mutation?.isError,
-      mutation?.isIdle,
-      mutation?.isPaused,
-      mutation?.isPending,
-      mutation?.isSuccess,
-      mutation?.mutate,
-      mutation?.mutateAsync,
-      mutation?.reset,
-      mutation?.status,
-      mutation?.submittedAt,
-      mutation?.variables,
-      serverVersion,
-      statusCode
-    ]
-  );
+  return {
+    /** normalized API response */
+    data,
+    error,
+    serverVersion,
+    statusCode,
+
+    /** mutation state */
+    isError: mutation.isError,
+    isIdle: mutation.isIdle,
+    isPending: mutation.isPending,
+    isPaused: mutation.isPaused,
+    isSuccess: mutation.isSuccess,
+
+    /** failure info */
+    failureCount: mutation.failureCount,
+    failureReason: mutation.failureReason,
+
+    /** execution */
+    mutate: (props: Props) => mutation.mutate(props),
+    mutateAsync: (props: Props) => mutation.mutateAsync(props),
+
+    /** misc */
+    reset: mutation.reset,
+    status: mutation.status,
+    submittedAt: mutation.submittedAt,
+    variables: mutation.variables
+  };
 };
