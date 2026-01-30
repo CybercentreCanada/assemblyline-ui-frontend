0c664fbd659a4d36aec6729d26e5a109f4c2c126
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2026-01-30 20:13:29 +0000

    Removing the Input2 code

--- src/components/visual/Inputs2/models/inputs-validation.models.ts
+++
@@ -1,8 +0,0 @@
-export type ValidationState = 'default' | 'info' | 'success' | 'warning' | 'error';
-
-export const VALIDATION_PRIORITY: ValidationState[] = ['error', 'warning', 'success', 'info', 'default'];
-
-export type ValidationStatus = {
-  state?: ValidationState;
-  message?: string;
-};
