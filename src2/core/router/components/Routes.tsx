cf76e9fd10fb11c9ad811268764f946fcdeccbf2
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2026-02-19 14:57:45 +0000

    minimal changes

--- src2/core/router/components/Routes.tsx
+++ src2/core/router/components/Routes.tsx
@@ -1,4 +1,3 @@
-import { getRegisteredRoutes } from 'core/router/store/router.store';
 import { Page1 } from 'pages/Page1';
 import Page2 from 'pages/Page2';
 import Page3 from 'pages/Page3';
@@ -10,21 +9,11 @@ export type RoutesProps = {
   location?: RouterRoutesProps['location'];
 };
 
-export const Routes = React.memo(({ location }: RoutesProps) => {
-  const registeredRoutes = getRegisteredRoutes();
-
-  console.log(registeredRoutes);
-
-  return (
-    <RouterRoutes location={location}>
-      {/* {registeredRoutes.map(route => {
-        const RegisteredRoute = route.route;
-        return <RegisteredRoute key={route.path} />;
-      })} */}
-      <Route path="/page1" element={<Page1 />} />
-      {Page2.route}
-      {Page3.route}
-      <Route path="*" element={'null'} />
-    </RouterRoutes>
-  );
-});
+export const Routes = React.memo(({ location }: RoutesProps) => (
+  <RouterRoutes location={location}>
+    <Route path="/page1" element={<Page1 />} />
+    {Page2.route}
+    {Page3.route}
+    <Route path="*" element={'null'} />
+  </RouterRoutes>
+));
