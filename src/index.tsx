c1df5838d524e4d4898e68e5cb1af0e3d30931c9
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2026-07-24 15:06:55 +0000

    Upgraded Typescript to `7.0.2` from `5.8.3`

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import '@fontsource/roboto/700.css';
 import { App } from 'app/app';
 import 'app/core.i18n';
 import 'fontsource-roboto';
-import 'index.css';
 import { createRoot } from 'react-dom/client';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(<App />);
